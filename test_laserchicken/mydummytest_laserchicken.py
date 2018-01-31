a69edb187161eb7e2e5c35ddb0e694fb9e10fc04
Zsofia Koma <komazsofi@gmail.com>
2018-01-31 12:48:38 +0100

    reorganize the code

--- test_laserchicken/mydummytest_laserchicken.py
+++ test_laserchicken/mydummytest_laserchicken.py
@@ -6,26 +6,13 @@ from laserchicken.keys import point
 from laserchicken.spatial_selections import points_in_polygon_wkt
 from laserchicken import write_ply
 
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from mpl_toolkits.mplot3d import Axes3D
-
 pc = read_las.read("D:/GitHub/eEcoLiDAR/eEcoLiDAR/testdata/AHN2.las")
-#pc_out = points_in_polygon_wkt(pc_in, "POLYGON(( 243590.0 572110.0, 243640.0 572160.0, 243700.0 572110.0, 243640.0 572060.0, 243590.0 572110.0 ))")
 
-print(pc)
+write_ply.write(pc, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply_orig.ply")
 
-#fig = plt.figure()
-#ax1 = fig.add_subplot(111, projection='3d')
-#ax1.scatter(pc_out[point]['x']['data'], pc_out[point]['y']['data'],pc_out[point]['z']['data'])
-#ax1.set_xlabel('X')
-#ax1.set_ylabel('Y')
-#ax1.set_zlabel('Z')
-#ax1.azim = 65
-#ax1.elev = 5
-#plt.show()
+pc_out = points_in_polygon_wkt(pc, "POLYGON(( 243590.0 572110.0, 243640.0 572160.0, 243700.0 572110.0, 243640.0 572060.0, 243590.0 572110.0 ))")
 
-#write_ply.write(pc_out, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply.ply")
-#write_ply.write(pc, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply_orig2.ply")
+write_ply.write(pc_out, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply.ply")
+
 
 
