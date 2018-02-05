8665a29810cf353a3b7b75591c9c77f8154fba9b
Zsofia Koma <komazsofi@gmail.com>
2018-02-05 21:27:21 +0100

    add nei and fea calc test

--- test_laserchicken/mydummytest_laserchicken.py
+++ test_laserchicken/mydummytest_laserchicken.py
@@ -1,18 +1,24 @@
 import sys
-sys.path.insert(0, 'D:/GitHub/eEcoLiDAR/eEcoLiDAR/')
+sys.path.insert(0, 'C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/eEcoLiDAR/')
 
 from laserchicken import read_las
 from laserchicken.keys import point
 from laserchicken.spatial_selections import points_in_polygon_wkt
 from laserchicken import write_ply
+from laserchicken import compute_neighbors
+from laserchicken.feature_extractor.height_statistics_feature_extractor import HeightStatisticsFeatureExtractor
 
-pc = read_las.read("D:/GitHub/eEcoLiDAR/eEcoLiDAR/testdata/AHN2.las")
-
-write_ply.write(pc, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply_orig.ply")
-
+pc = read_las.read("C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/eEcoLiDAR/testdata/AHN2.las")
 pc_out = points_in_polygon_wkt(pc, "POLYGON(( 243590.0 572110.0, 243640.0 572160.0, 243700.0 572110.0, 243640.0 572060.0, 243590.0 572110.0 ))")
 
-write_ply.write(pc_out, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply.ply")
+indices=compute_neighbors.compute_cylinder_neighbourhood_indicies(pc_out, pc_out, 2.5)
+#print(len(indices))
+#print(pc_out[point]['z']['data'][indices])
+
+extractor = HeightStatisticsFeatureExtractor()
+(max_z, min_z, mean_z, median_z, std_z, var_z, range_z, coeff_var_z, skew_z, kurto_z) = extractor.extract(pc_out, indices,None,None)
+
+write_ply.write(pc_out, "C:/zsofia/Amsterdam/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply_hightfea.ply")
 
 
 
