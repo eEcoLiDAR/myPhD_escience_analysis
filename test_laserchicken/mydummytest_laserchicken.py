a59f7eaa332a9922b8ea7b2df923bf7523ed908f
Zsofia Koma <komazsofi@gmail.com>
2018-02-06 15:15:25 +0100

    cylinder for NAEM

--- test_laserchicken/mydummytest_laserchicken.py
+++ test_laserchicken/mydummytest_laserchicken.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, 'C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/eEcoLiDAR/')
+sys.path.insert(0, 'D:/GitHub/eEcoLiDAR/develop-branch/eEcoLiDAR/')
 
 from laserchicken import read_las
 from laserchicken.keys import point
@@ -8,13 +8,13 @@ from laserchicken import write_ply
 from laserchicken import compute_neighbors
 from laserchicken.feature_extractor.height_statistics_feature_extractor import HeightStatisticsFeatureExtractor
 
-pc = read_las.read("C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/eEcoLiDAR/testdata/AHN2.las")
-pc_out = points_in_polygon_wkt(pc, "POLYGON(( 243590.0 572110.0, 243640.0 572160.0, 243700.0 572110.0, 243640.0 572060.0, 243590.0 572110.0 ))")
+pc = read_las.read("D:/NAEM/Data/ALS_AHN2/SelStudyArea2_v2.las")
+pc_out = points_in_polygon_wkt(pc, "POLYGON((196550 446510,196550 446540,196580 446540,196580 446510,196550 446510))")
 
 indices=compute_neighbors.compute_cylinder_neighbourhood_indicies(pc_out, pc_out, 0.5)
 #print(np.array(indices).shape)
 #print(pc_out[point]['z']['data'][indices])
-#write_ply.write(pc_out, "C:/zsofia/Amsterdam/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply_hightfea.ply")
+write_ply.write(pc_out, "D:/NAEM/Data/ALS_AHN2/SelStudyArea2.ply")
 
 output_text = ""
 
@@ -24,7 +24,7 @@ for i in range(len(indices)):
 
     output_text += "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s \n" % (pc_out[point]['x']['data'][i],pc_out[point]['y']['data'][i],pc_out[point]['z']['data'][i],max_z, min_z, mean_z, median_z, std_z, var_z, range_z, coeff_var_z, skew_z, kurto_z)
 
-fileout = open('C:/zsofia/Amsterdam/GitHub/komazsofi/myPhD_escience_analysis/test_data/pc_withfea.txt', "w")
+fileout = open('D:/NAEM/Data/ALS_AHN2/SelStudyArea2_withfea.txt', "w")
 fileout.write(output_text)
 fileout.close()
 
