3483c1ef44e963906e94dc1c8694a8a712b3b30e
Zsofia Koma <komazsofi@gmail.com>
2018-02-06 16:10:36 +0100

    small changes

--- test_laserchicken/mydummytest_laserchicken.py
+++ test_laserchicken/mydummytest_laserchicken.py
@@ -11,10 +11,11 @@ from laserchicken.feature_extractor.height_statistics_feature_extractor import H
 pc = read_las.read("D:/NAEM/Data/ALS_AHN2/SelStudyArea2_v2.las")
 pc_out = points_in_polygon_wkt(pc, "POLYGON((196550 446510,196550 446540,196580 446540,196580 446510,196550 446510))")
 
-indices=compute_neighbors.compute_cylinder_neighbourhood_indicies(pc_out, pc_out, 0.5)
+#indices=compute_neighbors.compute_cylinder_neighbourhood_indicies(pc_out, pc_out, 0.5)
+indices=compute_neighbors.compute_sphere_neighbourhood_indicies(pc_out, pc_out, 1)
 #print(np.array(indices).shape)
 #print(pc_out[point]['z']['data'][indices])
-write_ply.write(pc_out, "D:/NAEM/Data/ALS_AHN2/SelStudyArea2.ply")
+#write_ply.write(pc_out, "D:/NAEM/Data/ALS_AHN2/SelStudyArea2.ply")
 
 output_text = ""
 
@@ -27,4 +28,3 @@ for i in range(len(indices)):
 fileout = open('D:/NAEM/Data/ALS_AHN2/SelStudyArea2_withfea.txt', "w")
 fileout.write(output_text)
 fileout.close()
-
