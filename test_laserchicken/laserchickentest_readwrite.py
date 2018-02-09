ee2d4e5a907f88e25d384b189976127ca7d407dd
Zsofia Koma <komazsofi@gmail.com>
2018-02-09 12:37:39 +0100

    issue #63 closed

--- test_laserchicken/laserchickentest_readwrite.py
+++ test_laserchicken/laserchickentest_readwrite.py
@@ -5,5 +5,5 @@ from laserchicken import write_ply
 pc = read_las.read("D:/GitHub/eEcoLiDAR/eEcoLiDAR/testdata/AHN2.las")
 pc_out = points_in_polygon_wkt(pc, "POLYGON(( 243590.0 572110.0, 243640.0 572160.0, 243700.0 572110.0, 243640.0 572060.0, 243590.0 572110.0 ))")
 
-write_ply.write(pc, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply_orig.ply")
-write_ply.write(pc_out, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply.ply")
+write_ply.write(pc, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/testply_orig1.ply")
+write_ply.write(pc_out, "D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/withinplygonply1.ply")
