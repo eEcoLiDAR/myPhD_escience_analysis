0175dc15b4121d151e765117ad9879a02b3c5cdc
Zsofia Koma <komazsofi@gmail.com>
2018-02-22 17:16:13 +0100

    small changes

--- test_laserchicken/test_GEOBIA_v22Febr.py
+++ test_laserchicken/test_GEOBIA_v22Febr.py
@@ -27,17 +27,15 @@ start1 = time.time()
 compute_features(pc_sub, indices_cyl, pc_sub, ['max_z','min_z','mean_z','median_z','std_z','var_z','range','coeff_var_z','skew_z','kurto_z',
                                                'eigenv_1','eigenv_2','eigenv_3','z_entropy','sigma_z','perc_20','perc_40','perc_60','perc_80'], InfiniteCylinder(5))
 
-feadataframe=pd.DataFrame({'max_z':pc_sub[point]['max_z']['data'],'min_z':pc_sub[point]['min_z']['data'], 'mean_z':pc_sub[point]['mean_z']['data'],
+feadataframe=pd.DataFrame({'_x':pc_sub[point]['x']['data'],'_y':pc_sub[point]['y']['data'],'_z':pc_sub[point]['z']['data'],
+                           'max_z':pc_sub[point]['max_z']['data'],'min_z':pc_sub[point]['min_z']['data'], 'mean_z':pc_sub[point]['mean_z']['data'],
                            'median_z':pc_sub[point]['median_z']['data'],'std_z':pc_sub[point]['std_z']['data'], 'var_z':pc_sub[point]['var_z']['data'],
                            'range':pc_sub[point]['range']['data'],'coeff_var_z':pc_sub[point]['coeff_var_z']['data'], 'skew_z':pc_sub[point]['skew_z']['data'],'kurto_z':pc_sub[point]['kurto_z']['data'],
                            'eigenv_1':pc_sub[point]['eigenv_1']['data'],'eigenv_2':pc_sub[point]['eigenv_2']['data'], 'eigenv_3':pc_sub[point]['eigenv_3']['data'],
                            'z_entropy':pc_sub[point]['z_entropy']['data'],'sigma_z':pc_sub[point]['sigma_z']['data'], 'perc_20':pc_sub[point]['perc_20']['data'],
-                           'perc_40':pc_sub[point]['perc_40']['data'],'perc_60':pc_sub[point]['perc_60']['data'], 'perc_80':pc_sub[point]['perc_80']['data'],
-                           'z':pc_sub[point]['z']['data'],'y':pc_sub[point]['y']['data'],'x':pc_sub[point]['x']['data']})
+                           'perc_40':pc_sub[point]['perc_40']['data'],'perc_60':pc_sub[point]['perc_60']['data'], 'perc_80':pc_sub[point]['perc_80']['data']})
 
 feadataframe.to_csv('D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/SelStudyArea2_withfea2.csv',sep=";",index=False)
-
-#'eigenv_1':pc_sub[point]['eigenv_1']['data'],'eigenv_2':pc_sub[point]['eigenv_2']['data'], 'eigenv_3':pc_sub[point]['eigenv_3']['data'],
 
 end1 = time.time()
 difftime1=end1 - start1
