ceaf0e0b71a5b08b06a875f11caeeb754a88c04e
Zsofia Koma <komazsofi@gmail.com>
2018-02-22 10:06:29 +0100

    test feature extractor

--- test_laserchicken/test_GEOBIA_vxxxx.py
+++ test_laserchicken/test_GEOBIA_vxxxx.py
@@ -7,17 +7,47 @@ from laserchicken.volume_specification import Sphere, InfiniteCylinder
 from laserchicken.feature_extractor import compute_features
 
 import numpy as np
+import time
 
 # Import
-pc = read_las.read("D:/Wetland/GEOBIA/Data/AHN2/12en1_sel2_ground_height_clipped.las")
+pc = read_las.read("D:/NAEM/Data/ALS_AHN2/SelStudyArea2_v2.las")
+pc_sub = points_in_polygon_wkt(pc, "POLYGON((196550 446510,196550 446540,196580 446540,196580 446510,196550 446510))")
 
 # Neighborhood calculation
 
-indices_cyl=compute_neighborhoods(pc, pc, InfiniteCylinder(5))
+start = time.time()
+indices_cyl=compute_neighborhoods(pc_sub, pc_sub, InfiniteCylinder(5))
+end = time.time()
+difftime=end - start
+print(("build kd-tree: %f sec") % (difftime))
 
+output_text1 = ""
+start1 = time.time()
 for i in range(len(indices_cyl)):
-    features=compute_features(pc, indices_cyl, pc, ['eigenv_1', 'eigenv_2', 'eigenv_3'], InfiniteCylinder(5))
-    print(features)
+    features=compute_features(pc_sub, indices_cyl, pc_sub, ['max_z'], InfiniteCylinder(5))
 
+    output_text1 += "%s,%s,%s,%s\n" % (
+    pc_sub[point]['x']['data'][i], pc_sub[point]['y']['data'][i], pc_sub[point]['z']['data'][i],features)
 
+end1 = time.time()
+difftime1=end1 - start1
+print(("New laserchicken version calculation of max z took: %f sec") % (difftime1))
+
+fileout = open('D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/SelStudyArea2_withfea.txt', "w")
+fileout.write(output_text1)
+fileout.close()
+
+output_text2 = ""
+start2 = time.time()
+for i in range(len(indices_cyl)):
+    max_z=np.max(pc_sub[point]['z']['data'][indices_cyl[i]])
+    output_text2 += "%s,%s,%s,%s\n" % (
+    pc_sub[point]['x']['data'][i], pc_sub[point]['y']['data'][i], pc_sub[point]['z']['data'][i],max_z)
+end2 = time.time()
+difftime2=end2 - start2
+print(("Simple calculation of max z took: %f sec") % (difftime2))
+
+fileout = open('D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/SelStudyArea2_withfea2.txt', "w")
+fileout.write(output_text2)
+fileout.close()
 
