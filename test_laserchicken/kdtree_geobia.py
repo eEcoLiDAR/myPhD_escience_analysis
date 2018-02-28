b61c51d0c51035e93594a2098154f6000a562f9d
Zsofia Koma <komazsofi@gmail.com>
2018-02-28 13:58:42 +0100

    start to build pipeline

---
+++ test_laserchicken/kdtree_geobia.py
@@ -0,0 +1,31 @@
+import argparse
+import time
+import pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path_of_laserchicken', help='The path of laserchicken')
+parser.add_argument('input', help='absolute path of input point cloud')
+parser.add_argument('output', help='absolute path of output point cloud')
+args = parser.parse_args()
+
+import sys
+sys.path.insert(0, args.path_of_laserchicken)
+
+from laserchicken import read_las
+from laserchicken.volume_specification import Sphere, InfiniteCylinder
+from laserchicken.compute_neighbors import compute_neighborhoods
+
+# Import
+pc = read_las.read(args.input)
+
+# Neighborhood calculation
+
+start = time.time()
+indices_cyl=compute_neighborhoods(pc, pc, Sphere(2.5))
+end = time.time()
+difftime=end - start
+print(("build kd-tree: %f sec") % (difftime))
+
+output = open('kdtree.pkl', 'wb')
+pickle.dump(indices_cyl, output)
+output.close()
