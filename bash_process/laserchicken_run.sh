689e7ada463e8f2311c7ece200425862c069330e
Zsofia Koma <komazsofi@gmail.com>
2018-03-01 14:57:24 +0100

    add remove function

--- bash_process/laserchicken_run.sh
+++ bash_process/laserchicken_run.sh
@@ -1,9 +1,28 @@
 # download the data from WebDAV
 
-curl --netrc-file D:/GitHub/mypassword.netrc https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Data/Lauwersmeer/02gz2_merged_kiv1_kivtest.las --output D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/02gz2_merged_kiv1_kivtest.las 
+passw="$(<D:/GitHub/passw.txt)"
+
+#curl --insecure --fail --location --user $passw https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Data/Lauwersmeer/02gz2_merged_kiv1_kivtest.las --output D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest.las 
+#curl --insecure --fail --location --user zkoma https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Data/Lauwersmeer/02gz2_merged_kiv1_kivtest.las --output D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest.las
 
 # kd-tree
+
+#python D:/GitHub/komazsofi/myPhD_escience_analysis/test_laserchicken/kdtree_geobia_sphere.py D:/GitHub/eEcoLiDAR/develop-branch/eEcoLiDAR/ D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest.las D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest_sphere4.pkl 4
+
+#python D:/GitHub/komazsofi/myPhD_escience_analysis/test_laserchicken/kdtree_geobia_cylinder.py D:/GitHub/eEcoLiDAR/develop-branch/eEcoLiDAR/ D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest.las D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest_cylinder4.pkl 4
 
 # feature calculation
 
 # upload the data to WebDAV and delete on the local machines
+
+echo "--------Upload is started--------"
+
+#curl --insecure --fail --location --user $passw --upload-file D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest_sphere4.pkl https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Results/
+#curl --insecure --fail --location --user $passw --upload-file D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/m02gz2_merged_kiv1_kivtest_cylinder4.pkl https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Results/
+
+echo "--------Remove unnecessary files--------"
+
+#rm D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/*.pkl
+#rm D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/*.las
+
+echo "--------Script is finished--------"
