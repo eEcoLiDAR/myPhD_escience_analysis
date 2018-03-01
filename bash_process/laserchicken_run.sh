90de1ad79dd7b94337292a6956fce05ba4d452eb
Zsofia Koma <komazsofi@gmail.com>
2018-03-01 11:27:31 +0100

    add workflow process

---
+++ bash_process/laserchicken_run.sh
@@ -0,0 +1,9 @@
+# download the data from WebDAV
+
+curl --netrc-file D:/GitHub/mypassword.netrc https://webdav.grid.sara.nl/pnfs/grid.sara.nl/data/projects.nl/eecolidar/01_Work/zsofia/geobia/Data/Lauwersmeer/02gz2_merged_kiv1_kivtest.las --output D:/GitHub/komazsofi/myPhD_escience_analysis/test_data/02gz2_merged_kiv1_kivtest.las 
+
+# kd-tree
+
+# feature calculation
+
+# upload the data to WebDAV and delete on the local machines
