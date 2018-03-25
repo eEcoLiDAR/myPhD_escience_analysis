c5fc85142b78915ad6a1163a1c11315bc7fa123a
Zsofia Koma <komazsofi@gmail.com>
2018-03-25 19:35:45 +0200

    small fixes

--- analysis/RGB.py
+++ analysis/RGB.py
@@ -1,7 +1,15 @@
 """
+@author: Zsofia Koma, UvA
 Aim: 
-1. rasterize
-2. rescale 3 features into an RGB composite
+
+Input: laserchicken ply file with the calculated features (currently I am only using z), 3 features which we would like to use for colorization 
+Output: XYZRGB ascii
+
+Example usage (from command line):  python RGB.py C:/zsofia/Amsterdam/Geobia/Features/ tile_207500_598000_1_1.las.ply tile_207500_598000_1_1 echo_ratio e
+igenv_1 sigma_z
+
+ToDo: 
+
 """
 
 import sys
@@ -18,17 +26,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('path', help='where the files are located')
 parser.add_argument('features', help='point cloud with features')
 parser.add_argument('output', help='name of output')
+parser.add_argument('Red', help='Red')
+parser.add_argument('Green', help='Green')
+parser.add_argument('Blue', help='Blue')
 args = parser.parse_args()
 
-pc_wfea = pd.read_csv(args.path+args.features,sep=' ',names=['X','Y','Z','coeff_var_z','density_absolute_mean','echo_ratio','gps_time','intensity','kurto_z','max_z','mean_z',
-'median_z','min_z','perc_10','perc_100','perc_20','perc_30','perc_40','perc_50','perc_60','perc_70','perc_80','perc_90','pulse_penetration_ratio','range','raw_classification','sigma_z','skew_z','std_z','var_z'],skiprows=41)
+pc_wfea =pd.read_csv(args.path+args.features,sep=' ',names=['X','Y','Z','coeff_var_z','density_absolute_mean','echo_ratio','eigenv_1','eigenv_2','eigenv_3','gps_time','intensity','kurto_z','max_z','mean_z',
+'median_z','min_z','normal_vector_1','normal_vector_2','normal_vector_3','pulse_penetration_ratio','range','raw_classification','sigma_z','skew_z','slope','std_z','var_z'],skiprows=39)
 #print(pc_wfea.dtypes)
 
 # rescale on attribute into 0-255
 
 # RED
 
-attribute_R='sigma_z'
+attribute_R=args.Red
 
 min_orig_R=np.min(pc_wfea[attribute_R].values)
 max_orig_R=np.max(pc_wfea[attribute_R].values)
@@ -50,7 +61,7 @@ plt.show()
 
 # GREEN
 
-attribute_G='kurto_z'
+attribute_G=args.Green
 
 min_orig_G=np.min(pc_wfea[attribute_G].values)
 max_orig_G=np.max(pc_wfea[attribute_G].values)
@@ -64,7 +75,7 @@ Green_value=k_G*(pc_wfea[attribute_G].values-min_orig_G)+min_wish_G
 
 # BLUE
 
-attribute_B='range'
+attribute_B=args.Blue
 
 min_orig_B=np.min(pc_wfea[attribute_B].values)
 max_orig_B=np.max(pc_wfea[attribute_B].values)
@@ -82,4 +93,4 @@ pc_wfea['Red']=Red_value
 pc_wfea['Green']=Green_value
 pc_wfea['Blue']=Blue_value
 
-pc_wfea[['X','Y','Z','Red','Green','Blue']].to_csv(args.path+args.output,sep=';')
+pc_wfea[['X','Y','Z','Red','Green','Blue']].to_csv(args.path+args.output+str(attribute_R)+str(attribute_G)+str(attribute_B)+'.csv',sep=';')
