2609502e941d4e96a1c25d3293c945c4eeb845cb
Zsofia Koma <komazsofi@gmail.com>
2018-03-25 20:49:21 +0200

    small fix

--- grassgis_process/regiongrowing_ingrass.bat
+++ grassgis_process/regiongrowing_ingrass.bat
@@ -1,5 +1,6 @@
 r.in.xyz -s -g separator="," in=C:\zsofia\Amsterdam\Geobia\Features\tile_207500_598000_1_1.txt skip=1
 g.region n=598499.94 s=598000 e=207999.98 w=207649.25
+
 r.in.xyz --overwrite input=C:\zsofia\Amsterdam\Geobia\Features\tile_207500_598000_1_1.txt output=tile_207500_598000_1_1_echo separator=, skip=1 value_column=6
 r.out.gdal --overwrite input=tile_207500_598000_1_1_echo output=C:\zsofia\Amsterdam\Geobia\Features\tile_207500_598000_1_1_echo.tif
 
