122c0f0de81e58a007891ff73b34bf2263967336
Zsofia Koma <komazsofi@gmail.com>
2018-03-26 16:36:52 +0200

    fix on DT

--- analysis/decisiontree_forsegments.py
+++ analysis/decisiontree_forsegments.py
@@ -5,10 +5,12 @@ Aim: apply DT to classify segments
 Input: 
 Output: 
 
-Example usage (from command line): 
+Example usage (from command line): python decisiontree_forsegments.py D:/Geobia_2018/Lauw_island_tiles/ tile_208000_598000_1_1.las.plywfea.shp vlakken_union_structuur.shp
 
 ToDo: 
 
+Comment:
+segment_wlabel = gpd.GeoDataFrame( pd.concat( [segment_reed,segment_grass,segment_bushes,segment_openwater], ignore_index=True) )
 """
 
 import sys
@@ -28,6 +30,8 @@ from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import graphviz 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='where the files are located')
 parser.add_argument('segments', help='polygon shape file with class')
@@ -40,10 +44,8 @@ segments = gpd.GeoDataFrame.from_file(args.path+args.segments)
 training = gpd.GeoDataFrame.from_file(args.path+args.training)
 #print(training.head())
 
-segment_wlabel = gpd.sjoin(segments, training, how="inner", op='intersects')
-#print(segment_wlabel.dtypes)
-
-#segment_wlabel.to_file(args.path+args.segments+"wfealabel.shp", driver='ESRI Shapefile')
+segment_wlabel = gpd.sjoin(segments, training, how="inner", op='within')
+segment_wlabel.to_file(args.path+args.segments+"wfealabel.shp", driver='ESRI Shapefile')
 
 feature=segment_wlabel[['echo_ratio','max_z','kurto_z','pulse_pene']].values
 label=segment_wlabel['structyp_e'].values
@@ -51,11 +53,14 @@ label=segment_wlabel['structyp_e'].values
 mytrain, mytest, mytrainlabel, mytestlabel = train_test_split(feature,label,train_size = 0.6)
 target=['Grasland','Landriet, structuurrijk','Open water','Struweel']
 
-DT = tree.DecisionTreeClassifier()
+DT = tree.DecisionTreeClassifier(max_depth=5,class_weight="balanced")
 DT_classifier = DT.fit(mytrain, mytrainlabel)
 
 mypredtest=DT_classifier.predict(mytest)
 
 print(classification_report(mytestlabel, mypredtest,target_names=target))
 print(confusion_matrix(mytestlabel, mypredtest))
+
+dot_data = tree.export_graphviz(DT_classifier, out_file=args.path+args.segments+".dotfile") 
+
 
