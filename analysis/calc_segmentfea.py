c4764ccde1a1f3831c2219fc0faa031406055684
Zsofia Koma <komazsofi@gmail.com>
2018-04-02 14:14:45 +0200

    adapt for the new input

--- analysis/calc_segmentfea.py
+++ analysis/calc_segmentfea.py
@@ -37,8 +37,11 @@ crs = {'init': 'epsg:28992'}
 # Import
 segments = gpd.GeoDataFrame.from_file(args.path+args.segments+'.shp',crs=crs)
 
+"""
 pc_wfea = pd.read_csv(args.path+args.features+'.ply',sep=' ',names=['X','Y','Z','coeff_var_z','density_absolute_mean','echo_ratio','eigenv_1','eigenv_2','eigenv_3','gps_time','intensity','kurto_z','max_z','mean_z',
 'median_z','min_z','normal_vector_1','normal_vector_2','normal_vector_3','pulse_penetration_ratio','range','raw_classification','sigma_z','skew_z','slope','std_z','var_z'],skiprows=39)
+"""
+pc_wfea =pd.read_csv(args.path+args.features,sep=',')
 pc_wfea['geometry'] = pc_wfea.apply(lambda z: Point(z.X, z.Y), axis=1)
 pc_wfea = gpd.GeoDataFrame(pc_wfea,crs=crs)
 
@@ -57,12 +60,13 @@ labeled_segments.drop_duplicates('value').to_file(args.path+args.segments+"wlabe
 pointInPolys = sjoin(pc_wfea , segments, how='left',op='within')
 
 # Calculate aggreagted statistical features
-fea_insegments_mean=pointInPolys.groupby('value')['density_absolute_mean','echo_ratio','eigenv_1','eigenv_2','eigenv_3','kurto_z','max_z','mean_z','median_z','min_z','normal_vector_1','normal_vector_2','normal_vector_3','pulse_penetration_ratio','range','sigma_z','skew_z','slope','std_z','var_z'].mean().add_prefix('mean_').reset_index()
-fea_insegments_std=pointInPolys.groupby('value')['density_absolute_mean','echo_ratio','eigenv_1','eigenv_2','eigenv_3','kurto_z','max_z','mean_z','median_z','min_z','normal_vector_1','normal_vector_2','normal_vector_3','pulse_penetration_ratio','range','sigma_z','skew_z','slope','std_z','var_z'].std(ddof=0).add_prefix('std_').reset_index()
-fea_insegments_median=pointInPolys.groupby('value')['density_absolute_mean','echo_ratio','eigenv_1','eigenv_2','eigenv_3','kurto_z','max_z','mean_z','median_z','min_z','normal_vector_1','normal_vector_2','normal_vector_3','pulse_penetration_ratio','range','sigma_z','skew_z','slope','std_z','var_z'].median().add_prefix('median_').reset_index()
+fea_insegments_mean=pointInPolys.groupby('value')['echo_ratio','Planarity','Sphericity','Curviture','kurto_z','max_z','mean_z','median_z','pulse_penetration_ratio','range','sigma_z','skew_z','std_z','var_z'].mean().add_prefix('mean_').reset_index()
+fea_insegments_std=pointInPolys.groupby('value')['echo_ratio','Planarity','Sphericity','Curviture','kurto_z','max_z','mean_z','median_z','pulse_penetration_ratio','range','sigma_z','skew_z','std_z','var_z'].std(ddof=0).add_prefix('std_').reset_index()
+fea_insegments_median=pointInPolys.groupby('value')['echo_ratio','Planarity','Sphericity','Curviture','kurto_z','max_z','mean_z','median_z','pulse_penetration_ratio','range','sigma_z','skew_z','std_z','var_z'].median().add_prefix('median_').reset_index()
 
 fea_insegments1 = fea_insegments_mean.merge(fea_insegments_std, on='value')
 fea_insegments = fea_insegments1.merge(fea_insegments_median, on='value')
+fea_insegments = fea_insegments.drop_duplicates('value')
 
 # Calculate shape geometry features
 labeled_segments['poly_area']=labeled_segments['geometry'].area
