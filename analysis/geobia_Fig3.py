c0a28382892b55257e21fb0cb552cac9486e22ac
Zsofia Koma <komazsofi@gmail.com>
2018-04-14 23:16:39 +0200

    add uspo anal visualization

---
+++ analysis/geobia_Fig3.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+font = {'family': 'normal',
+        'weight': 'bold',
+        'size': 14}
+
+plt.rc('font', **font)
+
+workdir='C:/zsofia/Amsterdam/Geobia/Work_13April/'
+filename='1_all_tiles_clean.txt_PC1__PC2__PC3segm_parameters'
+
+segm_optimal=pd.read_csv(workdir+filename+'.csv',sep=',')
+#print(segm_optimal.dtypes)
+
+segm_optimal=segm_optimal[segm_optimal['Minimum size']!=50]
+
+segm_optimal=segm_optimal.sort_values(by=['Threshold'])
+
+"""
+fig, ax = plt.subplots(figsize=(8,6))
+segm_optimal.groupby('Minimum size').plot(x='threshold',y='optimization_criteria',ax=ax,marker='o')
+plt.show()
+"""
+
+sns.pointplot(x='Threshold',y='Optimization criteria',hue='Minimum size',data=segm_optimal,linestyle="-")
+plt.show()
