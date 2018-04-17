1cfb8962f43b9960e7fe5f3d01e2030511aa2556
Zsofia Koma <komazsofi@gmail.com>
2018-04-17 16:03:18 +0200

    small changes, rename coloumns

--- analysis/geobia_Fig3.py
+++ analysis/geobia_Fig3.py
@@ -5,19 +5,21 @@ import seaborn as sns
 
 font = {'family': 'normal',
         'weight': 'bold',
-        'size': 14}
+        'size': 18}
 
 plt.rc('font', **font)
 
-workdir='D:/Koma/DataProcess/Process_seltiles/'
+workdir='D:/Geobia_2018/Results_17ofApril/'
 filename='3all_tiles_clean.txt_PC1__PC2__PC3segm_parameters'
 
 segm_optimal=pd.read_csv(workdir+filename+'.csv',sep=',')
-#print(segm_optimal.dtypes)
+
+segm_optimal.rename(columns={'Threshold': 'Similarity threshold'}, inplace = True)
+print(segm_optimal.dtypes)
 
 segm_optimal=segm_optimal[segm_optimal['Minimum size']!=50]
 
-segm_optimal=segm_optimal.sort_values(by=['Threshold'])
+segm_optimal=segm_optimal.sort_values(by=['Similarity threshold'])
 
 """
 fig, ax = plt.subplots(figsize=(8,6))
@@ -25,5 +27,5 @@ segm_optimal.groupby('Minimum size').plot(x='threshold',y='optimization_criteria
 plt.show()
 """
 
-sns.pointplot(x='Threshold',y='Optimization criteria',hue='Minimum size',data=segm_optimal,linestyle="-")
+sns.pointplot(x='Similarity threshold',y='Optimization criteria',hue='Minimum size',data=segm_optimal,linestyle="-")
 plt.show()
