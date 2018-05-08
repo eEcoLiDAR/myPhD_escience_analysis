aee0ee813f9b2dcbb9f3d1d42584c67f5f0443af
Zsofia Koma <komazsofi@gmail.com>
2018-05-08 11:49:04 +0200

    add art. point cloud generator las export

--- test_laserchicken/helping_scripts/preprocessing/generate_targets.py
+++ test_laserchicken/helping_scripts/preprocessing/generate_targets.py
@@ -10,40 +10,38 @@ et.txt
 then txt->las txt2las -i D:\Results\Geobia\06en2_merged_kiv3._ground_height_kiv_kiv_target.txt -o D:\Results\Geobia\06en2_merged_kiv3._ground_height_kiv_kiv_target.las -parse xyzitc
 
 ToDo: 
-1. make direct las file export
 """
 
 import sys
 import argparse
+
+import laspy
+from laspy.file import File
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 parser = argparse.ArgumentParser()
-parser.add_argument('path_of_laserchicken', help='The path of laserchicken')
-parser.add_argument('path', help='The path of files')
 parser.add_argument('input', help='absolute path of input point cloud')
 parser.add_argument('output', help='absolute path of output point cloud')
 args = parser.parse_args()
 
-sys.path.insert(0, args.path_of_laserchicken)
+# read las file
 
-from laserchicken import read_las
-from laserchicken.keys import point
+in_pc = File(args.input, mode='r')
+in_pc_nparray = np.vstack([in_pc.x, in_pc.y, in_pc.z]).transpose()
 
-pc = read_las.read(args.path+args.input)
-
-print(("Number of points: %s ") % (pc[point]['x']['data'].shape[0]))
+print(("Number of points are: %s") % (in_pc_nparray.shape[0]))
 
 # generate grid in 2D z==0 (for 2.5D analysis within cylinder)
 
-min_x=np.min(pc[point]['x']['data'])
-max_x=np.max(pc[point]['x']['data'])
+min_x=np.min(in_pc_nparray[:,0])
+max_x=np.max(in_pc_nparray[:,0])
 step_x=1
 
-min_y=np.min(pc[point]['y']['data'])
-max_y=np.max(pc[point]['y']['data'])
+min_y=np.min(in_pc_nparray[:,1])
+max_y=np.max(in_pc_nparray[:,1])
 step_y=1
 
 bound_x = np.arange(min_x, max_x, step_x)
@@ -61,13 +59,13 @@ y=np.ravel(target_y)
 z=np.ones(len(x))
 
 false_intensity=np.zeros(len(x))
-false_gpstime=np.zeros(len(x))
-false_classification=np.zeros(len(x))
 
 #plt.scatter(x,y)
 #plt.show()
 
-xyz=np.vstack((x,y,z,false_intensity,false_gpstime,false_classification)).T
-
-np.savetxt(args.path+args.output, xyz, fmt='%1.5f %1.5f %1.5f %1.5f %1.5f %1.5f')
-
+out_LAS = File(args.output, mode = "w", header = in_pc.header)
+out_LAS.x = x
+out_LAS.y = y
+out_LAS.z = z
+out_LAS.intensity = false_intensity
+out_LAS.close()
