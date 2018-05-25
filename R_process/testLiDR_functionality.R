879d329f2ead9f69727a6827e5322a775ec5e511
Zsofia Koma <komazsofi@gmail.com>
2018-05-25 09:57:27 +0200

    add single tree segmentation

--- R_process/testLiDR_functionality.R
+++ R_process/testLiDR_functionality.R
@@ -1,9 +1,30 @@
 library("lidR")
+library("raster")
 
-las = readLAS("D:/Geobia_2018/Lau_tiles/Lauw_island_tiles/tile_208500_598500_1_1.las")
+las = readLAS("D:/Sync/_Amsterdam/Writing/MScThesis/tristan/Tristan_paper/g32hz1rect2.las")
 
-hmean = grid_metrics(las, mean(Z),res=1)
+hmean = grid_metrics(las, max(Z),res=1)
 plot(hmean)
 
-hmean_voxel=grid_metrics3d(las, mean(Z),res=1)
-plot(hmean_voxel)
+# ground classification
+lasground(las, "pmf", 1, 0.5)
+dtm = grid_terrain(las, method = "knnidw", k = 10L)
+plot3d(dtm)
+
+# normalization
+lasnormalize(las, method = "knnidw", k = 10L)
+plot(las)
+
+chm = grid_canopy(las, res = 1, subcircle = 2, na.fill = "knnidw", k = 1)
+chm = as.raster(chm)
+plot(chm)
+
+crowns = lastrees(las, "watershed", chm, th = 0.2, extra = TRUE)
+contour = rasterToPolygons(crowns, dissolve = TRUE)
+
+plot(hmean)
+plot(contour, add = T,lwd=4)
+
+ttops = tree_detection(hmean, 1, 2)
+lastrees_dalponte(las, hmean, ttops)
+plot(las, color = "treeID")
