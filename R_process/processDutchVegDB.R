7ff0d83e84400036e4d221e2fb26010130cf9ab5
Zsofia Koma <komazsofi@gmail.com>
2018-05-28 10:40:52 +0200

    add dummy visaulization

--- R_process/processDutchVegDB.R
+++ R_process/processDutchVegDB.R
@@ -9,7 +9,8 @@ Function:
 1. Import Dutch vegetation database
 2. Filter out based on year and maximum coordinate uncertainity
 3. Points convert into polygon based on recorded length and width
-4. Export as polygon for GEE
+4. Visualization
+5. Export as polygon for GEE
   
 Example usage (from command line):   
   
@@ -18,6 +19,8 @@ ToDo:
 
 # call the required libraries
 library("stringr")
+
+library("rworldmap")
 
 # set global variables
 setwd("D:/GitHub/eEcoLiDAR/myPhD_escience_analysis/test_data") # working directory
@@ -30,4 +33,12 @@ VegDB_header=read.csv(file="Swamp_communities_header.csv",header=TRUE,sep="\t")
 # introduce filters
 VegDB_header$year=as.numeric(str_sub(VegDB_header$Datum.van.opname,-4,-1)) # define a numeric year attribute
 
-VegDB_header_filtered=VegDB_header[ which(VegDB_header$year>2012),]
+VegDB_header_filtered=VegDB_header[ which(VegDB_header$year>min_year),]
+
+# Visualize
+
+# the point data
+
+newmap <- getMap(resolution = "low")
+plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1)
+points(VegDB_header_filtered$Longitude, VegDB_header_filtered$Latitude, col = "red", cex = .6)
