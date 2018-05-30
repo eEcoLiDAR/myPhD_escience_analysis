3dc3a3e371c86f18f120df1dcbbef7bf9b539453
Zsofia Koma <komazsofi@gmail.com>
2018-05-30 09:59:49 +0200

    export between years

--- R_process/processBirdData.R
+++ R_process/processBirdData.R
@@ -16,7 +16,7 @@ ToDo:
 1.
 
 Question:
-1. 
+1. coordinate uncertainity problem
 "
 
 # call the required libraries
@@ -28,17 +28,18 @@ library("raster")
 # set global variables
 setwd("D:/GitHub/eEcoLiDAR/myPhD_escience_analysis/test_data/birddata") # working directory
 
-min_year=2004
+year_min=2008
+year_max=2010
 
 # import data
 bird_data=read.csv(file="avimap_observations_reedland_birds.csv",header=TRUE,sep=";")
 
 # introduce filters
 
-bird_data_filtered=bird_data[ which(bird_data$year>min_year),]
+bird_data_filtered=bird_data[ which(bird_data$year>year_min & bird_data$year<year_max),]
 
 # Export the point data
 
 coordinates(bird_data_filtered)=~x+y
 proj4string(bird_data_filtered)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-raster::shapefile(bird_data_filtered, "bird_points.shp")
+raster::shapefile(bird_data_filtered, "bird_points.shp",overwrite=TRUE)
