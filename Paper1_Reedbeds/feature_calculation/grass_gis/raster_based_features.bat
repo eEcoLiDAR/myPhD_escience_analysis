cd2d52956448e303f2cdb76cf494ab20f44c4cd1
Zsofia Koma <komazsofi@gmail.com>
2018-06-06 11:35:21 +0200

    add new directories

---
+++ Paper1_Reedbeds/feature_calculation/grass_gis/raster_based_features.bat
@@ -0,0 +1,11 @@
+:: Aim: derive example features within GRASS GIS environment --> preprocessing step for pixel-based classification
+
+:: set global variables
+set filepath=D:\Geobia_2018\Results_12ofApril\
+set filename=tile_207500_598000_1_1.las_clean.txt_PC1__PC2__PC3
+
+set n=598499.94 
+set s=598000 
+set e=207999.98 
+set w=207649.25
+
