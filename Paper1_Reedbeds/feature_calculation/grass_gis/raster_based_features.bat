2b05dd5172f8fb846b6ba8b9558e63c7bc615a03
Zsofia Koma <komazsofi@gmail.com>
2018-06-06 16:04:51 +0200

    raster based features in grass

--- Paper1_Reedbeds/feature_calculation/grass_gis/raster_based_features.bat
+++ Paper1_Reedbeds/feature_calculation/grass_gis/raster_based_features.bat
@@ -1,11 +1,26 @@
 :: Aim: derive example features within GRASS GIS environment --> preprocessing step for pixel-based classification
 
 :: set global variables
-set filepath=D:\Geobia_2018\Results_12ofApril\
-set filename=tile_207500_598000_1_1.las_clean.txt_PC1__PC2__PC3
+set filepath=D:\GitHub\eEcoLiDAR\myPhD_escience_analysis\test_data\
+set filename=testdata
 
-set n=598499.94 
-set s=598000 
-set e=207999.98 
-set w=207649.25
+set n=600000
+set s=597000
+set e=220000
+set w=206000
 
+:: set region
+g.region n=%n% s=%s% e=%e% w=%w%
+
+:: calculate possible features within r.in.lidar fuction
+r.in.lidar --overwrite input=%filepath%%filename%.las method=max resolution=1 output=%filename%_max.tif
+r.out.gdal --overwrite input=%filename%_max.tif output=%filepath%%filename%_max.tif
+
+r.in.lidar --overwrite input=%filepath%%filename%.las method=mean resolution=1 output=%filename%_mean.tif
+r.out.gdal --overwrite input=%filename%_mean.tif output=%filepath%%filename%_mean.tif
+
+r.in.lidar --overwrite input=%filepath%%filename%.las method=stddev resolution=1 output=%filename%_stddev.tif
+r.out.gdal --overwrite input=%filename%_stddev.tif output=%filepath%%filename%_stddev.tif
+
+r.in.lidar --overwrite input=%filepath%%filename%.las method=skewness resolution=1 output=%filename%_skewness.tif
+r.out.gdal --overwrite input=%filename%_skewness.tif output=%filepath%%filename%_skewness.tif
