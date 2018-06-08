4956dcc7840d8a27ef373617d48a43308e4b4dde
Zsofia Koma <komazsofi@gmail.com>
2018-06-08 12:11:44 +0200

    LiDR with RF

--- R_process/testRandomForest.R
+++ R_process/testRandomForest.R
@@ -25,6 +25,7 @@ library(lidR)
 library(rgdal)
 library(sp)
 library(spatialEco)
+library(randomForest)
 
 # Set global variables
 setwd("D:/GitHub/eEcoLiDAR/myPhD_escience_analysis/test_data") # working directory
@@ -33,9 +34,10 @@ setwd("D:/GitHub/eEcoLiDAR/myPhD_escience_analysis/test_data") # working directo
 las = readLAS("lauwermeer_example.las")
 
 classes = rgdal::readOGR("poly_forclasstest.shp")
+#classes@data
 
 # Calculate features
-metrics=las %>% grid_metrics(.stdmetrics)
+metrics=las %>% grid_metrics(.stdmetrics,res=1)
 #nanvalues=sapply(metrics, function(x) all(is.nan(x)))
 #metrics_filt=metrics[,!nanvalues]
 
@@ -46,11 +48,39 @@ skew_z=as.raster(metrics[,c(1:2,9)])
 entr_z=as.raster(metrics[,c(1:2,10)])
 
 lidar_metrics = addLayer(max_z, std_z, skew_z, entr_z)
+
 plot(lidar_metrics)
+
+plot(entr_z)
+plot(classes, add = TRUE)
 
 #writeRaster(max_z, filename="max_z.tif", format="GTiff")
 
-# Select training areas
+# select training areas
 
+classes_rast <- rasterize(classes, max_z,field="id")
+plot(classes_rast)
 
+masked <- mask(lidar_metrics, classes_rast)
+plot(masked)
 
+# convert training data into the right format
+trainingbrick <- addLayer(masked, classes_rast)
+featuretable <- getValues(trainingbrick)
+featuretable <- na.omit(featuretable)
+featuretable <- as.data.frame(featuretable)
+
+# apply RF
+modelRF <- randomForest(x=featuretable[ ,c(1:4)], y=featuretable$layer,importance = TRUE)
+class(modelRF)
+modelRF$confusion
+varImpPlot(modelRF)
+
+# predict
+predLC <- predict(lidar_metrics, model=modelRF, na.rm=TRUE)
+
+names(featuretable)
+names(lidar_metrics)
+
+plot(predLC)
+print(predLC)
