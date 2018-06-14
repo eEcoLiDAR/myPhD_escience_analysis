d0b21e30946c32a2d296c4b250b549d87754c9fa
Zsofia Koma <komazsofi@gmail.com>
2018-06-14 08:38:51 +0200

    add class colors to importace plot

--- analysis/visualization/geobia_Fig5.py
+++ analysis/visualization/geobia_Fig5.py
@@ -17,11 +17,14 @@ importance=np.array([0.06870836,0.05689468,0.05652563,0.05641782,0.0539987,0.049
 features=np.array(['std(mean z)','mean(pulse pen.)','mean(mean z)','mean(echo rat.)','std(echo rat.)','std(max z)','std(range z)','mean(planarity)','std(pulse pen.)','mean(skew. z)',
 'std(std z)','mean(std z)','mean(range z)','mean(max z)','std(sphericity)','mean(sigma z)','std(sigma z)','std(curvature)','polygon area','mean(var. z)','mean(kurtosis z)','mean(curvature)','std(skew. z)','std(med. z)',
 'mean(sphericity)','std(kurtosis z)','std(planarity)','mean(med. z)']) 
+
+colors=['darkred','green','darkred','green','green','darkred','darkred','rebeccapurple','green','rebeccapurple','rebeccapurple','rebeccapurple','darkred','darkred','rebeccapurple','rebeccapurple','rebeccapurple','green',
+'rebeccapurple','rebeccapurple','rebeccapurple','rebeccapurple','darkred','rebeccapurple','rebeccapurple','rebeccapurple','darkred']
  
 plt.figure()
 plt.title("Feature importances")
 plt.bar(range(27), importance,
-       color="r", align="center")
+       color=colors, align="center")
 plt.xticks(range(27),features,rotation=45,horizontalalignment='right')
 plt.xlim([-1, 27])
 plt.tight_layout()
