6e2a6ae1af0af9c0c845b8c32285ebeb48d75608
Zsofia Koma <komazsofi@gmail.com>
2018-06-26 15:33:07 +0200

    add Fig plot

---
+++ R_process/Tristanetal_Fig1.R
@@ -0,0 +1,22 @@
+"
+@author: Zsofia Koma, UvA
+Aim: Create Fig.1. in Bakx et al., 2018 
+-- a.) data structure representation of features (area, voxel, object)
+-- b.) schematic overview of the integration of LiDAR into SDM workflow (based on Guisan et al.,2017 book page 43)
+
+Input: 
+Output: 
+
+Function:
+
+Example usage (from command line):   
+
+ToDo: 
+
+Question:
+
+"
+
+# B.) 
+
+
