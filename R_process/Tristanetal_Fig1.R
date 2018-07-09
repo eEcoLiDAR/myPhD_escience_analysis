5d88faeee6ac8e3eef5663ad21c403a25cc0e53d
Zsofia Koma <komazsofi@gmail.com>
2018-07-09 16:07:16 +0200

    add ggplot2 for fancier plotting

--- R_process/Tristanetal_Fig1.R
+++ R_process/Tristanetal_Fig1.R
@@ -156,3 +156,15 @@ plot(map,col = heat.colors(10))
 # Create response curves
 
 rp=response.plot2(model=c('glm_model'),Data=pts_dataframe[,c('V1.1','V1.2','V1.3')],show.variables=c('V1.1','V1.2','V1.3'),fixed.var.metric="mean",plot=TRUE,use.formal.names=TRUE)
+
+# Simulate response curves
+
+x <- seq(0,30,1)
+y <- 1/(1+exp(-.5*(x-15)))
+
+plot(x, y, type = "l", frame = FALSE, pch = 19, col = "red", xlab = "x", ylab = "y")
+
+response_df <- data.frame(data=seq(0,30,1),probability=1/(1+exp(-.5*(x-15))))
+
+ggplot(data=response_df , aes(x=data, y=probability)) + geom_line(color="blue", size=1.2) + scale_color_brewer(palette="Paired") + theme_minimal() + theme(axis.text=element_text(size=15,face="bold"),
+                                                                                                                                              axis.title=element_text(size=15,face="bold"))
