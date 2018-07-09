3370b2a3b4791d84505a2f7a72fdf5ec97402c98
Zsofia Koma <komazsofi@gmail.com>
2018-07-09 11:30:31 +0200

    add raster export

--- R_process/Tristanetal_Fig1.R
+++ R_process/Tristanetal_Fig1.R
@@ -78,6 +78,9 @@ plot(hmax)
 hmax_r <- rasterFromXYZ(hmax)
 plot3D(hmax_r)
 
+projection(hmax_r) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs"
+writeRaster(hmax_r, filename="hmax_test.tif", format="GTiff", overwrite=TRUE)
+
 ####### Voxel #######
 
 hmax_v = grid_metrics3d(las, max(Z),res=1)
@@ -113,3 +116,19 @@ plot(las, color = "treeID", colorPalette = col,bg="white",size=3)
 # B.) schematic overview of the integration of LiDAR into SDM workflow (based on Guisan et al.,2017 book page 43)#
 ##################################################################################################################
 
+# Create presence only data
+
+# Create layers
+
+hmax = grid_metrics(las, max(Z),res=1)
+plot(hmax)
+
+hsd = grid_metrics(las, sd(Z),res=1)
+plot(hsd)
+
+hmean = grid_metrics(las, mean(Z),res=1)
+plot(hmean)
+
+# Create response curves
+
+# Create prediction
