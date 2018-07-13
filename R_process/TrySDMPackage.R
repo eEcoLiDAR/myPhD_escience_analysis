283c38938af4ebe2b8a28079ffe37309d286d568
Zsofia Koma <komazsofi@gmail.com>
2018-07-13 21:13:59 +0200

    start to test sdm

---
+++ R_process/TrySDMPackage.R
@@ -0,0 +1,20 @@
+"
+@author: Zsofia Koma, UvA
+Aim: Test sdm package
+
+Input: 
+Output: 
+
+Function:
+
+Example usage (from command line):   
+
+ToDo: 
+
+Question:
+
+"
+
+# Import required libraries
+library("sdm")
+
