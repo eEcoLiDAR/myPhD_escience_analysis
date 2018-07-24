0adadfbcd923c53d8eb4aa1000c7789212551f3e
komazsofi <komazsofi@gmail.com>
2018-07-24 16:19:13 +0200

    small changes

--- lastools_process/Paper1_preprocess.bat
+++ lastools_process/Paper1_preprocess.bat
@@ -1,9 +1,9 @@
 : Preprocessing of ahn2 point clouds 
 : set the command line into the directory which contains the only-ground and object point cloud (laz files downloaded from ahn2 viewer)
 
-::C:\LAStools\bin\las2las -i g02gz2.laz -set_classification 2 -olaz
-::C:\LAStools\bin\lasmerge -i g02gz2_1.laz u02gz2.laz -o 02gz2.laz
+C:\LAStools\bin\las2las -i g02gz2.laz -set_classification 2 -olaz
+C:\LAStools\bin\lasmerge -i g02gz2_1.laz u02gz2.laz -o 02gz2.laz
 
-::C:\LAStools\bin\lasheight -i *.las -olaz -replace_z
+:: tiling with lidr
 
-C:\LAStools\bin\las2dem -i *.laz -otif -keep_class 2 -step 2.5
+C:\LAStools\bin\lasheight -i *.las -olaz -replace_z
