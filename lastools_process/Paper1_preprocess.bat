1a22f3a61f2920a14888c563325af8d86eed266c
komazsofi <komazsofi@gmail.com>
2018-07-24 09:17:48 +0200

    normalizing is failing

--- lastools_process/Paper1_preprocess.bat
+++ lastools_process/Paper1_preprocess.bat
@@ -1,9 +1,9 @@
 : Preprocessing of ahn2 point clouds 
 : set the command line into the directory which contains the only-ground and object point cloud (laz files downloaded from ahn2 viewer)
 
-::C:\LAStools\bin\las2las -i g02gz2.laz -set_classification 2 -olaz
-::C:\LAStools\bin\lasmerge -i g02gz2.laz u02gz2.laz -o 02gz2.laz
+C:\LAStools\bin\las2las -i g02gz2.laz -set_classification 2 -olaz
+C:\LAStools\bin\lasmerge -i g02gz2_1.laz u02gz2.laz -o 02gz2.laz
 
 ::C:\LAStools\bin\lasheight -i 02gz2.laz -olaz -replace_z
 
-C:\LAStools\bin\las2dem -i 02gz2_1.laz -keep_class 2 -o 02gz2.tiff -step 2.5
+::C:\LAStools\bin\las2dem -i 02gz2_1.laz -o 02gz2.tiff -step 2.5
