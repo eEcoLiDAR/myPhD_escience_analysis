af65f8eb21b7f3a54be979d095008d50b1987b4d
komazsofi <komazsofi@gmail.com>
2018-07-25 14:20:38 +0200

    remove DTM and normalization part

--- R_process/onlytiling.R
+++ R_process/onlytiling.R
@@ -1,17 +1,12 @@
 # Import required libraries
 library("lidR")
-library("rlas")
-library("raster")
-
-# set working directory up
-setwd("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/")
 
 ##########################
 # Label ground points    #
 ##########################
 
-las_ground = readLAS("g02gz2.laz",filter="-set_classification 2")
-writeLAS(las_ground, "g02gz2_ground.laz")
+las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2.laz",filter="-set_classification 2")
+writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/02gz2_lidr/g02gz2_ground.laz")
 
 ##########################
 # Create catalog         #
@@ -23,28 +18,4 @@ cores(ctg) <- 15L
 tiling_size(ctg) <- 1000
 buffer(ctg) <- 5
 
-ctg = catalog_retile(ctg, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/02gz2_lidr/tiled/", "tile_") 
-
-##########################
-# Normalize              #
-##########################
-
-
-
-##########################
-# DTM, DSM, nDSM         #
-##########################
-
-createDTM <- function(las) 
-{
-  las_ground = lasfilter(las, Classification == 2)
-  dtm = grid_terrain(las_ground, method = "knnidw", k = 10L)
-  return(dtm)
-}
-
-dtm_tiles <- catalog_apply(ctg, createDTM)
-dtm <- data.table::rbindlist(dtm_tiles)
-
-dtm_r <- rasterFromXYZ(dtm)
-plot(dtm_r,colorPalette = terrain.colors(100))
-writeRaster(dtm_r, "dtm.tif")
+ctg = catalog_retile(ctg, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/02gz2_lidr/tiled/", "tile_",ext="laz") 
