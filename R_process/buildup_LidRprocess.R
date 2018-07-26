f94d3f1cdc5a77e58a78f3d6890ea9e8260e348d
komazsofi <komazsofi@gmail.com>
2018-07-26 15:52:30 +0200

    build up pre-process workflow -- test

--- R_process/buildup_LidRprocess.R
+++ R_process/buildup_LidRprocess.R
@@ -1,59 +1,70 @@
 "
 @author: Zsofia Koma, UvA
-Aim: Apply and test LidR over large areas
+Aim: test preprocessing steps
 
-Input: 
-Output: 
+input:
+output:
 
-Function:
+fuction:
+Preprocessing
+1. create DTM
+2. normalize
+3. create and export DTM, DSM
 
-Example usage:   
-
-ToDo: 
-
-Question:
 
 "
 # Import required libraries
 library("lidR")
+library("rlas")
 library("raster")
 
 # Set global variables
-setwd("D:/Paper1_ReedbedStructure/Data/ALS/ForProcess/tiled/") # working directory
+setwd("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/02gz2_lidr/tiled/") # working directory
 
 ##########################
 # Import                 #
 ##########################
 
-las = readLAS("tile_00005.las")
+writelax("tile_00011.laz")
+las = readLAS("tile_00011.laz")
 plot(las)
 
 hist(las@data$Z)
 
-dtm = raster("dtm.tif")
-dtm[is.na(dtm)] <- 0 # I fill up with 0 where DTM do not give velues back
+##########################
+# DTM                    #
+##########################
+
+start_time <- Sys.time()
+
+las_ground = lasfilter(las, Classification == 2)
+dtm = grid_terrain(las_ground, 5, method = "knnidw", k = 10L)
+
+end_time <- Sys.time()
+print(end_time - start_time)
+
 plot(dtm)
+plot3d(dtm)
+
+dtm_2 = grid_canopy(las_ground, 2.5, subcircle = 0.2)
+plot(dtm_2)
 
 ##########################
-# Preprocess - Normalize #
+# Normalize              #
 ##########################
+
+start_time <- Sys.time()
 
 lasnormalize(las, dtm)
 hist(las@data$Z)
 
-#############################
-# LiDAR metrics calculation #
-#############################
+end_time <- Sys.time()
+print(end_time - start_time)
 
-myMetrics = function(z)
-{
-  metrics = list(
-    zmean = mean(z),
-    zmax = max(z)
-  )
-  
-  return(metrics)
-}
+##########################
+# DSM                    #
+##########################
 
-metrics = grid_metrics(las, myMetrics(Z), 2.5)
-plot(metrics)
+chm = grid_canopy(las, 2.5, subcircle = 0.2)
+plot(chm)
+plot3d(chm)
