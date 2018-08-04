d7b8cc8a3e38a067811dff1f0bbe16bd6cffbcee
komazsofi <komazsofi@gmail.com>
2018-08-04 14:04:11 +0200

    small adaption 2

--- R_process/onlytiling.R
+++ R_process/onlytiling.R
@@ -6,7 +6,7 @@ library("lidR")
 ##########################
 
 las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2.laz",filter="-set_classification 2")
-writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/02gz2_lidr/g02gz2_ground.laz")
+writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2_ground.laz")
 
 ##########################
 # Create catalog         #
@@ -19,7 +19,3 @@ tiling_size(ctg) <- 1000
 buffer(ctg) <- 5
 
 ctg = catalog_retile(ctg, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/tiled/", "tile_",ext="laz") 
-
-####################################################
-# Run LAStools with Paper1preprocess.bat           #
-####################################################
