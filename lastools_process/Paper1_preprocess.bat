f9508201217b127db89263ee46a5cf8e2b1f6566
komazsofi <komazsofi@gmail.com>
2018-08-04 12:08:47 +0200

    small changes

--- lastools_process/Paper1_preprocess.bat
+++ lastools_process/Paper1_preprocess.bat
@@ -1,5 +1,5 @@
 : Preprocessing of ahn2 point clouds; create DTM, DSM, and normalized point cloud
 : the dataset was tiled with lidR beforehand and the ground points are indicated with classification 2
 
-::C:\LAStools\bin\lasheight -i *.laz -olaz -replace_z
-C:\LAStools\bin\blast2dem -i *_1.laz -o *_shd.tif -step 1 -hillshade
+C:\LAStools\bin\lasheight -i *.laz -olaz -replace_z
+
