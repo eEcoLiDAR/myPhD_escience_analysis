c146533655da7bb6ac91b38e295f51ad1dfd61a7
Zsofia Koma <komazsofi@gmail.com>
2018-08-06 15:42:49 +0200

    test penratio

--- R_process/buildup_lidRmetrics.R
+++ R_process/buildup_lidRmetrics.R
@@ -9,10 +9,8 @@ Fuctions:
 
 
 Question:
-
-
-
 "
+
 # Import required libraries
 library("lidR")
 library("rlas")
@@ -24,6 +22,26 @@ setwd(full_path) # working directory
 
 # calculate LiDAR metrics
 
+HeightMetrics = function(z)
+{
+  heightmetrics = list(
+    zmax = max(z), 
+    zmean = mean(z),
+    zmedian = median(z)
+  )
+  return(heightmetrics)
+}
+
+CoverageMetrics = function(z,classification)
+{
+  coveragemetrics = list(
+    pulsepenrat = (length(z[classification!=2])/length(z[classification==2]))
+  )
+  return(coveragemetrics)
+}
+
+#executation
+
 file.names <- dir(full_path, pattern =".laz")
 for(i in 1:length(file.names)){
   
@@ -33,6 +51,12 @@ for(i in 1:length(file.names)){
   writelax(paste(full_path,file.names[i],sep=""))
   
   las = readLAS(file.names[i])
+  
+  #heightmetrics = grid_metrics(las, HeightMetrics(Z),res=1)
+  #plot(heightmetrics)
+  
+  coveragemetrics = grid_metrics(las, CoverageMetrics(Z,Classification),res=1)
+  plot(coveragemetrics)
   
   end_time <- Sys.time()
   print(end_time - start_time)
