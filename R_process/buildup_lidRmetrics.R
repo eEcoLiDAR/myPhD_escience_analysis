42132c8d8bed5c40ae38cee2aba6d94a2ae58519
Zsofia Koma <komazsofi@gmail.com>
2018-08-06 17:14:40 +0200

    start to add vert distr

--- R_process/buildup_lidRmetrics.R
+++ R_process/buildup_lidRmetrics.R
@@ -35,9 +35,20 @@ HeightMetrics = function(z)
 CoverageMetrics = function(z,classification)
 {
   coveragemetrics = list(
-    pulsepenrat = (length(z[classification!=2])/length(z[classification==2]))
+    nofveg=length(z[classification!=2]),
+    nofpoints=length(z),
+    pulsepenrat = length(z[classification!=2])/length(z)
   )
   return(coveragemetrics)
+}
+
+VegStr_VertDistr_Metrics = function(z)
+{
+  vertdistr_metrics = list(
+    zstd = sd(z),
+    zvar = var(z)
+  )
+  return(vertdistr_metrics)
 }
 
 #executation
@@ -55,10 +66,12 @@ for(i in 1:length(file.names)){
   #heightmetrics = grid_metrics(las, HeightMetrics(Z),res=1)
   #plot(heightmetrics)
   
-  coveragemetrics = grid_metrics(las, CoverageMetrics(Z,Classification),res=1)
-  plot(coveragemetrics)
+  #coveragemetrics = grid_metrics(las, CoverageMetrics(Z,Classification),res=1)
+  #plot(coveragemetrics)
+  
+  vertdistr_metrics = grid_metrics(las, VegStr_VertDistr_Metrics(Z),res=1)
+  plot(vertdistr_metrics)  
   
   end_time <- Sys.time()
   print(end_time - start_time)
-  
 }
