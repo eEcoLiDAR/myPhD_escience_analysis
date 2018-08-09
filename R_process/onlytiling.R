ad4aa0e5f4a774bce4890d2c9297ea8b8b2598ad
komazsofi <komazsofi@gmail.com>
2018-08-09 20:10:42 +0200

    retile entire study area

--- R_process/onlytiling.R
+++ R_process/onlytiling.R
@@ -5,17 +5,25 @@ library("lidR")
 # Label ground points    #
 ##########################
 
-las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2.laz",filter="-set_classification 2")
-writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2_ground.laz")
+#las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2.laz",filter="-set_classification 2")
+#writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/g02gz2_ground.laz")
+
+#las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/02hz1/g02hz1.laz",filter="-set_classification 2")
+#writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/02hz1/g02hz1_ground.laz")
+
+#las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/06en2/g06en2.laz",filter="-set_classification 2")
+#writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/06en2/g06en2_ground.laz")
+
+#las_ground = readLAS("D:/Koma/Paper1_ReedStructure/Data/ALS/06fn1/g06fn1.laz",filter="-set_classification 2")
+#writeLAS(las_ground, "D:/Koma/Paper1_ReedStructure/Data/ALS/06fn1/g06fn1_ground.laz")
 
 ##########################
 # Create catalog         #
 ##########################
 
-ctg = catalog("D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/classified/")
+ctg = catalog("D:/Koma/Paper1_ReedStructure/Data/ALS/WholeLau/")
 
-cores(ctg) <- 4L
+cores(ctg) <- 15L
 tiling_size(ctg) <- 1000
-buffer(ctg) <- 5
 
-ctg = catalog_retile(ctg, "D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/tiled/", "tile_",ext="laz") 
+ctg = catalog_retile(ctg, "D:/Koma/Paper1_ReedStructure/Data/ALS/WholeLau/tiled/", "tile_",ext="laz") 
