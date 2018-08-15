ecefd3bf039afbf7f5b427d25b6a2a38694174c8
Zsofia Koma <komazsofi@gmail.com>
2018-08-15 14:56:22 +0200

    add exploration analysis

---
+++ R_process/Explore.R
@@ -0,0 +1,45 @@
+library(raster)
+library(sp)
+library(spatialEco)
+library(randomForest)
+library(caret)
+
+library(ggplot2)
+
+# Set global variables
+setwd("D:/Koma/Paper1_ReedStructure/WholeLau_TotalVegetation") # working directory
+
+# Import
+classes = rgdal::readOGR("D:/Koma/Paper1_ReedStructure/selectedclasses_polygon.shp")
+plot(classes)
+
+#ani=raster("mosaic_anisotropy.tif")
+#curva=raster("mosaic_curvature.tif")
+#omni=raster("mosaic_omnivariance.tif")
+heightkurto=raster("mosaic_heightkurto.tif")
+height_max=raster("mosaic_heightmax.tif")
+height_med=raster("mosaic_heightmedian.tif")
+height_std=raster("mosaic_heightstd.tif")
+height_dtm=raster("mosaic_meandtm.tif")
+
+
+# Preprocess import data (rasterizing, masking)
+classes_rast <- rasterize(classes, height_max,field="classes")
+plot(classes_rast)
+
+lidar_metrics = addLayer(heightkurto,height_max,height_med,height_std)
+
+a_ofint = extent(206000,212000,596000,598000)
+lidar_metrics_c=crop(lidar_metrics, a_ofint)
+classes_rast_c=crop(classes_rast, a_ofint)
+
+masked <- mask(lidar_metrics_c, classes_rast_c)
+
+trainingbrick <- addLayer(masked, classes_rast_c)
+featuretable <- getValues(trainingbrick)
+featuretable <- na.omit(featuretable)
+featuretable <- as.data.frame(featuretable)
+
+# create exploration analysis
+
+ggplot(featuretable, aes(group=layer, x=layer,y=mosaic_heightstd)) + geom_boxplot()
