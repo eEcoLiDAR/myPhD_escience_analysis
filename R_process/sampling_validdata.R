7c0075e7e9d0042227c1cefe5948107ac7397fed
Zsofia Koma <komazsofi@gmail.com>
2018-08-16 15:53:09 +0200

    building sampling

--- R_process/sampling_validdata.R
+++ R_process/sampling_validdata.R
@@ -14,12 +14,24 @@ Example:
 library(raster)
 library(sp)
 library(spatialEco)
-
+library(ggplot2)
+library(maptools)
+library(plyr)
 
 # Set global variables
 setwd("D:/Koma/Paper1_ReedStructure") # working directory
 
 # Import
-classes = rgdal::readOGR("vlakken_union_structuur.shp")
-plot(classes)
+veg_map = rgdal::readOGR("vlakken_union_structuur.shp")
 
+# ggplot visualization
+veg_map@data$id = rownames(veg_map@data)
+veg_map.points = fortify(veg_map, region="id")
+veg_map.df = join(veg_map.points, veg_map@data, by="id")
+ggplot(veg_map.df) + aes(long,lat,group=group,fill=structyp_e) + geom_polygon() + geom_path(color="white") + coord_equal() + scale_color_brewer(palette="Set1")
+
+# Sampling
+veg_map_sample=spsample(veg_map,500,type="stratified")
+
+plot(veg_map)
+plot(veg_map_sample,add=TRUE)
