1c635ee5f04fd0d69436af08a05ddf43c46d8c3b
Zsofia Koma <komazsofi@gmail.com>
2018-08-23 10:48:47 +0200

    small changes

--- R_process/test_escience_lidarprocess.R
+++ R_process/test_escience_lidarprocess.R
@@ -18,13 +18,20 @@ setwd("D:/Koma/escience/NL_features")
 
 # Import
 
+# escience gtiff
 all_data=stack("terrainData1km_v2.tif")
 all_data=flip(all_data,direction = 'y')
 
-# Explore
+# Lauwersmeer
+
+
+
+# Explore escience
 
 par(mfrow=c(1,2))
-hist(all_data[[1]],breaks=50,col = "grey")
-plot(all_data[[1]])
+hist(all_data[[9]],breaks=50,col = "grey")
+plot(all_data[[9]])
 
-summary(all_data[[1]])
+summary(all_data[[9]])
+
+# Explore for Lauwersmeer
