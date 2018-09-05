ae19bb7e7ef2464f689ed73ce3122222784d386b
Zsofia Koma <komazsofi@gmail.com>
2018-09-05 10:08:45 +0200

    test another eigenvalue calc.

--- R_process/test_3dshape_metrics.R
+++ R_process/test_3dshape_metrics.R
@@ -1,28 +1,31 @@
 # Import required libraries
 library("lidR")
+library("factoextra")
 
-library("rARPACK")
-
-full_path="D:/Koma/Paper1_ReedStructure/Data/ALS/02gz2/testmetrics/"
+full_path="D:/Paper1_ReedbedStructure/Data/ALS/Test/"
 setwd(full_path) # working directory
 
-las = readLAS("tile_00003_norm.laz")
+las = readLAS("tile_00003.las")
 
 xyz = las@data[,1:3]
+
+res.pca <- prcomp(xyz,  scale = FALSE)
+get_eig(res.pca)
+
 eigenvalues = eigen(cov(xyz))$values
 print(eigenvalues)
 
 Shape_VertDistr_Metrics = function(xyz)
 {
-  covar_m = cov(xyz)
-  eigenvalues=eigs(covar_m, k=3, opts = list(retvec = FALSE))
-  eigen_list = as.list(eigenvalues$values)
+  res.pca <- prcomp(xyz,  scale = FALSE)
+  eigenvalues = get_eig(res.pca)
+  eigen_list = as.list(eigenvalues)
   return(eigen_list)
 }
 
 start_time <- Sys.time()
 
-shapemetrics = grid_metrics(las, Shape_VertDistr_Metrics(las@data[,1:3]),res=1)
+shapemetrics = grid_metrics(las, Shape_VertDistr_Metrics(las@data[,1:3]),res=5)
 
 end_time <- Sys.time()
 print(end_time - start_time)
