a285cfe1ed1064c77af7551c91c164bb7f8fca34
Zsofia Koma <komazsofi@gmail.com>
2018-09-06 16:43:58 +0200

    try to set up cpp

--- R_process/test_3dshape_metrics.R
+++ R_process/test_3dshape_metrics.R
@@ -3,42 +3,16 @@ library("lidR")
 library("RcppEigen")
 library("Rcpp")
 
-full_path="C:/zsofia/Amsterdam/Paper1/"
+# Global variable
+full_path="D:/Paper1_ReedbedStructure/Data/ALS/Test/"
 setwd(full_path) # working directory
 
-las = readLAS("g32hz1rect2.las")
 
-xyz = las@data[,1:3]
-
-sourceCpp("C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/myPhD_escience_analysis/R_process/eigen.cpp")
-
-getEigenValues(xyz)
+sourceCpp("D:\\GitHub\\eEcoLiDAR\\myPhD_escience_analysis\\R_process\\eigen.cpp")
 
 set.seed(42)
 X <- matrix(rnorm(4*4), 4, 4)
-Z <- xyz %*% t(matrix(xyz))
+Z <- X %*% t(X)
 
 getEigenValues(Z)
 
-Shape_VertDistr_Metrics = function(xyz)
-{
-  res.pca <- prcomp(xyz,  scale = FALSE)
-  eigenvalues = get_eig(res.pca)
-  eigen_list = as.list(eigenvalues)
-  return(eigen_list)
-}
-
-start_time <- Sys.time()
-
-shapemetrics = grid_metrics(las, Shape_VertDistr_Metrics(las@data[,1:3]),res=5)
-
-end_time <- Sys.time()
-print(end_time - start_time)
-
-plot(shapemetrics)
-
-xyz = las@data[,1:3]
-covar_m = cov(xyz)
-eigenvalues=eigs(covar_m, k=3, opts = list(retvec = FALSE))
-
-print(eigenvalues)
