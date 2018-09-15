297e4aac3cf004bea88d18d121d609164d76d531
Zsofia Koma <komazsofi@gmail.com>
2018-09-15 21:01:33 +0200

    eigenvalue calc with c++ and R - not th same result

--- R_process/test_3dshape_metrics.R
+++ R_process/test_3dshape_metrics.R
@@ -4,15 +4,25 @@ library("RcppEigen")
 library("Rcpp")
 
 # Global variable
-full_path="D:/Paper1_ReedbedStructure/Data/ALS/Test/"
+#full_path="D:/Paper1_ReedbedStructure/Data/ALS/Test/"
+full_path="C:/zsofia/Amsterdam/Paper1/"
+
 setwd(full_path) # working directory
 
+#sourceCpp("D:\\GitHub\\eEcoLiDAR\\myPhD_escience_analysis\\R_process\\eigen.cpp")
+sourceCpp("C:/zsofia/Amsterdam/GitHub/eEcoLiDAR/myPhD_escience_analysis/R_process/eigen.cpp")
 
-sourceCpp("D:\\GitHub\\eEcoLiDAR\\myPhD_escience_analysis\\R_process\\eigen.cpp")
+#set.seed(42)
+#X <- matrix(rnorm(4*4), 4, 4)
+#Z <- X %*% t(X)
 
-set.seed(42)
-X <- matrix(rnorm(4*4), 4, 4)
-Z <- X %*% t(X)
+las = readLAS("g32hz1rect2.las")
+len = length(las@data$X)
 
-getEigenValues(Z)
+xyz=as.matrix(las@data[2:4,1:3])
+xyz_t <- xyz %*% t(xyz)
+
+getEigenValues(xyz_t)
+
+eigen(xyz_t)
 
