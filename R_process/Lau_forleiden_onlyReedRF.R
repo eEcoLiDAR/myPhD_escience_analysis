8bce19e4a60395e8a9eab22b98ac9dcd17eac887
komazsofi <komazsofi@gmail.com>
2018-09-30 13:34:19 +0200

    apply classification for only reed class

---
+++ R_process/Lau_forleiden_onlyReedRF.R
@@ -0,0 +1,95 @@
+library(raster)
+library(sp)
+library(spatialEco)
+library(randomForest)
+library(caret)
+
+# Set global variables
+setwd("D:/Koma/Paper1_ReedStructure/Lau_Island/Data") # working directory
+
+# Import
+classes = rgdal::readOGR("training_buffer3.shp")
+#plot(classes)
+
+# Classified
+RF_class=raster("classified3.tif")
+
+formask <- setValues(raster(RF_class), NA)
+formask[RF_class==4] <- 1
+plot(formask, col="dark green", legend = FALSE)
+
+# LiDAR metrics
+eiglargest=raster("Lau_island_eigenlargest.tif")
+eigmedium=raster("Lau_island_eigenmedium.tif")
+eigsmallest=raster("Lau_island_eigensmallest.tif")
+ani=raster("Lau_island_anisotrophy.tif")
+curva=raster("Lau_island_curvature.tif")
+lin=raster("Lau_island_linearity.tif")
+plan=raster("Lau_island_planarity.tif")
+sph=raster("Lau_island_sphericity.tif")
+
+height_max=raster("Lau_island_heightq090.tif")
+height_025=raster("Lau_island_heightq025.tif")
+height_075=raster("Lau_island_heightq075.tif")
+height_med=raster("Lau_island_heightmedian.tif")
+height_mean=raster("Lau_island_heightmean.tif")
+
+heightcover=raster("Lau_island_cover_pulsepenrat.tif")
+
+heightskew=raster("Lau_island_vertdistr_heightskew.tif")
+height_std=raster("Lau_island_vertdistr_heightstd.tif")
+height_kurto=raster("Lau_island_vertdistr_heightkurto.tif")
+height_var=raster("Lau_island_vertdistr_heightvar.tif")
+
+height_dtm=raster("Lau_island_terrainmean.tif")
+height_dtmvar=raster("Lau_island_terrainvar.tif")
+
+# Preprocess import data (rasterizing, masking)
+classes_rast <- rasterize(classes, formask,field="Structure")
+#plot(classes_rast)
+
+lidar_metrics = addLayer(height_max,height_025,height_075,height_med,height_mean,heightcover,heightskew,height_std,height_kurto,height_var,
+                         eiglargest,eigmedium,eigsmallest,ani,curva,lin,plan,sph,height_dtm,height_dtmvar)
+
+masked <- mask(lidar_metrics, classes_rast)
+lidar_metrics_masked <- mask(lidar_metrics,formask)
+
+# convert training data into the right format
+trainingbrick <- addLayer(masked, classes_rast)
+featuretable <- getValues(trainingbrick)
+featuretable <- na.omit(featuretable)
+featuretable <- as.data.frame(featuretable)
+
+# apply RF
+modelRF <- randomForest(x=featuretable[ ,c(1:20)], y=factor(featuretable$layer),importance = TRUE)
+class(modelRF)
+varImpPlot(modelRF)
+
+# accuracy assessment
+first_seed <- 5
+accuracies <-c()
+for (i in 1:1){
+  set.seed(first_seed)
+  first_seed <- first_seed+1
+  trainIndex <- createDataPartition(y=featuretable$layer, p=0.75, list=FALSE)
+  trainingSet<- featuretable[trainIndex,]
+  testingSet<- featuretable[-trainIndex,]
+  modelFit <- randomForest(factor(layer)~.,data=trainingSet)
+  prediction <- predict(modelFit,testingSet[ ,c(1:20)])
+  testingSet$rightPred <- prediction == testingSet$layer
+  t<-table(prediction, testingSet$layer)
+  print(t)
+  accuracy <- sum(testingSet$rightPred)/nrow(testingSet)
+  accuracies <- c(accuracies,accuracy)
+  print(accuracy)
+}
+
+# predict
+predLC <- predict(lidar_metrics_masked, model=modelRF, na.rm=TRUE)
+
+names(featuretable)
+names(lidar_metrics)
+
+plot(predLC)
+
+writeRaster(predLC, filename="classified_reed.tif", format="GTiff",overwrite=TRUE)
