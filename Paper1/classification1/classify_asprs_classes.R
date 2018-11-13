02b2bc3fa542c023132085c4071b292dfc0045c8
Zsofia Koma <komazsofi@gmail.com>
2018-11-13 12:31:35 +0100

    parallel computation of metrics with catalog

--- Paper1/classification1/classify_asprs_classes.R
+++ Paper1/classification1/classify_asprs_classes.R
@@ -38,3 +38,36 @@ hrange <- data.table::rbindlist(hrange_fea)
 
 hrange_r <- rasterFromXYZ(hrange)
 writeRaster(hrange_r, paste(substr(lidarfile, 1, nchar(lidarfile)-4) ,"_hrange.tif",sep=""))
+
+# Calculate multiple metrics with parallelization
+HeightMetrics = function(z)
+{
+  heightmetrics = list(
+    zmax = max(z), 
+    zmean = mean(z),
+    zmedian = median(z),
+    z025quantile = quantile(z, 0.25),
+    z075quantile = quantile(z, 0.75),
+    z090quantile = quantile(z, 0.90)
+  )
+  return(heightmetrics)
+}
+
+create_hmetrics <- function(las) 
+{
+  hmetrics = grid_metrics(las, HeightMetrics(Z), res=2.5)
+  return(hmetrics)
+}
+
+height_fea <- catalog_apply(nonground_ctg, create_hmetrics)
+height_fea_db <- data.table::rbindlist(height_fea)
+
+hmax_r <- rasterFromXYZ(height_fea_db[,c(1,2,3)])
+hmean_r <- rasterFromXYZ(height_fea_db[,c(1,2,4)])
+hmedian_r <- rasterFromXYZ(height_fea_db[,c(1,2,5)])
+h025quantile <- rasterFromXYZ(height_fea_db[,c(1,2,6)])
+h075quantile <- rasterFromXYZ(height_fea_db[,c(1,2,7)])
+h090quantile <- rasterFromXYZ(height_fea_db[,c(1,2,8)])
+
+lidarmetrics_raster= stack(hmax_r, hmean_r, hmedian_r, h025quantile, h075quantile, h090quantile) 
+plot(lidarmetrics_raster)
