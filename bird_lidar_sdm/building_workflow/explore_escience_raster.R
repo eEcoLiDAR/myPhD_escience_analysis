a1438e5993e30d93d372bc9564644ecf53c5b68b
Zsofia Koma <komazsofi@gmail.com>
2018-11-15 12:00:11 +0100

    add landcovers and make boxplots

--- bird_lidar_sdm/building_workflow/explore_escience_raster.R
+++ bird_lidar_sdm/building_workflow/explore_escience_raster.R
@@ -76,6 +76,7 @@ filtered_lidarmetrics=lidarmetrics_masked_df[myvars]
 
 filtered_lidarmetrics_r=rasterFromXYZ(filtered_lidarmetrics, digits = 3)
 plot(filtered_lidarmetrics_r)
+crs(filtered_lidarmetrics_r) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
 
 writeRaster(filtered_lidarmetrics_r, paste(substr(filename, 1, nchar(filename)-4) ,"_filtered.tif",sep=""),overwrite=TRUE)
 
