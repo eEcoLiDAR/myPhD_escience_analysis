68d846eba4c2cb353bf0773213f38fb5aea07d4b
Zsofia Koma <komazsofi@gmail.com>
2018-11-16 15:02:53 +0100

    add export

--- bird_lidar_sdm/building_workflow/detect_outliers_escienceraster.R
+++ bird_lidar_sdm/building_workflow/detect_outliers_escienceraster.R
@@ -23,15 +23,11 @@ setwd(full_path)
 
 # Import data
 lidar_data=readRDS(file = filename)
+print(summary(lidar_data))
 
-# Boxplot
-sel_attr=5
 colnames=names(lidar_data)
 
-par(mfrow=c(1,2))
-boxplot(lidar_data[,sel_attr],ylab=colnames[sel_attr],col="darkgreen")
-boxplot(lidar_data[,sel_attr],outline=FALSE, ylab=colnames[sel_attr],col="darkgreen")
-title(paste(colnames[sel_attr],"with and without outliers",sep=" "), outer = TRUE,line = -2.5)
+# Boxplot
 
 for (i in 3:16){
   print(colnames[i])
@@ -47,15 +43,8 @@ for (i in 3:16){
 }
 
 # Boxplot per landcover classes
-sel_attr=5
-colnames=names(lidar_data)
 
-par(mfrow=c(1,2))
-boxplot(lidar_data[[colnames[sel_attr]]]~landcover_class,data=lidar_data,ylab=colnames[sel_attr],col=c("gold","darkgreen","darkblue"))
-legend(x = "top",inset = c(0,0), title="Land cover classes",
-       c("agricult.","forest","nature"), fill=c("gold","darkgreen","darkblue"), horiz=TRUE,xpd = TRUE)
-boxplot(lidar_data[[colnames[sel_attr]]]~landcover_class,data=lidar_data,outline=FALSE, ylab=colnames[sel_attr],col=c("gold","darkgreen","darkblue"))
-title(paste(colnames[sel_attr],"with and without outliers",sep=" "), outer = TRUE,line = -2.5)
+# Just one metrics plot
 
 for (i in 3:16){
   print(colnames[i])
@@ -73,3 +62,58 @@ for (i in 3:16){
 }
 
 # Map outliers per lidar metrics using the outlier estimae from boxplot 
+# Based on max_z
+sel_attr="max_z"
+min=0
+max=35
+
+print(summary(lidar_data[,sel_attr]))
+
+lidar_oulier_above=lidar_data[ which(lidar_data[,sel_attr]>max),]
+ggplot() + geom_raster(data=lidar_oulier_above,aes(x,y,fill=lidar_oulier_above[,sel_attr])) + coord_equal() + scale_fill_gradientn(colours=topo.colors(7),na.value = "transparent",limits=c(min((lidar_oulier_below[,sel_attr])),max(lidar_oulier_above[,sel_attr])))
+
+lidar_oulier_below=lidar_data[ which(lidar_data[,sel_attr]<min),]
+ggplot() + geom_raster(data=lidar_oulier_below,aes(x,y,fill=lidar_oulier_below[,sel_attr])) + coord_equal() + scale_fill_gradientn(colours=topo.colors(7),na.value = "transparent",limits=c(min((lidar_oulier_below[,sel_attr])),max(lidar_oulier_below[,sel_attr])))
+
+# Export
+lidar_outlier <- rbind(lidar_oulier_above, lidar_oulier_below) 
+
+outlier_hmax <- rasterFromXYZ(lidar_outlier)
+writeRaster(outlier_hmax, paste(substr(filename, 1, nchar(filename)-4) ,"_maxh_out.tif",sep=""),overwrite=TRUE)
+
+# Delete max_z related outliers
+lidar_whoutoulier=lidar_data[ which(lidar_data[,sel_attr]<max & lidar_data[,sel_attr]>min),]
+print(summary(lidar_whoutoulier))
+
+# Based on var_z -seems to be not outlier - stdz is normal already
+sel_attr2="var_z"
+max2=14
+
+lidar_oulier_above2=lidar_whoutoulier[ which(lidar_whoutoulier[,sel_attr2]>max),]
+ggplot() + geom_raster(data=lidar_oulier_above2,aes(x,y,fill=lidar_oulier_above2[,sel_attr2])) + coord_equal() + scale_fill_gradientn(colours=topo.colors(7),na.value = "transparent",limits=c(min((lidar_oulier_above2[,sel_attr2])),max(lidar_oulier_above2[,sel_attr2])))
+
+# Do boxplot without outliers
+
+for (i in 3:16){
+  print(colnames[i])
+  
+  jpeg(paste(colnames[i],'_boxplot_whoutoutliers.jpg',sep=''))
+  
+  par(mfrow=c(1,2))
+  boxplot(lidar_whoutoulier[,i],ylab=colnames[i],col="darkgreen")
+  boxplot(lidar_whoutoulier[[colnames[i]]]~landcover_class,data=lidar_whoutoulier,ylab=colnames[i],col=c("gold","darkgreen","darkblue"))
+  title(paste(colnames[i],"without outliers",sep=" "), outer = TRUE,line = -2.5)
+  
+  dev.off()
+}
+
+# Export
+
+writeWorksheetToFile(paste(substr(filename, 1, nchar(filename)-4) ,"_summarytable.xlsx",sep=""), 
+                     data = data.frame(unclass(summary(lidar_whoutoulier)), check.names = FALSE, stringsAsFactors = FALSE), 
+                     sheet = "masked_stat", 
+                     header = TRUE,
+                     clearSheets = TRUE)
+
+lidar_whoutoulier_r <- rasterFromXYZ(lidar_whoutoulier)
+writeRaster(lidar_whoutoulier_r, paste(substr(filename, 1, nchar(filename)-4) ,"_lidar_whoutoulier.tif",sep=""),overwrite=TRUE)
