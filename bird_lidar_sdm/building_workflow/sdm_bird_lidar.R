89b855c9a39ad085c48bbbcf6025873eeb63237a
Zsofia Koma <komazsofi@gmail.com>
2018-11-17 18:02:06 +0100

    add sdm modelling with sdm pcakage

---
+++ bird_lidar_sdm/building_workflow/sdm_bird_lidar.R
@@ -0,0 +1,54 @@
+"
+@author: Zsofia Koma, UvA
+Aim: explore sdm modelling for Global Ecology and Biodiversity course
+"
+
+# Import libraries
+library("sp")
+library("rgdal")
+library("raster")
+library("spatialEco")
+library("rgeos")
+library("plyr")
+library("dplyr")
+library("maptools")
+library("gridExtra")
+
+library("ggplot2")
+library("ggmap")
+library("maps")
+library("mapdata")
+
+library("sdm")
+
+# Set global variables
+full_path="C:/Users/zsofi/Google Drive/_Amsterdam/xOther/lidar_bird_dsm_workflow/birdatlas/"
+birdfile="Breeding_bird_atlas_aggregated_data_kmsquaresKleine Karekiet_grouped_presabs_lidar.rds"
+lidarfile="terrainData100m_run1_filtered_lidarmetrics_lidar_whoutoulier.tif"
+
+setwd(full_path)
+
+# Import LiDAR and bird observations
+birdand_lidar=readRDS(file = birdfile)
+lidarmetrics=stack(lidarfile)
+
+bird_obs=birdand_lidar[c("x","y","presence")]
+coordinates(bird_obs)=~x+y
+proj4string(bird_obs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# Data prep. for SDM
+data_forsdm <- sdmData(formula=presence~., train=bird_obs, predictors=lidarmetrics)
+data_forsdm
+
+# Modelling
+model1 <- sdm(presence~.,data=data_forsdm,methods=c('glm','brt','rf'))
+model1
+
+roc(model1)
+
+# Predict
+p1 <- predict(model1,newdata=lidarmetrics,filename='p3m.img')
+plot(p1)
+
+#GUI
+gui(model1)
