0f6872a09234954abd5fd07c7a6fa737128108f9
Zsofia Koma <komazsofi@gmail.com>
2018-11-17 22:02:24 +0100

    add some clustering for trying

--- bird_lidar_sdm/building_workflow/sdm_bird_lidar.R
+++ bird_lidar_sdm/building_workflow/sdm_bird_lidar.R
@@ -20,6 +20,8 @@ library("maps")
 library("mapdata")
 
 library("sdm")
+library("corrplot")
+library("Hmisc")
 
 # Set global variables
 full_path="C:/Users/zsofi/Google Drive/_Amsterdam/xOther/lidar_bird_dsm_workflow/birdatlas/"
@@ -32,11 +34,45 @@ setwd(full_path)
 birdand_lidar=readRDS(file = birdfile)
 lidarmetrics=stack(lidarfile)
 
+# Explore variables
+# Analyse bird-lidar
+corr_m <- rcorr(as.matrix(birdand_lidar[,7:20]))
+
+corrplot(corr_m$r, type="upper", order="hclust", 
+         p.mat = corr_m$P, sig.level = 0.01, insig = "blank")
+
+hclust_avg <- hclust(dist(birdand_lidar[,7:20], method = 'euclidean'), method = 'average')
+plot(hclust_avg)
+
+cut_avg <- cutree(hclust_avg, k = 2)
+birdand_lidar_clust <- mutate(birdand_lidar, cluster = cut_avg)
+
+filteredbird_lidarmetrics_r=rasterFromXYZ(birdand_lidar_clust[,c(2,3,21)])
+writeRaster(filteredbird_lidarmetrics_r, paste(substr(birdfile, 1, nchar(birdfile)-4) ,"_clustered.tif",sep=""),overwrite=TRUE)
+
+#Cluster lidar
+lidarmetrics_pc = rasterToPoints(lidarmetrics)
+lidarmetrics_df = data.frame(lidarmetrics_pc)
+
+hclust_avg <- hclust(dist(lidarmetrics_df[50000:60000,c(3,5,13)], method = 'euclidean'), method = 'average')
+plot(hclust_avg)
+
+cut_avg <- cutree(hclust_avg, k = 2)
+lidarmetrics_clust <- mutate(lidarmetrics_df[50000:60000,c(1,2)], cluster = cut_avg)
+
+lidarmetrics_r=rasterFromXYZ(lidarmetrics_clust[,c(1,2,3)])
+writeRaster(lidarmetrics_r, paste(substr(lidarfile, 1, nchar(lidarfile)-4) ,"_clustered.tif",sep=""),overwrite=TRUE)
+
+
+# Correlation
+
+
+# Data prep. for SDM
+
 bird_obs=birdand_lidar[c("x","y","presence")]
 coordinates(bird_obs)=~x+y
 proj4string(bird_obs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-# Data prep. for SDM
 data_forsdm <- sdmData(formula=presence~., train=bird_obs, predictors=lidarmetrics)
 data_forsdm
 
