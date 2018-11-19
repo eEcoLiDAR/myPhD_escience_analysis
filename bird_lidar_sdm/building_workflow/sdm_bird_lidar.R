b4f1e8ce0a2e4bb7f2cafb81eca2ef75ccd1288b
Zsofia Koma <komazsofi@gmail.com>
2018-11-19 16:24:24 +0100

    Babak help

--- bird_lidar_sdm/building_workflow/sdm_bird_lidar.R
+++ bird_lidar_sdm/building_workflow/sdm_bird_lidar.R
@@ -22,6 +22,7 @@ library("mapdata")
 library("sdm")
 library("corrplot")
 library("Hmisc")
+library(usdm)
 
 # Set global variables
 full_path="D:/Koma/lidar_bird_dsm_workflow/birdatlas/"
@@ -35,6 +36,14 @@ setwd(full_path)
 birdand_lidar=readRDS(file = birdfile)
 lidarmetrics=stack(lidarfile)
 
+v <- vifstep(lidarmetrics)
+v
+lidarmetrics2 <- exclude(lidarmetrics,v)
+
+v <- vifcor(extract(lidarmetrics,bird_obs),th=0.9)
+
+
+
 # Correlations in bird-lidar dataset
 corr_m <- rcorr(as.matrix(birdand_lidar[,7:20]))
 
@@ -47,18 +56,23 @@ bird_obs=birdand_lidar[c("x","y","presence")]
 coordinates(bird_obs)=~x+y
 proj4string(bird_obs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-data_forsdm <- sdmData(formula=presence~., train=bird_obs, predictors=lidarmetrics)
+data_forsdm <- sdmData(formula=presence~., train=bird_obs, predictors=lidarmetrics2)
 data_forsdm
-
+#getmethodNames()
 # Modelling
-model1 <- sdm(presence~.,data=data_forsdm,methods=c('glm','brt','rf'))
+model1 <- sdm(presence~.,data=data_forsdm,methods=c('glm','brt','rf','svm','mars','mlp','glmnet'),replication=c('boot'),n=2)
 model1
 
 roc(model1)
+
+
 
 # Predict
 p1 <- predict(model1,newdata=lidarmetrics,filename='p3m.img')
 plot(p1)
 
+e1 <- ensemble(model1,newdata=lidarmetrics,filename='e3m.img',setting=list(method="weighted",stat="AUC"))
+
 #GUI
 gui(model1)
+rcurve(model1)
