fc82a8ddea7cf4ddb659beae6c2448438b83737b
Zsofia Koma <komazsofi@gmail.com>
2018-11-22 09:44:42 +0100

    small changes

--- bird_lidar_sdm/escience/pre_process.bat
+++ bird_lidar_sdm/escience/pre_process.bat
@@ -1,4 +1,4 @@
-:: Using PDAl for transfer laz->las and normalize the point cloud
+:: Using PDAL for transfer laz->las and normalize the point cloud
 
 ::pdal translate D:\Koma\lidar_bird_dsm_workflow\escience_test\tile0_1.laz D:\Koma\lidar_bird_dsm_workflow\escience_test\tile0_1.las
 pdal translate D:\Koma\lidar_bird_dsm_workflow\escience_test\tile0_1.las D:\Koma\lidar_bird_dsm_workflow\escience_test\tile0_1_norm.las hag ferry --filters.ferry.dimensions="HeightAboveGround=Z"
