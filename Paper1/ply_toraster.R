f7568e59e3d1db1424248e9403d2fe82882f36fb
Zsofia Koma <komazsofi@gmail.com>
2018-11-23 10:07:13 +0100

    non-normalization, ply to raster scrpit R from command line

--- Paper1/ply_toraster.R
+++ Paper1/ply_toraster.R
@@ -9,14 +9,19 @@ library("data.table")
 
 # global variables
 
-workingdirectory="D:/Koma/Paper1_ReedStructure/1_ProcessingLiDAR/02gz2/tiled/"
-filename="tile_1_0.las_ground"
+args = commandArgs(trailingOnly=TRUE)
+
+workingdirectory=args[1]
+filename=args[2]
+
+#workingdirectory="D:/Koma/Paper1_ReedStructure/1_ProcessingLiDAR/02gz2/tiled/"
+#filename="tile_1_0.las_ground"
 
 setwd(workingdirectory)
 
 # Metrics without normalization
 
-pc_fea_veg = fread(paste(filename ,"2.5m_cell.ply",sep=""), header = F, sep = ' ',skip=31)
+pc_fea_veg = fread(paste(filename ,"2.5m_cell.ply",sep=""), header = F, sep = ' ',skip=42)
 #print(head(pc_fea))
 
 pc_fea_veg_dens_amean <- rasterFromXYZ(pc_fea_veg[,c(1,2,4)])
@@ -34,49 +39,44 @@ writeRaster(pc_fea_veg_eigv3, paste(filename,"_eigv3.tif",sep=""),overwrite=TRUE
 pc_fea_veg_kurto_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,10)])
 writeRaster(pc_fea_veg_kurto_z, paste(filename,"_kurto_z.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_pdens <- rasterFromXYZ(pc_fea_veg[,c(1,2,11)])
+pc_fea_veg_max_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,11)])
+writeRaster(pc_fea_veg_max_z, paste(filename,"_max_z.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_mean_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,12)])
+writeRaster(pc_fea_veg_mean_z, paste(filename,"_mean_z.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_median_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,13)])
+writeRaster(pc_fea_veg_median_z, paste(filename,"_median_z.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_perc_10 <- rasterFromXYZ(pc_fea_veg[,c(1,2,14)])
+writeRaster(pc_fea_veg_perc_10, paste(filename,"_perc_10.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_perc_30 <- rasterFromXYZ(pc_fea_veg[,c(1,2,15)])
+writeRaster(pc_fea_veg_perc_30, paste(filename,"_perc_30.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_perc_50 <- rasterFromXYZ(pc_fea_veg[,c(1,2,16)])
+writeRaster(pc_fea_veg_perc_50, paste(filename,"_perc_50.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_perc_70 <- rasterFromXYZ(pc_fea_veg[,c(1,2,17)])
+writeRaster(pc_fea_veg_perc_70, paste(filename,"_perc_70.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_perc_90 <- rasterFromXYZ(pc_fea_veg[,c(1,2,18)])
+writeRaster(pc_fea_veg_kurto_z, paste(filename,"_perc_90.tif",sep=""),overwrite=TRUE)
+
+pc_fea_veg_pdens <- rasterFromXYZ(pc_fea_veg[,c(1,2,19)])
 writeRaster(pc_fea_veg_pdens, paste(filename,"_pdens.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_pulsepen <- rasterFromXYZ(pc_fea_veg[,c(1,2,12)])
+pc_fea_veg_pulsepen <- rasterFromXYZ(pc_fea_veg[,c(1,2,20)])
 writeRaster(pc_fea_veg_pulsepen, paste(filename,"_pulsepen.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_skew_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,14)])
+pc_fea_veg_skew_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,22)])
 writeRaster(pc_fea_veg_skew_z, paste(filename,"_skew_z.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_std_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,15)])
+pc_fea_veg_std_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,23)])
 writeRaster(pc_fea_veg_std_z, paste(filename,"_std_z.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_var_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,16)])
+pc_fea_veg_var_z <- rasterFromXYZ(pc_fea_veg[,c(1,2,24)])
 writeRaster(pc_fea_veg_var_z, paste(filename,"_var_z.tif",sep=""),overwrite=TRUE)
 
-pc_fea_veg_z_entropy <- rasterFromXYZ(pc_fea_veg[,c(1,2,17)])
+pc_fea_veg_z_entropy <- rasterFromXYZ(pc_fea_veg[,c(1,2,25)])
 writeRaster(pc_fea_veg_z_entropy, paste(filename,"_z_entropy.tif",sep=""),overwrite=TRUE)
-
-# Metrics with normalization
-
-pc_fea_veg_norm = fread(paste(filename ,".las_ground_norm2.5m_cell.ply",sep=""), header = F, sep = ' ',skip=24)
-#print(head(pc_fea))
-
-pc_fea_veg_max_z <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,6)])
-writeRaster(pc_fea_veg_max_z, paste(filename,"_max_z_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_mean_z <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,7)])
-writeRaster(pc_fea_veg_mean_z, paste(filename,"_mean_z_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_median_z <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,8)])
-writeRaster(pc_fea_veg_median_z, paste(filename,"_median_z_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_perc_10 <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,9)])
-writeRaster(pc_fea_veg_perc_10, paste(filename,"_perc_10_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_perc_30 <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,10)])
-writeRaster(pc_fea_veg_perc_30, paste(filename,"_perc_30_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_perc_50 <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,11)])
-writeRaster(pc_fea_veg_perc_50, paste(filename,"_perc_50_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_perc_70 <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,12)])
-writeRaster(pc_fea_veg_perc_70, paste(filename,"_perc_70_norm.tif",sep=""),overwrite=TRUE)
-
-pc_fea_veg_perc_90 <- rasterFromXYZ(pc_fea_veg_norm[,c(1,2,13)])
-writeRaster(pc_fea_veg_perc_90, paste(filename,"_perc_90_norm.tif",sep=""),overwrite=TRUE)
--- Paper1/run_featureextraction.sh
+++ Paper1/run_featureextraction.sh
@@ -7,8 +7,4 @@ Aim: extract vegetation related features with laserchicken
 
 path="D:/Koma/Paper1_ReedStructure/1_ProcessingLiDAR/02gz2/tiled/"
 
-#python vegfeaturecalc_norm.py $path tile_1_0.las_ground.las_ground_norm 
-#python vegfeaturecalc_non_norm.py $path tile_1_0.las_ground
-
-#for f in $path/*.las_ground.las_ground_norm.las; do python vegfeaturecalc_norm.py ${f%.*};done
-#for f in $path/*.las_ground.las; do python vegfeaturecalc_non_norm.py ${f%.*};done
+for f in $path/*.las_ground.las; do python vegfeaturecalc_non_norm.py ${f%.*};done
