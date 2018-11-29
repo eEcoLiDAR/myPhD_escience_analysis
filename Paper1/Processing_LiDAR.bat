f3b235539c47dee763b658d2d94f818538ae4fc6
komazsofi <komazsofi@gmail.com>
2018-11-29 15:11:45 +0100

    small change

--- Paper1/Processing_LiDAR.bat
+++ Paper1/Processing_LiDAR.bat
@@ -5,7 +5,8 @@
 ::set workingdirectory=D:\Koma\Paper1_ReedStructure\1_ProcessingLiDAR\02gz2
 ::set workingdirectory=D:\Koma\Paper1\ALS\02gz2
 ::set workingdirectory=D:\Koma\Paper1\ALS\06en2
-set workingdirectory=D:\Koma\Paper1\ALS\06fn1
+::set workingdirectory=D:\Koma\Paper1\ALS\06fn1
+set workingdirectory=D:\Koma\Paper1\ALS\02hz1
 
 :: Tiling
 mkdir %workingdirectory%\tiled
