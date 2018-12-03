f8571d0a3766bc0416bbaba9b888f8fb933f0310
Zsofia Koma <komazsofi@gmail.com>
2018-12-03 15:23:31 +0100

    fusion with the polygon

--- bird_lidar_sdm/building_workflow/forMScCourse_fusion_diffresolutions.R
+++ bird_lidar_sdm/building_workflow/forMScCourse_fusion_diffresolutions.R
@@ -20,27 +20,39 @@ library("maps")
 library("mapdata")
 
 # Set global variables
-full_path="C:/Users/zsofi/Google Drive/_Amsterdam/00_PhD/Teaching/MScCourse_GlobEcol_Biodiv/Project_dataset/Data_Prep/"
+full_path="D:/Sync/_Amsterdam/00_PhD/Teaching/MScCourse_GlobEcol_Biodiv/Project_dataset/Data_Prep/"
 
 lidar_data="/lidar/Lidarmetrics_forMScCourse.grd"
 landcover_file="LGN7.tif"
 bird_data="/bird/Breeding_bird_atlas_individual_observationsRoerdomp_grouped_presonly_nl.csv"
 polygon_file="/bird/kmsquares.shp"
+nl_boundary="/bird/Boundary_NL_RDNew.shp"
 
 setwd(full_path)
 
 # Import
 polygon=readOGR(dsn=paste(full_path,polygon_file,sep=""))
-plot(polygon)
+nl=readOGR(dsn=paste(full_path,nl_boundary,sep=""))
+#plot(polygon)
+
+
+birds=read.csv(paste(full_path,bird_data,sep=""), header=TRUE, sep=",")
 
 lidarmetrics=stack(paste(full_path,lidar_data,sep=""))
 
-# Join bird presence data to polygon
+landcover=stack(paste(full_path,landcover_file,sep=""))
 
+# Join bird presence data to polygon
+bird_polygons=merge(polygon,birds,by.x="KMHOK",by.y="kmsquare")
+bird_polygons@data[is.na(bird_polygons@data$occurrence), "occurrence"] <- 0
+bird_polygons_pres <- bird_polygons[bird_polygons@data$occurrence==1,]
+plot(bird_polygons_pres)
 
 # Create landcover mask in 1 km x 1km resolution and add to the polygon
 
 # Aggregate LiDAR to 1 km x 1km and add to the polygon
 beginCluster()
-polygon <- extract(lidarmetrics, polygon, fun = mean, na.rm = TRUE, sp = TRUE)
+polygon_withmetrics <- extract(lidarmetrics, bird_polygons_pres, fun = mean, na.rm = TRUE, sp = TRUE)
 endCluster()
+
+spplot(polygon_withmetrics, zcol="perc_90_all",sp.layout = nl)
