8a946a72c745cde543b240593ec1fe484c2948fa
Zsofia Koma <komazsofi@gmail.com>
2018-12-07 13:45:36 +0100

    automatic training data within polygon

--- Paper1/createTraining.R
+++ Paper1/createTraining.R
@@ -27,15 +27,33 @@ setwd(full_path)
 
 # Import
 polygon=readOGR(dsn=paste(full_path,polygon_file,sep=""))
-print(unique(polygon@data$StructDef))
 
-Human <- polygon[polygon@data$StructDef == "A",]
-plot(Human)
+# Create trainings per classes
+classes=unique(polygon@data$StructDef)
 
-points_inpoly=spsample(Human, n = 15, "random")
+for (cat in classes) { 
+  print(cat)
+  sel_poly <- polygon[polygon@data$StructDef == cat,]
+  points_inpoly=spsample(sel_poly, n = 50, "random")
+  points_inpoly_df=as.data.frame(points_inpoly)
+  points_inpoly_df$level3=cat
+  write.table(points_inpoly_df, file = paste(cat,"_selpolyperclass.csv",sep="_"),row.names=FALSE,col.names=FALSE,sep=",")
+}
 
-points_inpoly_df=as.data.frame(points_inpoly)
-points_inpoly_df$level3="A"
+# Reorganize
+files <- list.files(pattern = "_selpolyperclass.csv")
 
-plot(points_inpoly)
-plot(Human,add=TRUE)
+allcsv <- lapply(files,function(i){
+  read.csv(i, header=FALSE)
+})
+
+allcsv_df <- do.call(rbind.data.frame, allcsv)
+
+coordinates(allcsv_df)=~V1+V2
+proj4string(allcsv_df)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+#plot(polygon)
+#plot(allcsv_df,add=TRUE)
+
+# Export shapefile
+rgdal::writeOGR(allcsv_df, '.', 'training', 'ESRI Shapefile',overwrite_layer = TRUE)
