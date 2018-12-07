ee104ed1ecbbf058ccd37405c41e9011b9c1b0af
Zsofia Koma <komazsofi@gmail.com>
2018-12-07 15:38:07 +0100

    create classes

--- Paper1/createTraining.R
+++ Paper1/createTraining.R
@@ -34,7 +34,7 @@ classes=unique(polygon@data$StructDef)
 for (cat in classes) { 
   print(cat)
   sel_poly <- polygon[polygon@data$StructDef == cat,]
-  points_inpoly=spsample(sel_poly, n = 50, "random")
+  points_inpoly=spsample(sel_poly, n = 25, "random")
   points_inpoly_df=as.data.frame(points_inpoly)
   points_inpoly_df$level3=cat
   write.table(points_inpoly_df, file = paste(cat,"_selpolyperclass.csv",sep="_"),row.names=FALSE,col.names=FALSE,sep=",")
@@ -52,8 +52,23 @@ allcsv_df <- do.call(rbind.data.frame, allcsv)
 coordinates(allcsv_df)=~V1+V2
 proj4string(allcsv_df)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-#plot(polygon)
-#plot(allcsv_df,add=TRUE)
+# Add levels and buffer zone
+
+allcsv_df@data$V4=NA
+allcsv_df@data$V4[allcsv_df@data$V3=='W']="W"
+allcsv_df@data$V4[allcsv_df@data$V3=='A']="A"
+allcsv_df@data$V4[allcsv_df@data$V3=='Rwo' | allcsv_df@data$V3=='Rwd' | allcsv_df@data$V3=='Rlo' 
+                  | allcsv_df@data$V3=='Rld' | allcsv_df@data$V3=='Rko' | allcsv_df@data$V3=='Rkd' 
+                  | allcsv_df@data$V3=='U']="R"
+allcsv_df@data$V4[allcsv_df@data$V3=='Gl' | allcsv_df@data$V3=='Gh' | 
+                    allcsv_df@data$V3=='K' | allcsv_df@data$V3=='P']="G"
+allcsv_df@data$V4[allcsv_df@data$V3=='Slo' | allcsv_df@data$V3=='Sld' | allcsv_df@data$V3=='Smo' 
+                  | allcsv_df@data$V3=='Smd' | allcsv_df@data$V3=='Sho' | allcsv_df@data$V3=='Shd'] = "S"
+allcsv_df@data$V4[allcsv_df@data$V3=='Bo' | allcsv_df@data$V3=='Bd']="B"
+
+allcsv_df@data$V5=as.numeric(factor(allcsv_df@data$V4))
+
+allcsv_df_buff5 <- gBuffer( allcsv_df, width=5, byid=TRUE )
 
 # Export shapefile
-rgdal::writeOGR(allcsv_df, '.', 'training', 'ESRI Shapefile',overwrite_layer = TRUE)
+rgdal::writeOGR(allcsv_df_buff5, '.', 'training', 'ESRI Shapefile',overwrite_layer = TRUE)
