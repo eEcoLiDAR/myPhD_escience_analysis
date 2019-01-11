d0c3951afd35ec349a4f8272e1fdb2a9283b05b4
komazsofi <komazsofi@gmail.com>
2019-01-11 18:19:30 +0100

    make for to download all required data

--- Paper1_inR/PreProcess_Pipeline.R
+++ Paper1_inR/PreProcess_Pipeline.R
@@ -9,18 +9,18 @@ library("lidR")
 workingdirectory="D:/Koma/Paper1/ALS/01_test/"
 setwd(workingdirectory)
 
-# Set filenames
-req_tile="02gz2"
+# Set filenames and dwnload and unzip the required dataset
+req_tile=list("02gz2","02hz1")
 
-# Download the required files
-download.file(paste("http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_","gefilterd/g",req_tile,".laz.zip",sep=""),
-              paste("g",req_tile,".laz.zip",sep=""))
-download.file(paste("http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_","uitgefilterd/u",req_tile,".laz.zip",sep=""),
-              paste("u",req_tile,".laz.zip",sep=""))
-
-# Unzip
-unzip(paste("g",req_tile,".laz.zip",sep=""))
-unzip(paste("u",req_tile,".laz.zip",sep=""))
+for (tile in req_tile){
+  download.file(paste("http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_","gefilterd/g",tile,".laz.zip",sep=""),
+                                     paste("g",tile,".laz.zip",sep=""))
+  download.file(paste("http://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_","uitgefilterd/u",tile,".laz.zip",sep=""),
+                paste("u",tile,".laz.zip",sep=""))
+  
+  unzip(paste("g",tile,".laz.zip",sep=""))
+  unzip(paste("u",tile,".laz.zip",sep=""))
+  }
 
 # Create catalog
 ctg <- catalog(workingdirectory)
@@ -41,10 +41,10 @@ opt_output_files(newctg) <- "D:/Koma/Paper1/ALS/01_test/tiled/{XLEFT}_{YBOTTOM}_
 ground_output <- lasground(newctg, csf(sloop_smooth = TRUE))
 
 # Create DTM
-opt_output_files(ground_output) <- "D:/Koma/Paper1/ALS/01_test/tiled/{XLEFT}_{YBOTTOM}_ground_dtm2"
+opt_output_files(ground_output) <- "D:/Koma/Paper1/ALS/01_test/tiled/{XLEFT}_{YBOTTOM}_ground_dtm"
 ground_output@input_options$filter <- "-keep_class 2"
 
-dtm_output = grid_metrics(ground_output,min(Z),res=1.25)
+dtm_output = grid_metrics(ground_output,min(Z),res=1.25) #somehow an windows error message occur related to usage gdal -- should accept and restart the program from here
 
 # Hillshade
 setwd(paste(workingdirectory,"tiled/",sep=""))
@@ -71,30 +71,4 @@ dtm_shd <- hillShade(slope, aspect, 40, 270)
 plot(dtm_shd, col=grey(0:100/100))
 
 writeRaster(dtm_shd, "dtm_shd.tif",overwrite=TRUE)
-
-# Normalize
-opt_output_files(ground_output) <- "D:/Koma/Paper1/ALS/01_test/tiled/{XLEFT}_{YBOTTOM}_ground_normalized"
-ground_output@input_options$filter <- ""
-
-normalized_ctg <- lasnormalize(ground_output, dtm)
-
-# Canopy Modell
-opt_output_files(normalized_ctg) <- "D:/Koma/Paper1/ALS/01_test/tiled/{XLEFT}_{YBOTTOM}_ground_chm"
-grid_canopy(normalized_ctg, res = 1.25, p2r())
-
-chmfiles <- list.files(pattern = "_ground_chm.tif")
-
-alltiff <- lapply(chmfiles,function(i){
-  stack(i)
-})
-
-alltiff$fun <- mean
-alltiff$na.rm <- TRUE
-chm <- do.call(mosaic, alltiff)
-
-plot(chm)
-
-crs(chm) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
-
-writeRaster(chm, "chm.tif",overwrite=TRUE)
 
