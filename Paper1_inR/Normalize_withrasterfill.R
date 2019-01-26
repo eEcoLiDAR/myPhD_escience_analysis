d142988b11cebe00ec1a35f64cfb254f39a2e3f8
komazsofi <komazsofi@gmail.com>
2019-01-26 17:17:01 +0100

    test normalization

--- Paper1_inR/Normalize_withrasterfill.R
+++ Paper1_inR/Normalize_withrasterfill.R
@@ -4,32 +4,17 @@ Aim: Pre-process AHN2 data
 "
 library("lidR")
 library("raster")
+library("zoo")
 
 # Set working dirctory
 workingdirectory="C:/Koma/Paper1/ALS/"
 
-setwd(paste(workingdirectory,"ground/",sep=""))
+setwd(paste(workingdirectory,"homogenized/",sep=""))
 
-ground_ctg <- catalog(paste(workingdirectory,"ground/",sep=""))
+homogenized_ctg <- catalog(paste(workingdirectory,"homogenized/",sep=""))
 
-opt_chunk_buffer(ground_ctg) <- 0
-opt_cores(ground_ctg) <- 3
-
-# Homogenize the point cloud
-
-orig_pdens=grid_density(ground_ctg)
-plot(orig_pdens)
-writeRaster(orig_pdens, "orig_pdens.tif",overwrite=TRUE)
-
-opt_output_files(ground_ctg) <- paste(workingdirectory,"homogenized/{XLEFT}_{YBOTTOM}",sep="")
-
-homogenized_ctg=lasfilterdecimate(ground_ctg,homogenize(10,1))
-
-opt_output_files(homogenized_ctg)=""
-
-hom_pdens=grid_density(homogenized_ctg)
-plot(hom_pdens)
-writeRaster(hom_pdens, "hom_pdens.tif",overwrite=TRUE)
+opt_chunk_buffer(homogenized_ctg) <- 2.5
+opt_cores(homogenized_ctg) <- 3
 
 # DTM
 dtm = grid_metrics(homogenized_ctg,min(Z),res=2.5)
@@ -38,30 +23,35 @@ writeRaster(dtm, "dtm.tif",overwrite=TRUE)
 
 plot(dtm)
 
-# Hillshade
+# Fill gaps
+dtm_m=as.matrix(dtm)
 
-slope <- terrain(dtm, opt='slope')
-aspect <- terrain(dtm, opt='aspect')
-dtm_shd <- hillShade(slope, aspect, 40, 270)
-
-plot(dtm_shd, col=grey(0:100/100))
-
-writeRaster(dtm_shd, "dtm_shd.tif",overwrite=TRUE)
+dtm_m_f=na.locf(dtm_m)
+dtm_m_f_r=raster(dtm_m_f)
+extent(dtm_m_f_r) <- extent(dtm)
 
 # Normalize
-opt_output_files(homogenized_ctg) <- paste(workingdirectory,"normalized/{XLEFT}_{YBOTTOM}",sep="")
-normalized_ctg=lasnormalize(homogenized_ctg,knnidw(k=50))
+opt_output_files(homogenized_ctg) <- paste(workingdirectory,"normalized2/{XLEFT}_{YBOTTOM}_1",sep="")
+normalized_ctg=lasnormalize(homogenized_ctg,knnidw(k=5,p=6))
 
-# Check DSMs
-opt_output_files(homogenized_ctg) <- ""
+opt_output_files(homogenized_ctg) <- paste(workingdirectory,"normalized2/{XLEFT}_{YBOTTOM}_2",sep="")
+normalized_ctg2=lasnormalize(homogenized_ctg,knnidw(k=5,p=2))
+
+opt_output_files(homogenized_ctg) <- paste(workingdirectory,"normalized2/{XLEFT}_{YBOTTOM}_3",sep="")
+normalized_ctg3=lasnormalize(homogenized_ctg,knnidw(k=10,p=10))
+
+# Check back
 opt_output_files(normalized_ctg) <- ""
+norm_error1 = grid_metrics(normalized_ctg,length(Z[Z<0]),res=2.5)
+plot(norm_error1)
+hist(norm_error1)
 
-dsm_whnorm = grid_metrics(homogenized_ctg,range(Z),res=2.5)
-dsm_wnorm = grid_metrics(normalized_ctg,max(Z),res=2.5)
+opt_output_files(normalized_ctg2) <- ""
+norm_error2 = grid_metrics(normalized_ctg2,length(Z[Z<0]),res=2.5)
+plot(norm_error2)
+hist(norm_error2)
 
-plot(dsm_whnorm)
-plot(dsm_wnorm)
-
-plot(dsm_whnorm-dsm_wnorm)
-hist(dsm_whnorm-dsm_wnorm)
-
+opt_output_files(normalized_ctg3) <- ""
+norm_error3 = grid_metrics(normalized_ctg3,length(Z[Z<0]),res=2.5)
+plot(norm_error3)
+hist(norm_error3)
