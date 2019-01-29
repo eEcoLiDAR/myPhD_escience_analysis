22d60f608f989b35fad288586cf188e183381f2c
Zsofia Koma <komazsofi@gmail.com>
2019-01-29 11:31:58 +0100

    add shannon

--- Paper1_inR/test_heightlayers_fea.R
+++ Paper1_inR/test_heightlayers_fea.R
@@ -49,3 +49,36 @@ simpson = function(z, by = 1, zmax = NULL)
 z = abs(rnorm(50, 25, 1))
 simpson = simpson(z, by = 1, zmax = NULL)
 simpson
+
+shannon = function(z, by = 1, zmax = NULL)
+{
+  # Fixed entropy (van Ewijk et al. (2011)) or flexible entropy
+  if (is.null(zmax))
+    zmax = max(z)
+  
+  # If zmax < 3 it is meaningless to compute entropy
+  if (zmax < 2 * by)
+    return(NA_real_)
+  
+  if (min(z) < 0)
+    return(NA_real_)
+  
+  # Define the number of x meters bins from 0 to zmax (rounded to the next integer)
+  bk = seq(0, ceiling(zmax/by)*by, by)
+  
+  # Compute the p for each bin
+  hist = hist(z,bk,plot=FALSE)
+  
+  # Proportion
+  p=(hist$counts/length(z))
+  p=p[p>0]
+  
+  # Simpson index
+  S=-sum(p*log(p))
+  
+  return(S)
+}
+
+z = abs(rnorm(50, 25, 1))
+shannon = shannon(z, by = 1, zmax = NULL)
+shannon
