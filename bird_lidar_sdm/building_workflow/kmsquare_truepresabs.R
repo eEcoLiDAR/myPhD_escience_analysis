478488553e2e380727b690f48cb1f26372050b72
Zsofia Koma <komazsofi@gmail.com>
2019-02-22 12:21:59 +0100

    add actual kmsquare coordinates to kmsquares

--- bird_lidar_sdm/building_workflow/kmsquare_truepresabs.R
+++ bird_lidar_sdm/building_workflow/kmsquare_truepresabs.R
@@ -25,6 +25,8 @@ filename="Breeding_bird_atlas_aggregated_data_kmsquares.csv"
 
 nl="Boundary_NL_RDNew.shp"
 
+kmsquares="kmsquare_aslist.csv"
+
 setwd(full_path)
 
 # Import
@@ -35,44 +37,65 @@ nl_bound@data$id = rownames(nl_bound@data)
 nl_bound.points = fortify(nl_bound, region="id")
 nl_bound.df = join(nl_bound.points, nl_bound@data, by="id")
 
-# Pre-processing
+#kmsquares_poly = readOGR(dsn=kmsquares)
+#kmsquares_poly@data$id = rownames(kmsquares_poly@data)
+#kmsquares_poly.points = fortify(kmsquares_poly, region="id")
+#kmsquares_poly.df = join(kmsquares_poly.points, kmsquares_poly@data, by="id")
 
-# Filter species
-#bird_species="Kleine Karekiet"
-#bird_species="Roerdomp" 
-bird_species="Snor" 
-#bird_species="Baardman" 
-#bird_species="Grote Karekiet" 
+#as.numeric(kmsquares_poly.df$KMHOK)
+#colnames(kmsquares_poly.df)[colnames(kmsquares_poly.df)=="KMHOK"] <- "kmsquare"
+#kmsquares_poly.df=ddply(kmsquares_poly.df,~kmsquare+X+Y,summarise,sum=length(kmsquare))
 
-bird_data_onebird=bird_data[ which(bird_data$species==bird_species),]
+#write.csv(kmsquares_poly.df, file = "kmsquare_aslist.csv",row.names=FALSE)
+kmsquares_poly.df=read.csv(file=kmsquares,header=TRUE,sep=",")
+colnames(kmsquares_poly.df)[colnames(kmsquares_poly.df)=="sum"] <- "nofkmsquare"
 
-# generate id from coords
-#bird_data_onebird$coordid <- with(bird_data_onebird, paste0(x,y))
+# Processing
 
-observationmap=ddply(bird_data_onebird,~kmsquare+species+x+y,summarise,sum=sum(present))
-observationmap = within(observationmap, {
-  occurrence = ifelse(sum >0, 1, 0)
-})
+species=c("Kleine Karekiet","Roerdomp","Snor","Baardman","Grote Karekiet")
 
-pres=observationmap[which(observationmap$occurrence==1),]
-length(pres$occurrence)
+for (bird_species in species) {
+  
+  bird_data_onebird=bird_data[ which(bird_data$species==bird_species),]
+  
+  observationmap=ddply(bird_data_onebird,~kmsquare+species,summarise,sum=sum(present))
+  observationmap = within(observationmap, {
+    occurrence = ifelse(sum >0, 1, 0)
+  })
+  
+  
+  observation_presabs=merge(observationmap,kmsquares_poly.df,by="kmsquare",all.x = TRUE)
+  
+  # Visualize
+  p6=ggplot() + 
+    geom_polygon(data=nl_bound.df,aes(long,lat,group=group),fill = NA, color="black") +
+    geom_path(color="white") +
+    coord_equal() +
+    geom_point(data=observation_presabs, aes(x=X,y=Y,color=as.factor(occurrence)),inherit.aes = FALSE,size=1) +
+    scale_color_manual(values=c('#6666FF','#990000'))+
+    labs(x="x",y="y",color="Presence-Absence") +
+    ggtitle(paste("Aggregated atlas data of",bird_species,"2013-2016",sep=" ")) +
+    theme_bw()
+  
+  p6
+  ggsave(paste(substr(filename, 1, nchar(filename)-4),bird_species,'_kmsquare_presabs_nl.jpg',sep=''),p6)
+  
+  # Export
+  write.csv(observation_presabs, file = paste(substr(filename, 1, nchar(filename)-4),bird_species,'_kmsquare_presabs_nl.csv',sep=''),row.names=FALSE)
+}
 
-# Visualize
-p6=ggplot() + 
-  geom_polygon(data=nl_bound.df,aes(long,lat,group=group),fill = NA, color="black") +
-  geom_path(color="white") +
-  coord_equal() +
-  geom_point(data=observationmap, aes(x=x,y=y,color=as.factor(occurrence)),inherit.aes = FALSE) +
-  scale_color_manual(values=c('#6666FF','#990000'))+
-  labs(x="x",y="y",color="Presence-Absence") +
-  ggtitle(paste("kmsquare observation of",bird_species,"2013-2016",sep=" ")) +
-  theme_bw()
+# Stat
 
-p6
-ggsave(paste(substr(filename, 1, nchar(filename)-4),bird_species,'_grouped_presabs_nl.jpg',sep=''),p6)
+bird_data_notnull=bird_data[ which(bird_data$present==1),]
+bird_data_notnull_grouped=ddply(bird_data_notnull,~kmsquare+species,summarise,sum=sum(present))
 
-# Export
-write.csv(observationmap, file = paste(substr(filename, 1, nchar(filename)-4),bird_species,'_grouped_presabs_nl.csv',sep=''),row.names=FALSE)
+overall_kmstat <- bird_data_notnull_grouped %>%
+  group_by(species) %>%
+  summarise(nofsp = length(species))
 
+p1<-ggplot(overall_kmstat, aes(x=species, y=nofsp, fill=species)) +
+  geom_bar(stat="identity")+scale_fill_manual(values=c("#493829", "#A9A18C", "#613318","#B99C6B","#D57500")) +
+  geom_text(aes(label=nofsp), vjust=1.6, color="white", size=3.5)
+p1
 
-
+ggsave(paste(substr(filename, 1, nchar(filename)-4),'_kmsquarestat.jpg',sep=''),p1)
