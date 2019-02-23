691c1aa78f6e4415cdf87282752bb9237eba59ba
komazsofi <komazsofi@gmail.com>
2019-02-23 10:30:06 +0100

    add sdm

--- bird_lidar_sdm/building_workflow/baardman_anal.R
+++ bird_lidar_sdm/building_workflow/baardman_anal.R
@@ -14,6 +14,7 @@ library("dplyr")
 library("gridExtra")
 
 library("ggplot2")
+library("sdm")
 
 # Set global variables
 full_path="C:/Koma/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/DataProcess/"
@@ -37,7 +38,7 @@ formask=crop(formask,extent(lidarmetrics))
 formask_resampled=resample(formask,lidarmetrics)
 
 lidarmetrics_sel <- mask(lidarmetrics, formask_resampled)
-writeRaster(lidarmetrics_sel, "lidarmetrics_sel.grd",overwrite=TRUE)
+#writeRaster(lidarmetrics_sel, "lidarmetrics_sel.grd",overwrite=TRUE)
 
 gc()
 
@@ -49,7 +50,7 @@ bird_data$km_y=bird_data$Y
 coordinates(bird_data)=~X+Y
 proj4string(bird_data)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-vals <- extract(lidarmetrics_sel,bird_data)
+vals <- raster::extract(lidarmetrics_sel,bird_data)
 bird_data$pulsepen <- vals[,13]
 bird_data$maxh <- vals[,17]
 bird_data$std <- vals[,24]
@@ -61,7 +62,7 @@ bird_withLiDAR=bird_withLiDAR[complete.cases(bird_withLiDAR), ]
 write.csv(bird_withLiDAR, file = "bird_withLiDAR.csv",row.names=FALSE)
 
 # Exploration analysis
-#bird_withLiDAR=bird_withLiDAR[-sample(which(bird_withLiDAR$occurrence==0), 500),]
+bird_withLiDAR=bird_withLiDAR[-sample(which(bird_withLiDAR$occurrence==0), 400),]
 
 p <- ggplot(bird_withLiDAR, aes(x=occurrence,y=pulsepen,group=factor(occurrence))) + 
   geom_boxplot()
@@ -76,4 +77,32 @@ p <- ggplot(bird_withLiDAR, aes(x=occurrence,y=std,group=factor(occurrence))) +
 p
 
 # SDM
+bird_obs=bird_withLiDAR[c("km_x","km_y","occurrence")]
+coordinates(bird_obs)=~km_x+km_y
+proj4string(bird_obs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
+data_forsdm <- sdmData(formula=occurrence~., train=bird_obs, predictors=lidarmetrics_sel)
+data_forsdm
+
+model1 <- sdm(occurrence~.,data=data_forsdm,methods=c('glm','rf'),replication=c('boot'),n=2)
+model1
+
+roc(model1)
+rcurve(model1,id = 3)
+
+feaimp_rf=getVarImp(model1,id = 3)
+
+feaimp_rf_ord <- feaimp_rf@varImportance[ order(feaimp_rf@varImportance[,3]), ]
+
+response_glm=getResponseCurve(model1,id = 1)
+response_rf=getResponseCurve(model1,id = 3)
+
+ggplot(data=response_rf@response$perc_10_nonground, aes(x=perc_10_nonground, y=`rf_ID-3`)) +
+  geom_line()+
+  geom_point()
+
+ggplot(data=response_glm@response$perc_90_nonground, aes(x=perc_90_nonground, y=`glm_ID-1`)) +
+  geom_line()+
+  geom_point()
+
+#gui(model1)
