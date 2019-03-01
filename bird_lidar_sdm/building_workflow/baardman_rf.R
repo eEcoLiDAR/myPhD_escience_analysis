15e186f199bf1e3d33620ae6c34f9cdca7903e8d
Zsofia Koma <komazsofi@gmail.com>
2019-03-01 14:52:25 +0100

    test rf

--- bird_lidar_sdm/building_workflow/baardman_rf.R
+++ bird_lidar_sdm/building_workflow/baardman_rf.R
@@ -48,42 +48,46 @@ bird_obs=presabs_bird[c("X","Y","occurrence")]
 coordinates(bird_obs)=~X+Y
 proj4string(bird_obs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-data_forsdm <- sdmData(formula=occurrence~., train=bird_obs, predictors=lidarmetrics)
-data_forsdm
+# Intersection
 
-model1 <- sdm(occurrence~.,data=data_forsdm,methods=c('glm','rf','maxent'),replication=c('boot'),n=2)
-model1
+presabsrast <- rasterize(bird_obs, lidarmetrics[[22]],field="occurrence")
 
-p1 <- predict(model1,newdata=lidarmetrics,filename='')
-plot(p1)
+masked <- mask(lidarmetrics, presabsrast)
 
-writeRaster(p1,"Baardman_sdm_predict_2.tif",overwrite=TRUE)
-writeRaster(p1[[3]],"Baardman_sdm_predict_rf_2.tif",overwrite=TRUE)
-writeRaster(p1[[5]],"Baardman_sdm_predict_maxent_2.tif",overwrite=TRUE)
+# convert training data into the right format
+trainingbrick <- addLayer(masked, presabsrast)
+featuretable <- getValues(trainingbrick)
+featuretable <- na.omit(featuretable)
+featuretable <- as.data.frame(featuretable)
 
-save(model1,file = "Baardman_model1.RData")
-load("Baardman_model1.RData")
+library(randomForest)
+library(caret)
 
-roc(model1)
-rcurve(model1,id = 1)
-rcurve(model1,id = 3)
-rcurve(model1,id = 5)
+trainIndex <- caret::createDataPartition(y=featuretable$layer, p=0.75, list=FALSE)
+trainingSet<- featuretable[trainIndex,]
+testingSet<- featuretable[-trainIndex,]
 
-feaimp_rf=getVarImp(model1,id = 3)
-feaimp_maxent=getVarImp(model1,id = 5)
+modelFit <- randomForest(factor(layer)~.,data=trainingSet)
 
-feaimp_rf_ord <- feaimp_rf@varImportance[ order(feaimp_rf@varImportance[,3]), ]
-feaimp_maxent_ord <- feaimp_maxent@varImportance[ order(feaimp_maxent@varImportance[,3]), ]
+prediction <- predict(modelFit,testingSet[ ,c(1:25)])
 
-feaimp_rf_ord
-feaimp_maxent_ord
+conf_m=confusionMatrix(factor(prediction), factor(testingSet$layer),mode = "everything")
 
-response_rf=getResponseCurve(model1,id = 3)
+print(conf_m)
 
-ggplot(data=response_rf@response$perc_10_nonground, aes(x=perc_10_nonground, y=`rf_ID-3`)) +
-  geom_line()+
-  geom_point()
+library(plotmo)
 
-ggplot(data=response_rf@response$perc_90_nonground, aes(x=perc_90_nonground, y=`rf_ID-3`)) +
-  geom_line()+
-  geom_point()
+plotmo(modelFit, type="prob", nresponse="0",all1=TRUE,all2 = FALSE)
+plotmo(modelFit, type="response",all1=TRUE,all2 = FALSE)
+
+p <- ggplot(featuretable, aes(x=layer,y=max_z_all,group=factor(layer))) + 
+  geom_boxplot()
+p
+
+colNames <- names(featuretable)[1:25]
+
+for (name in colNames) {
+  p <- ggplot(featuretable, aes_string(x="layer",y=name)) + geom_boxplot(aes(group=factor(layer)))
+  
+  ggsave(paste(name,'_baardman_boxplot.jpg',sep=''),p)
+}
