520386665957bd4d2e7680a3818c89f74364e684
komazsofi <komazsofi@gmail.com>
2019-03-01 19:48:23 +0100

    modify download to curl (only bash)

--- bird_lidar_sdm2/AHN3_downloader.R
+++ bird_lidar_sdm2/AHN3_downloader.R
@@ -4,20 +4,20 @@ Aim: This script download the required AHN3 data from the pdok website
 "
 
 # Set working directory
-workingdirectory="C:/Koma/Paper3/ALS/" ## set this directory where you would like to put your las files
+workingdirectory="D:/Koma/Paper3/" ## set this directory where you would like to put your las files
 setwd(workingdirectory)
 
 # Import filenames file
 ahn3file="listahn3.csv"
 ahn3list=read.csv(file=ahn3file,header=TRUE,sep=",")
 
-# Set filenames and dwnload and unzip the required dataset
-req_tile=ahn3list$bladnr_up[1:2]
+ahn3list$list=paste("https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_laz/C_",ahn3list$bladnr_up,".LAZ",sep="")
 
-for (tile in req_tile){
-  print(tile[1])
-  
-  download.file(paste("https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_laz/C_",tile,".LAZ",sep=""),
-                paste("C_",tile,".LAZ",sep=""))
-}
+write.table(ahn3list$list, file = "ahn3list.txt", append = FALSE, quote = FALSE, sep = "", 
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE, 
+            col.names = FALSE, qmethod = c("escape", "double")) 
+
+#while read p; do curl -o ${p:62:73} "${p%?}";done < ahn3list.txt
+
+
 
