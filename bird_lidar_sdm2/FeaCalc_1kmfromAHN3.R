0b8c44f90207d1c2eeeb0c3be9d30b56e3a3f827
komazsofi <komazsofi@gmail.com>
2019-03-02 10:53:51 +0100

    lidar metrics calc

--- bird_lidar_sdm2/FeaCalc_1kmfromAHN3.R
+++ bird_lidar_sdm2/FeaCalc_1kmfromAHN3.R
@@ -12,7 +12,7 @@ source("C:/Koma/Github/komazsofi/myPhD_escience_analysis/bird_lidar_sdm2/Functio
 workingdirectory="C:/Koma/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/DataProcess_2/ALS/" ## set this directory where your input las files are located
 setwd(workingdirectory)
 
-cores=3
+cores=18
 resolution=10
 
 rasterOptions(maxmemory = 200000000000)
@@ -46,3 +46,17 @@ names(sd_cover) <- "sd_cover"
 
 horizontal_metrics=stack(rough_dsm,tpi_dsm,tri_dsm,sd_dsm,var_dsm,sd_cover) 
 plot(horizontal_metrics)
+
+opt_filter(ctg) <- "-keep_class 1"
+
+vertdistr_metrics_whgr = grid_metrics(ctg, vertDistr_Metrics(Z),res=resolution)
+plot(vertdistr_metrics_whgr)
+
+heightmetrics_whgr = grid_metrics(ctg, HeightMetrics(Z),res=resolution)
+plot(heightmetrics_whgr)
+
+lidarmetrics=stack(covermetrics,vertdistr_metrics,heightmetrics,horizontal_metrics)
+writeRaster(lidarmetrics,"lidarmetrics.grd",overwrite=TRUE)
+
+lidarmetrics_whgr=stack(vertdistr_metrics_whgr,heightmetrics_whgr)
+writeRaster(lidarmetrics_whgr,"lidarmetrics_whgr.grd",overwrite=TRUE)
