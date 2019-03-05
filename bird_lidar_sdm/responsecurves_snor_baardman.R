d5be923048ddf10e9c2952b09be8c4a2f76cff79
komazsofi <komazsofi@gmail.com>
2019-03-05 23:20:12 +0100

    try add ribbon

--- bird_lidar_sdm/responsecurves_snor_baardman.R
+++ bird_lidar_sdm/responsecurves_snor_baardman.R
@@ -72,52 +72,63 @@ rcurve(baardman_model,id = 11)
 
 # make nice graph
 
-response_snor_svm=getResponseCurve(snor_model,id = 201)
-response_baardman_svm=getResponseCurve(baardman_model,id = 201)
+response_snor_svm=getResponseCurve(snor_model,id = c(seq(from=201,to=225,by=1)))
+response_baardman_svm=getResponseCurve(baardman_model,id = c(seq(from=201,to=225,by=1)))
 
 # pulsepen
 
+response_snor_svm@response$pulse_pen_ratio_all$sd=apply(response_snor_svm@response$pulse_pen_ratio_all,1, sd, na.rm = TRUE)
+response_snor_svm@response$pulse_pen_ratio_all$mean=apply(response_snor_svm@response$pulse_pen_ratio_all,1, mean, na.rm = TRUE)
 response_snor_svm@response$pulse_pen_ratio_all$species <- "Savi's Warbler"
+
+response_baardman_svm@response$pulse_pen_ratio_all$sd=apply(response_baardman_svm@response$pulse_pen_ratio_all,1, sd, na.rm = TRUE)
+response_baardman_svm@response$pulse_pen_ratio_all$mean=apply(response_baardman_svm@response$pulse_pen_ratio_all,1, mean, na.rm = TRUE)
 response_baardman_svm@response$pulse_pen_ratio_all$species <- "Bearded Reedling"
 
 pulse_pen=rbind(response_snor_svm@response$pulse_pen_ratio_all,response_baardman_svm@response$pulse_pen_ratio_all)
 
 # rough
 
+response_snor_svm@response$roughness.1$sd=apply(response_snor_svm@response$roughness.1,1, sd, na.rm = TRUE)
+response_snor_svm@response$roughness.1$mean=apply(response_snor_svm@response$roughness.1,1, mean, na.rm = TRUE)
 response_snor_svm@response$roughness.1$species <- "Savi's Warbler"
+
+response_baardman_svm@response$roughness.1$sd=apply(response_baardman_svm@response$roughness.1,1, sd, na.rm = TRUE)
+response_baardman_svm@response$roughness.1$mean=apply(response_baardman_svm@response$roughness.1,1, mean, na.rm = TRUE)
 response_baardman_svm@response$roughness.1$species <- "Bearded Reedling"
 
 roughness=rbind(response_snor_svm@response$roughness.1,response_baardman_svm@response$roughness.1)
 
-# var
-
-response_snor_svm@response$var_z_all$species <- "Savi's Warbler"
-response_baardman_svm@response$var_z_all$species <- "Bearded Reedling"
-
-var_z_all=rbind(response_snor_svm@response$var_z_all,response_baardman_svm@response$var_z_all)
-
 # max
 
+response_snor_svm@response$max_z__nonground$sd=apply(response_snor_svm@response$max_z__nonground,1, sd, na.rm = TRUE)
+response_snor_svm@response$max_z__nonground$mean=apply(response_snor_svm@response$max_z__nonground,1, mean, na.rm = TRUE)
 response_snor_svm@response$max_z__nonground$species <- "Savi's Warbler"
+
+response_baardman_svm@response$max_z__nonground$sd=apply(response_baardman_svm@response$max_z__nonground,1, sd, na.rm = TRUE)
+response_baardman_svm@response$max_z__nonground$mean=apply(response_baardman_svm@response$max_z__nonground,1, mean, na.rm = TRUE)
 response_baardman_svm@response$max_z__nonground$species <- "Bearded Reedling"
 
 max_z__nonground=rbind(response_snor_svm@response$max_z__nonground,response_baardman_svm@response$max_z__nonground)
 
 # plot
 
-p1=ggplot(data=pulse_pen, aes(x=pulse_pen_ratio_all, y=`svm_ID-201`,group=species,color=species)) + 
+p1=ggplot(data=pulse_pen, aes(x=pulse_pen_ratio_all, y=mean,group=species,color=species)) + 
   scale_color_manual(values = c("Savi's Warbler"="tan2","Bearded Reedling"="chocolate4")) + 
   geom_line(size=3,show.legend = FALSE) + 
+  geom_ribbon(aes(x=pulse_pen$pulse_pen_ratio_all,ymin=pulse_pen$mean-pulse_pen$sd, ymax=pulse_pen$mean+pulse_pen$sd), linetype=2, alpha=0.1,show.legend = FALSE) +
   xlab("Pulse penetration ratio [%]") + ylab("Response") + theme_bw(base_size = 30) + ylim(0.25, 1)
 
-p2=ggplot(data=roughness, aes(x=roughness.1, y=`svm_ID-201`,group=species,color=species)) + 
+p2=ggplot(data=roughness, aes(x=roughness.1, y=mean,group=species,color=species)) + 
   scale_color_manual(values = c("Savi's Warbler"="tan2","Bearded Reedling"="chocolate4")) + 
-  geom_line(size=3,show.legend = FALSE) + 
-  xlab("Surface roughness [m]") + ylab("Response") + theme_bw(base_size = 30) + ylim(0.25, 1)
+  geom_line(size=3,show.legend = FALSE) +
+  geom_ribbon(aes(x=roughness.1,ymin=mean-sd, ymax=mean+sd), linetype=2, alpha=0.1,show.legend = FALSE) +
+  xlab("Surface roughness [m]") + ylab("Response") + theme_bw(base_size = 30) + ylim(0.6, 2)
 
-p3=ggplot(data=max_z__nonground, aes(x=max_z__nonground, y=`svm_ID-201`,group=species,color=species)) + 
+p3=ggplot(data=max_z__nonground, aes(x=max_z__nonground, y=mean,group=species,color=species)) + 
   scale_color_manual(values = c("Savi's Warbler"="tan2","Bearded Reedling"="chocolate4")) + 
-  geom_line(size=3,show.legend = FALSE) + 
+  geom_line(size=3,show.legend = FALSE) +
+  geom_ribbon(aes(ymin=max_z__nonground["mean"]-max_z__nonground["sd"], ymax=max_z__nonground["mean"]+max_z__nonground["sd"]), linetype=2, alpha=0.1,show.legend = FALSE) +
   xlab("Vegetation height [m]") + ylab("Response") + theme_bw(base_size = 30) + ylim(0.25, 1)
 
 grid.arrange(
