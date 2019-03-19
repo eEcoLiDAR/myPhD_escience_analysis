432ab1304475ccd0250029d3f3869405538c0cd9
komazsofi <komazsofi@gmail.com>
2019-03-19 17:35:23 +0100

    stack metrics

--- Paper1_inR_v2/Pipeline_PrepareLiDARMetrics.R
+++ Paper1_inR_v2/Pipeline_PrepareLiDARMetrics.R
@@ -29,97 +29,71 @@ proj4string(covermetrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.
 heightmetrics=stack("height_metrics_gr.grd")
 proj4string(heightmetrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-heightmetrics_wgr=stack("height_metrics_whgr_gr_norm.grd")
-proj4string(heightmetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-
 horizontalmetrics=stack("horizontal_metrics_gr.grd")
 proj4string(horizontalmetrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-
-horizontalmetrics_wgr=stack("horizontal_metrics_whgr_gr_norm.grd")
-proj4string(horizontalmetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 shapemetrics=stack("shapemetrics_gr.grd")
 proj4string(shapemetrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-shapemetrics_wgr=stack("shapemetrics_whgr_gr_norm.grd")
-proj4string(shapemetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-
 vertdistrmetrics=stack("vertdistr_metrics_gr.grd")
 proj4string(vertdistrmetrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+formask=stack("building_rast.grd")
+proj4string(formask)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# Merge files into one lidarmetrics file
+ex_m=extent(shapemetrics)
+
+covermetrics_m=crop(covermetrics,ex_m)
+heightmetrics_mod_m=crop(heightmetrics,ex_m)
+horizontalmetrics_mod_m=crop(horizontalmetrics,ex_m)
+vertdistrmetrics_mod_m=crop(vertdistrmetrics,ex_m)
+
+lidarmetrics=stack(covermetrics_m,shapemetrics,vertdistrmetrics_mod_m,horizontalmetrics_mod_m,heightmetrics_mod_m)
+#writeRaster(lidarmetrics,"lidarmetrics.grd",overwrite=TRUE)
+
+# Exclude buildings
+
+#buildings=readOGR(dsn="buldings.shp")
+
+#building_rast <- rasterize(buildings, lidarmetrics[[1]],field="DN")
+formask <- setValues(raster(lidarmetrics[[1]]), 1)
+formask[building_rast>0] <- NA
+
+writeRaster(formask,"building_rast.grd",overwrite=TRUE)
+
+lidarmetrics_masked <- mask(lidarmetrics, formask)
+writeRaster(lidarmetrics_masked,"lidarmetrics_gr_masked.grd",overwrite=TRUE)
+
+# Read the separate lidarmetrics files into memory (excluding ground points)
+
+covermetrics_wgr=stack("covermetrics_gr_norm.grd")
+proj4string(covermetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+heightmetrics_wgr=stack("height_metrics_whgr_gr_norm.grd")
+proj4string(heightmetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+horizontalmetrics_wgr=stack("horizontal_metrics_whgr_gr_norm.grd")
+proj4string(horizontalmetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+shapemetrics_wgr=stack("shapemetrics_whgr_gr_norm.grd")
+proj4string(shapemetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 vertdistrmetrics_wgr=stack("vertdistr_metrics_whgr_gr_norm.grd")
 proj4string(vertdistrmetrics_wgr)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-# Combine gr and wgr
+ex_m=extent(shapemetrics_wgr)
 
-#Height
-ex = extent(heightmetrics_wgr)
-heightmetrics_m = crop(heightmetrics, ex)
+covermetrics_m_wgr=crop(covermetrics_wgr,ex_m)
+heightmetrics_mod_m_wgr=crop(heightmetrics_wgr,ex_m)
+horizontalmetrics_mod_m_wgr=crop(horizontalmetrics_wgr,ex_m)
+vertdistrmetrics_mod_m_wgr=crop(vertdistrmetrics_wgr,ex_m)
+shapemetrics_mod_m_wgr=crop(shapemetrics_wgr,ex_m)
 
-heightmetrics_mod <- overlay(heightmetrics_m, heightmetrics_wgr, fun = function(x, y) {
-  y[is.na(y[])] <- x[is.na(y[])]
-  return(y)
-})
+lidarmetrics_wgr=stack(covermetrics_m_wgr,shapemetrics_mod_m_wgr,vertdistrmetrics_mod_m_wgr,horizontalmetrics_mod_m_wgr,heightmetrics_mod_m_wgr)
 
-names(heightmetrics_mod) <- names(heightmetrics_wgr)
-#writeRaster(heightmetrics_mod,"heightmetrics_merged.grd",overwrite=TRUE)
-
-# Shape
-ex = extent(shapemetrics_wgr)
-shapemetrics_m = crop(shapemetrics, ex)
-
-shapemetrics_mod <- overlay(shapemetrics_m, shapemetrics_wgr, fun = function(x, y) {
-  y[is.na(y[])] <- x[is.na(y[])]
-  return(y)
-})
-
-names(shapemetrics_mod) <- names(shapemetrics_wgr)
-#writeRaster(shapemetrics_mod,"shapemetrics_merged.grd",overwrite=TRUE)
-
-# vert. distr
-ex = extent(vertdistrmetrics_wgr)
-vertdistrmetrics_m = crop(vertdistrmetrics, ex)
-
-vertdistrmetrics_mod <- overlay(vertdistrmetrics_m, vertdistrmetrics_wgr, fun = function(x, y) {
-  y[is.na(y[])] <- x[is.na(y[])]
-  return(y)
-})
-
-names(vertdistrmetrics_mod) <- names(vertdistrmetrics_wgr)
-#writeRaster(vertdistrmetrics_mod,"vertdistrmetrics_merged.grd",overwrite=TRUE)
-
-#horizontal
-
-ex = extent(horizontalmetrics_wgr)
-horizontalmetrics_m = crop(horizontalmetrics, ex)
-
-horizontalmetrics_mod <- overlay(horizontalmetrics_m, horizontalmetrics_wgr, fun = function(x, y) {
-  y[is.na(y[])] <- x[is.na(y[])]
-  return(y)
-})
-
-names(horizontalmetrics_mod) <- names(horizontalmetrics_wgr)
-
-# Merge files into one lidarmetrics file
-ex_m=extent(shapemetrics_mod)
-
-covermetrics_m=crop(covermetrics,ex_m)
-heightmetrics_mod_m=crop(heightmetrics_mod,ex_m)
-horizontalmetrics_mod_m=crop(horizontalmetrics_mod,ex_m)
-vertdistrmetrics_mod_m=crop(vertdistrmetrics_mod,ex_m)
-
-lidarmetrics=stack(covermetrics_m,shapemetrics_mod,vertdistrmetrics_mod_m,horizontalmetrics_mod_m,heightmetrics_mod_m)
-#writeRaster(lidarmetrics,"lidarmetrics.grd",overwrite=TRUE)
-
-# Exclude buildings
-buildings=readOGR(dsn="buldings.shp")
-
-building_rast <- rasterize(buildings, lidarmetrics[[1]],field="DN")
-formask <- setValues(raster(lidarmetrics[[1]]), 1)
+formask <- setValues(raster(lidarmetrics_wgr[[1]]), 1)
 formask[building_rast>0] <- NA
 
-writeRaster(formask,"building_formask.grd",overwrite=TRUE)
-
-lidarmetrics_masked <- mask(lidarmetrics, formask)
-writeRaster(lidarmetrics_masked,"lidarmetrics_masked.grd",overwrite=TRUE)
-
+lidarmetrics_masked_wgr <- mask(lidarmetrics_wgr, formask)
+writeRaster(lidarmetrics_masked_wgr,"lidarmetrics_gr_masked_wgr.grd",overwrite=TRUE)
