7c228c4fae0127ac2b46718cb738aeee6e35e502
komazsofi <komazsofi@gmail.com>
2019-03-25 22:32:43 +0100

    intersect ahn2 and transect

---
+++ RV_MSc/Intersect_ahn_transect.R
@@ -0,0 +1,47 @@
+"
+@author: Zsofia Koma, UvA
+Aim: overlay butterfly transect with LiDAR 
+"
+
+# Import libraries
+library("sp")
+library("rgdal")
+library("raster")
+library("spatialEco")
+library("rgeos")
+library("plyr")
+library("dplyr")
+library("gridExtra")
+
+library("ggplot2")
+library("sdm")
+library("sf")
+
+# Set global variables
+full_path="C:/Koma/Sync/_Amsterdam/08_coauthor_MScProjects/Reinier/datapreprocess/"
+
+transectfile="Transects_0712_Complete.csv"
+ahnfile="ahn2.shp"
+
+setwd(full_path)
+
+# Import 
+
+ahn2 = readOGR(dsn=ahnfile)
+transect=read.csv(file=transectfile,header=TRUE,sep=";",dec=",")
+
+# Convert transect to point shp
+
+transect$X=transect$x
+transect$Y=transect$y
+
+transect_shp=transect[c("X","Y","x","y","Tr_sec","Transect","Section")]
+coordinates(transect_shp)=~X+Y
+proj4string(transect_shp)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+intersect_points_wpoly = point.in.poly(transect_shp, ahn2)
+intersect_points_wpoly_df=intersect_points_wpoly@data
+
+per_ahn2 <- intersect_points_wpoly_df %>%
+  group_by(bladnr) %>%
+  summarise(nofobs = length(bladnr))
