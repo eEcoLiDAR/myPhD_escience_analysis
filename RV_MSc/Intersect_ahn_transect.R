c75fba9752862b2d1644ab5252c5ee199faa6956
Zsofia Koma <komazsofi@gmail.com>
2019-03-29 09:51:06 +0100

    small changes

--- RV_MSc/Intersect_ahn_transect.R
+++ RV_MSc/Intersect_ahn_transect.R
@@ -4,21 +4,18 @@ Aim: overlay butterfly transect with LiDAR
 "
 
 # Import libraries
-library("sp")
 library("rgdal")
 library("raster")
-library("spatialEco")
 library("rgeos")
-library("plyr")
+library("spatialEco")
 library("dplyr")
-library("gridExtra")
 
 library("ggplot2")
-library("sdm")
-library("sf")
+library("gridExtra")
 
 # Set global variables
-full_path="C:/Koma/Sync/_Amsterdam/08_coauthor_MScProjects/Reinier/datapreprocess/"
+#full_path="C:/Koma/Sync/_Amsterdam/08_coauthor_MScProjects/Reinier/datapreprocess/"
+full_path="D:/Sync/_Amsterdam/08_coauthor_MScProjects/Reinier/datapreprocess/"
 
 transectfile="Transects_0712_Complete.csv"
 ahnfile="ahn2.shp"
@@ -39,9 +36,13 @@ transect_shp=transect[c("X","Y","x","y","Tr_sec","Transect","Section")]
 coordinates(transect_shp)=~X+Y
 proj4string(transect_shp)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-intersect_points_wpoly = point.in.poly(transect_shp, ahn2)
+intersect_points_wpoly = spatialEco::point.in.poly(transect_shp, ahn2)
 intersect_points_wpoly_df=intersect_points_wpoly@data
 
 per_ahn2 <- intersect_points_wpoly_df %>%
   group_by(bladnr) %>%
   summarise(nofobs = length(bladnr))
+
+# Create shapefile for area of interest selection within tile
+
+intersect_points_wpoly_circles <- gBuffer( intersect_points_wpoly, width=5000, byid=TRUE,capStyle="SQUARE")
