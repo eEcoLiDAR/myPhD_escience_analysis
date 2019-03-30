5b117f8985dad61f3ad86c0b26bb6cd1d0d9a82f
komazsofi <komazsofi@gmail.com>
2019-03-30 09:57:35 +0100

    data extraction script

---
+++ RV_MSc/ahn2_extract_areaofint.R
@@ -0,0 +1,26 @@
+"
+@author: Zsofia Koma, UvA
+Aim: Extract area of interest from AHN2 data
+"
+library("lidR")
+library("rgdal")
+
+# Set working dirctory
+workingdirectory="C:/Koma/Sync/_Amsterdam/08_coauthor_MScProjects/Reinier/datapreprocess/"
+setwd(workingdirectory)
+
+#Import csv
+bytransect_wcoord=read.csv(file="boundaries_pertransects.csv",header=TRUE,sep=",")
+
+ctg = catalog(workingdirectory)
+
+for (i in seq(from=14,to=nrow(bykmsquare_wcoord))){ 
+  print(bytransect_wcoord$Transect[i])
+  
+  subset = lasclipRectangle(ctg, bytransect_wcoord$xmin[i]-1000, bytransect_wcoord$ymin[i]-1000, bytransect_wcoord$xmax[i]+1000, bytransect_wcoord$ymax[i]+1000)
+  
+  if (subset@header@PHB[["Number of point records"]]>0) {
+    writeLAS(subset,paste("Transect_",bytransect_wcoord$Transect[i],"_x_",round(bytransect_wcoord$xmin[i]-1000,digit=0),"_y_",round(bytransect_wcoord$ymin[i]-1000,digit=0),".laz",sep=""))
+  }
+  
+}
