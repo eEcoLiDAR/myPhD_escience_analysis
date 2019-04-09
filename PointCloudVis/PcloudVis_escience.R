2518041873ca06d76f7f3451fdc9dd86536422aa
komazsofi <komazsofi@gmail.com>
2019-04-09 20:50:37 +0200

    small changes

--- PointCloudVis/PcloudVis_escience.R
+++ PointCloudVis/PcloudVis_escience.R
@@ -9,3 +9,6 @@ las=readLAS("g32hz1rect2.las")
 #Vis
 scatter3D(las@data$X, las@data$Y, las@data$Z, pch = 16,  theta = 25, phi = 25, bty="u",
           cex = 0.4,expand =0.3,colkey = FALSE,col.panel ="white",col.grid = "black")
+
+
+
