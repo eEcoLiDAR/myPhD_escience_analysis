f99a54fffb2a3b24ea0756b1e04695c4f113c575
komazsofi <komazsofi@gmail.com>
2019-04-09 09:03:11 +0200

    clean up

--- Paper1_inR_v2/Pipeline_ClassificationAnalysis.R
+++ Paper1_inR_v2/Pipeline_ClassificationAnalysis.R
@@ -37,100 +37,13 @@ names(featuretable_l2) <- c("C_puls","C_can","3S_curv","3S_lin","S_plan","3S_sph
 names(featuretable_l3) <- c("C_puls","C_can","3S_curv","3S_lin","S_plan","3S_sph","3S_ani","VV_sd","VV_var","VV_skew","VV_kurt","VV_cr","VV_vdr","VV_simp","VV_shan","HV_rough","HV_tpi","HV_tri",
                             "HV_sd","HV_var","H_max","H_mean","H_med","H_25p","H_75p","H_90p","layer")
 
-
-# RF
-forest_l1 <- randomForest(x=featuretable_l1[ ,c(1:26)], y=factor(featuretable_l1$layer),importance = TRUE,ntree = 100)
-forest_l2 <- randomForest(x=featuretable_l2[ ,c(1:26)], y=factor(featuretable_l2$layer),importance = TRUE,ntree = 100)
-forest_l3 <- randomForest(x=featuretable_l3[ ,c(1:26)], y=factor(featuretable_l3$layer),importance = TRUE,ntree = 100)
-
-#load("forest_l1.RData")
-#load("forest_l2.RData")
-#load("forest_l3.RData")
-
-# Fig.4. : Feature Importance
-
-importance_frame_l1=Analysis_FeatureImportance(forest_l1)
-importance_frame_l2=Analysis_FeatureImportance(forest_l2)
-importance_frame_l3=Analysis_FeatureImportance(forest_l3)
-
-importance_frame_l1_m=add_varclass(importance_frame_l1)
-importance_frame_l2_m=add_varclass(importance_frame_l2)
-importance_frame_l3_m=add_varclass(importance_frame_l3)
-
-data_l1 <- importance_frame_l1_m[importance_frame_l1_m$no_of_trees > 0, ]
-data_for_labels_l1 <- importance_frame_l1_m[importance_frame_l1_m$variable %in%
-                                              important_variables(importance_frame_l1_m, k = 7,
-                                                                  measures = c("norm_accuracy_decrease","norm_gini_decrease")), ]
-
-data_l2 <- importance_frame_l2_m[importance_frame_l2_m$no_of_trees > 0, ]
-data_for_labels_l2 <- importance_frame_l2_m[importance_frame_l2_m$variable %in%
-                                              important_variables(importance_frame_l2_m, k = 5,
-                                                                  measures = c("norm_accuracy_decrease","norm_gini_decrease")), ]
-
-data_l3 <- importance_frame_l3_m[importance_frame_l3_m$no_of_trees > 0, ]
-data_for_labels_l3 <- importance_frame_l3_m[importance_frame_l3_m$variable %in%
-                                              important_variables(importance_frame_l3_m, k = 5,
-                                                                  measures = c("norm_accuracy_decrease","norm_gini_decrease")), ]
-
-p0=ggplot(data_l1, aes_string(x = "norm_accuracy_decrease", y = "norm_gini_decrease")) +
-  geom_point(aes(color=factor(data_l1$varclass)),size=5,show.legend = TRUE) + geom_point(data = data_l1, aes(color=factor(data_l1$varclass)),size=5,show.legend = FALSE) +
-  geom_text_repel(data = data_for_labels_l1, aes(label = variable,size=30), show.legend = FALSE, size=5,box.padding=0.5) +
-  scale_color_manual(values = c("1" = "deeppink", "2" = "chocolate4", "3" = "blueviolet","4"="darkolivegreen3", "5"="blue"),name="Feature class",labels=c("Coverage (C_*)","3D shape (3S_*)", "Vertical variability (VV_*)","Height (H_*)","Horizontal variability (HV_*)")) +
-  xlab("Normalized accuracy decrease") + ylab("Normalized gini decrease") +
-  theme_bw(base_size = 20) + ylim(0, 1) + xlim(0, 1) + theme(legend.position="right")
-
-p1=ggplot(data_l1, aes_string(x = "norm_accuracy_decrease", y = "norm_gini_decrease")) +
-  geom_point(aes(color=factor(data_l1$varclass)),size=5,show.legend = FALSE) + geom_point(data = data_l1, aes(color=factor(data_l1$varclass)),size=5,show.legend = FALSE) +
-  geom_text_repel(data = data_for_labels_l1, aes(label = variable,size=15), show.legend = FALSE, size=5,box.padding=0.8,direction="x") +
-  scale_color_manual(values = c("1" = "deeppink", "2" = "chocolate4", "3" = "blueviolet","4"="darkolivegreen3", "5"="blue"),name="Feature class",labels=c("Coverage","3D shape", "Vertical variability","Height","Horizontal variability")) +
-  xlab("Normalized accuracy decrease") + ylab("Normalized gini decrease") +
-  theme_bw(base_size = 17) + ylim(0, 1) + xlim(0, 1)
-
-p2=ggplot(data_l2, aes_string(x = "norm_accuracy_decrease", y = "norm_gini_decrease")) +
-  geom_point(aes(color=factor(data_l2$varclass)),size=5,show.legend = FALSE) + geom_point(data = data_l2, aes(color=factor(data_l2$varclass)),size=5,show.legend = FALSE) +
-  geom_text_repel(data = data_for_labels_l2, aes(label = variable,size=15), show.legend = FALSE, size=5,box.padding=0.8,direction="x") +
-  scale_color_manual(values = c("1" = "deeppink", "2" = "chocolate4", "3" = "blueviolet","4"="darkolivegreen3", "5"="blue"),name="Feature class",labels=c("Coverage","3D shape", "Vertical variability","Height","Horizontal variability")) +
-  xlab("Normalized accuracy decrease") + ylab("Normalized gini decrease") +
-  theme_bw(base_size = 17) + ylim(0, 1) + xlim(0, 1)
-
-p3=ggplot(data_l3, aes_string(x = "norm_accuracy_decrease", y = "norm_gini_decrease")) +
-  geom_point(aes(color=factor(data_l3$varclass)),size=5,show.legend = FALSE) + geom_point(data = data_l3, aes(color=factor(data_l3$varclass)),size=5,show.legend = FALSE) +
-  geom_text_repel(data = data_for_labels_l3, aes(label = variable,size=15), show.legend = FALSE, size=5,box.padding=0.8,direction="x") +
-  scale_color_manual(values = c("1" = "deeppink", "2" = "chocolate4", "3" = "blueviolet","4"="darkolivegreen3", "5"="blue"),name="Feature class",labels=c("Coverage","3D shape", "Vertical variability","Height","Horizontal variability")) +
-  xlab("Normalized accuracy decrease") + ylab("Normalized gini decrease") +
-  theme_bw(base_size = 17) + ylim(0, 1) + xlim(0, 1)
-
-get_legend<-function(myggplot){
-  tmp <- ggplot_gtable(ggplot_build(myggplot))
-  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend <- tmp$grobs[[leg]]
-  return(legend)
-}
-
-legend <- get_legend(p0)
-
-grid.arrange(
-  p1,
-  p2,
-  p3,
-  legend,
-  ncol=4,
-  nrow=1,
-  layout_matrix=rbind(c(1,2,3,4)),
-  widths = c(2.5, 2.5,2.5,2.5)
-)
-
-# Fig3: RFE
+# RFE
 
 # level 1
-<<<<<<< HEAD
 control <- rfeControl(functions=rfFuncs, method="cv", number=50,returnResamp = "all")
 set.seed(50)
 rfe_l1 <- rfe(featuretable_l1[,1:26], factor(featuretable_l1$layer), rfeControl=control,sizes=c(1:26),ntree=100)
-=======
-control <- rfeControl(functions=rfFuncs, method="cv", number=50,saveDetails = TRUE)
-rfe_l1 <- rfe(featuretable_l1[,1:26], factor(featuretable_l1$layer), rfeControl=control,sizes=c(1:26))
->>>>>>> 37eef20a51b4b7152e6d26b920e827a54c21a8a2
+
 
 # level 2
 control <- rfeControl(functions=rfFuncs, method="cv", number=50,returnResamp = "all")
@@ -157,31 +70,7 @@ grid.arrange(
   nrow = 1
 )
 
-fig4=grid.arrange(
-  p7,
-  p1,
-  p8,
-  p2,
-  p9,
-  p3,
-  legend,
-  ncol=3,
-  nrow=3,
-  layout_matrix=rbind(c(1,2,8), c(3,4,8),c(5,6,8)),
-  widths = c(1.25,2,0.8),
-  heights = c(2,2,2)
-)
-
-ggsave("Fig4.png",plot = fig4,width = 16, height = 18)
-
 # Export
-save(forest_l1,file = "forest_l1.RData")
-save(forest_l2,file = "forest_l2.RData")
-save(forest_l3,file = "forest_l3.RData")
-
-write.csv(importance_frame_l1_m, file = "importance_frame_l1.csv",row.names=FALSE)
-write.csv(importance_frame_l2_m, file = "importance_frame_l2.csv",row.names=FALSE)
-write.csv(importance_frame_l3_m, file = "importance_frame_l3.csv",row.names=FALSE)
 
 save(rfe_l1,file = "rfe_l1.RData")
 save(rfe_l2,file = "rfe_l2.RData")
