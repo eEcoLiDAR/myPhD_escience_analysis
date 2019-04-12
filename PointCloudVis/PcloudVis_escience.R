36c82a5db07fde57d9411e0a0d3c15ff643414c8
Zsofia Koma <komazsofi@gmail.com>
2019-04-12 15:24:00 +0200

    mod

--- PointCloudVis/PcloudVis_escience.R
+++ PointCloudVis/PcloudVis_escience.R
@@ -1,14 +1,37 @@
 library(lidR)
 library(plot3D)
 
-setwd("C:/Koma/Sync/_Amsterdam/07_Paper_escience_software/")
+#setwd("C:/Koma/Sync/_Amsterdam/07_Paper_escience_software/")
+setwd("D:/Sync/_Amsterdam/07_Paper_escience_software/")
 
 #Import
 las=readLAS("g32hz1rect2.las")
 
+las.classified <- lasground(las, csf())
+
 #Vis
-scatter3D(las@data$X, las@data$Y, las@data$Z, pch = 16,  theta = 25, phi = 25, bty="u",
-          cex = 0.4,expand =0.3,colkey = FALSE,col.panel ="white",col.grid = "black")
+scatter3D(las.classified@data$X, las.classified@data$Y, las.classified@data$Z, pch = 16,  theta = 25, phi = 10, bty="u",
+          cex = 0.4,expand =0.35,colkey = FALSE,col.panel ="white",col.grid = "black", colvar=las.classified@data$Classification,col=ramp.col(c("darkgreen","brown")))
+
+#Raster
+
+hsd <- grid_metrics(las.classified, sd(Z), 1)
+plot(hsd,col=jet.col())
+
+#Voxel
+myMetrics = function(i)
+   {
+      ret = list(
+      npoints = length(i),
+      imean   = mean(i))
+
+    return(ret)
+}
 
 
 
+
+
+#Sphere
+scatter3D(las.classified@data$X, las.classified@data$Y, las.classified@data$Z, pch = 16,  theta = 25, phi = 10, bty="u",
+          cex = 0.4,expand =0.35,colkey = FALSE,col.panel ="white",col.grid = "black",col=jet.col(),ticktype = "detailed")
