b0ccc20a8bae5c08e485513d0359215ebeedff0a
komazsofi <komazsofi@gmail.com>
2019-04-17 17:12:31 +0200

    adapt classifier

--- Paper1_inR_v2/Pipeline_ApplyClassification.R
+++ Paper1_inR_v2/Pipeline_ApplyClassification.R
@@ -2,12 +2,14 @@
 @author: Zsofia Koma, UvA
 Aim: Classify the data
 "
+library(caret)
+library(randomForest)
 library(raster)
 library(rgdal)
 source("D:/Koma/GitHub/myPhD_escience_analysis/Paper1_inR_v2/Function_Classification.R")
 
 # Set working dirctory
-workingdirectory="D:/Koma/Paper1_v2/Run4_2019April/"
+workingdirectory="D:/Koma/Paper1_v2/Results_17April/"
 setwd(workingdirectory)
 
 #Skipped: selection of training data from polygon + buffer and recategorization
@@ -21,9 +23,9 @@ lidarmetrics_forl23=stack("lidarmetrics_l2l3_masked_wgr.grd")
 names(lidarmetrics_forl23) <- c("C_puls","C_can","S_curv","S_lin","S_plan","S_sph","S_ani","VV_sd","VV_var","VV_skew","VV_kurt","VV_cr","VV_vdr","VV_simp","VV_shan","HV_rough","HV_tpi","HV_tri",
                                "HV_sd","HV_var","H_max","H_mean","H_med","H_25p","H_75p","H_90p")
 
-featuretable_l1=read.csv("featuretable_level1_b2o5_test.csv")
-featuretable_l2=read.csv("featuretable_level2_b2o5_test.csv")
-featuretable_l3=read.csv("featuretable_level3_b2o5_test.csv")
+featuretable_l1=read.csv("featuretable_level1_b2o5.csv")
+featuretable_l2=read.csv("featuretable_level2_b2o5.csv")
+featuretable_l3=read.csv("featuretable_level3_b2o5.csv")
 
 names(featuretable_l1) <- c("C_puls","C_can","S_curv","S_lin","S_plan","S_sph","S_ani","VV_sd","VV_var","VV_skew","VV_kurt","VV_cr","VV_vdr","VV_simp","VV_shan","HV_rough","HV_tpi","HV_tri",
                             "HV_sd","HV_var","H_max","H_mean","H_med","H_25p","H_75p","H_90p","layer")
@@ -34,19 +36,17 @@ names(featuretable_l2) <- c("C_puls","C_can","S_curv","S_lin","S_plan","S_sph","
 names(featuretable_l3) <- c("C_puls","C_can","S_curv","S_lin","S_plan","S_sph","S_ani","VV_sd","VV_var","VV_skew","VV_kurt","VV_cr","VV_vdr","VV_simp","VV_shan","HV_rough","HV_tpi","HV_tri",
                             "HV_sd","HV_var","H_max","H_mean","H_med","H_25p","H_75p","H_90p","layer")
 
-load("rfe_l1.RData")
-load("rfe_l2.RData")
-load("rfe_l3.RData")
-
 ### Level1 ###
 
 # Classification
 
-Pred_l1=predict(lidarmetrics_forl1, model=rfe_l1$fit, na.rm=TRUE)
-writeRaster(Pred_l1, filename="classified_level1.tif", format="GTiff",overwrite=TRUE)
+lidarmetrics_forl1_sel=lidarmetrics_forl1[[c("HV_var","HV_sd","HV_rough","S_plan","C_can")]]
+featuretable_l1_sel=featuretable_l1[,c("HV_var","HV_sd","HV_rough","S_plan","C_can","layer")]
 
-level1="level1"
-Classification_werrorass(featuretable_l1,level1,rfe_l1$fit)
+modelFit_1 <- randomForest(factor(layer)~.,data=featuretable_l1_sel,ntree=100)
+
+Pred_l1 <- predict(lidarmetrics_forl1_sel, model=modelFit_1, na.rm=TRUE)
+writeRaster(Pred_l1, filename="classified_level1.tif", format="GTiff",overwrite=TRUE)
 
 # Mask 
 formask <- setValues(raster(lidarmetrics_forl23), NA)
@@ -59,11 +59,13 @@ lidarmetrics_masked1 <- mask(lidarmetrics_forl23,formask)
 
 # Classification
 
-Pred_l2=predict(lidarmetrics_masked1, model=rfe_l2$fit, na.rm=TRUE)
-writeRaster(Pred_l2, filename="classified_level2.tif", format="GTiff",overwrite=TRUE)
+lidarmetrics_masked1_sel=lidarmetrics_masked1[[c("H_max","H_75p","H_90p","HV_rough","HV_sd","VV_shan","H_mean","HV_var","VV_sd","VV_var")]]
+featuretable_l2_sel=featuretable_l2[,c("H_max","H_75p","H_90p","HV_rough","HV_sd","VV_shan","H_mean","HV_var","VV_sd","VV_var","layer")]
 
-level1="level2"
-Classification_werrorass(featuretable_l2,level1,rfe_l2$fit)
+modelFit_2 <- randomForest(factor(layer)~.,data=featuretable_l2_sel,ntree=100)
+
+Pred_l2 <- predict(lidarmetrics_masked1_sel, model=modelFit_2, na.rm=TRUE)
+writeRaster(Pred_l2, filename="classified_level2.tif", format="GTiff",overwrite=TRUE)
 
 # Mask 
 formask2 <- setValues(raster(lidarmetrics_forl23), NA)
@@ -77,8 +79,10 @@ lidarmetrics_masked2 <- mask(lidarmetrics_masked1,formask2)
 
 # Classification
 
-Pred_l3=predict(lidarmetrics_masked2, model=rfe_l3$fit, na.rm=TRUE)
-writeRaster(Pred_l3, filename="classified_level3.tif", format="GTiff",overwrite=TRUE)
+lidarmetrics_masked2_sel=lidarmetrics_masked2[[c("H_90p","H_max","C_puls","HV_sd","C_can","HV_var","H_25p","H_75p","HV_rough","H_med")]]
+featuretable_l3_sel=featuretable_l3[,c("H_90p","H_max","C_puls","HV_sd","C_can","HV_var","H_25p","H_75p","HV_rough","H_med","layer")]
 
-level1="level3"
-Classification_werrorass(featuretable_l3,level1,rfe_l3$fit)
+modelFit_3 <- randomForest(factor(layer)~.,data=featuretable_l3_sel,ntree=100)
+
+Pred_l3 <- predict(lidarmetrics_masked2_sel, model=modelFit_3, na.rm=TRUE)
+writeRaster(Pred_l3, filename="classified_level3.tif", format="GTiff",overwrite=TRUE)
