e3d553f5fa55b33c67f7385e30cae0158234f1e5
komazsofi <komazsofi@gmail.com>
2019-06-02 16:15:28 +0200

    modify for running on row numbers

--- RV_MSc/ahn3_extract_intersection.R
+++ RV_MSc/ahn3_extract_intersection.R
@@ -17,11 +17,11 @@ areaofint=readOGR(dsn=areaofintfile)
 # Extract
 ctg = catalog(workingdirectory)
 
-for (i in areaofint@data[["Transect"]]){ 
-  print(i) 
-  subset = lasclip(ctg, areaofint[areaofint$Transect==i,])
+for (i in seq(44,length(areaofint$Transect),1)){ 
+  print(areaofint$Transect[i]) 
+  subset = lasclip(ctg, areaofint[areaofint$Transect==areaofint$Transect[i],])
   
   if (subset@header@PHB[["Number of point records"]]>0) {
-    writeLAS(subset,paste("C:/Reinier_output/Transect_",i,".laz",sep=""))
+    writeLAS(subset,paste("C:/Reinier_output/Transect_",areaofint$Transect[i],".laz",sep=""))
   }
 }
