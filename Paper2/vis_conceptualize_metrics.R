7729baee200c973cc815e7e907ab72cbd00edc30
Zsofia Koma <komazsofi@gmail.com>
2019-06-18 10:28:05 +0200

    add ggplot for vis rasters

--- Paper2/vis_conceptualize_metrics.R
+++ Paper2/vis_conceptualize_metrics.R
@@ -6,6 +6,10 @@ library("lidR")
 library("rgdal")
 library("snow")
 
+library("ggplot2")
+library("viridis")
+library("ggthemes")
+
 # Set working dirctory
 workingdirectory="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/DataProcess_Paper2_1/"
 #workingdirectory="D:/Koma/SelectedWetlands/"
@@ -29,3 +33,16 @@ sd_dsm=clusterR(vegheight_10, focal, args=list(w=matrix(1,5,5), fun=sd, pad=TRUE
 endCluster()
 
 plot(sd_dsm)
+
+# Visulaize with ggplot
+vegheight_10_spdf <- as(vegheight_10, "SpatialPixelsDataFrame")
+vegheight_10_df <- as.data.frame(vegheight_10_spdf)
+colnames(vegheight_10_df) <- c("value", "x", "y")
+
+ggplot() +  
+  geom_tile(data=vegheight_10_df, aes(x=x, y=y, fill=value), alpha=0.8) + 
+  scale_fill_viridis() +
+  coord_equal() +
+  theme_map() +
+  theme(legend.position="bottom") +
+  theme(legend.key.width=unit(2, "cm"))
