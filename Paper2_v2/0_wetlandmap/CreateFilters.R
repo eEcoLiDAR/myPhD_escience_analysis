49db97020eca2a467771fd2e3a91d774a58761b2
Zsofia Koma <komazsofi@gmail.com>
2019-07-10 14:58:12 +0200

    updates

--- Paper2_v2/0_wetlandmap/CreateFilters.R
+++ Paper2_v2/0_wetlandmap/CreateFilters.R
@@ -11,9 +11,11 @@ workingdirectory="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2
 setwd(workingdirectory)
 
 lidarfile=""
+landcoverfile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/_Input_Datasets/filters/3_LGN7/Data/LGN7.tif"
 
 # Import
 lidar_forfilter=stack(lidarfile)
+landcover=stack(landcoverfile)
 
 # Create vegetation filter
 mask_lidar <- setValues(raster(lidar_forfilter), NA)
@@ -22,3 +24,11 @@ mask_lidar[lidar_forfilter>0.5] <- 1
 plot(mask_lidar, col="dark green", legend = FALSE)
 
 # LGN 7 filter species specific
+wetland_mask <- setValues(raster(landcover), NA)
+wetland_mask[landcover==16 | landcover==17 | landcover==30 | landcover==41 | landcover==42 | landcover==43 | landcover==45] <- 1
+
+plot(wetland_mask, col="dark green", legend = FALSE)
+
+writeRaster(wetland_mask, "wetland_mask.grd",overwrite=TRUE)
+
+#wetland_mask_poly <- rasterToPolygons(wetland_mask, fun=function(x){x==1})
--- Paper2_v2/1_bird_data_preprocess/Process_PresOnly.R
+++ Paper2_v2/1_bird_data_preprocess/Process_PresOnly.R
@@ -15,14 +15,15 @@ setwd(workingdirectory)
 birdfile="avimap_observations_reedland_birds.csv" # using the one which contains more observation also outside of NL
 
 ahn3="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/DataProcess_Paper2_1/lidar/ahn3.shp"
-
 ahn3_actimefile="ahn3_measuretime.shp"
+sovonwetlandfile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/_Input_Datasets/filters/Sovon/Moeras_gaten_gevuld_100m.shp"
 
 #Import
 birds=read.csv(birdfile,sep=";")
 
 ahn3_poly = readOGR(dsn=ahn3)
 ahn3_actime = readOGR(dsn=ahn3_actimefile)
+sovonwetland = readOGR(dsn=sovonwetlandfile)
 
 # Select only one species
 species=unique(birds$species)
@@ -36,15 +37,13 @@ SaviW=birds[birds$species=="Snor" & birds$year>"2013",]
 
 # Create shp
 GreedW_shp=CreateShape(GreedW)
-raster::shapefile(GreedW_shp, "GreedW_avi.shp",overwrite=TRUE)
-
 ReedW_shp=CreateShape(ReedW)
-raster::shapefile(ReedW_shp, "ReedW_avi.shp",overwrite=TRUE)
-
 BReed_shp=CreateShape(BReed)
-raster::shapefile(BReed_shp, "BReed_avi.shp",overwrite=TRUE)
-
 SaviW_shp=CreateShape(SaviW)
+
+raster::shapefile(GreedW_shp, "GreedW_avi.shp",overwrite=TRUE)
+raster::shapefile(ReedW_shp, "ReedW_avi.shp",overwrite=TRUE)
+raster::shapefile(BReed_shp, "BReed_avi.shp",overwrite=TRUE)
 raster::shapefile(SaviW_shp, "SaviW_avi.shp",overwrite=TRUE)
 
 # Intersect with AHN3 
@@ -90,7 +89,7 @@ GreedW_ahn3ac_df=GreedW_ahn3ac@data
 
 GreedW_ahn3ac_df$acq_sync <- GreedW_ahn3ac_df$year == GreedW_ahn3ac_df$Jaar
 
-GreedW_withac_shp=CreateShape(GreedW_ahn3ac_df)
+GreedW_withac_shp=CreateShape(GreedW_ahn3ac_df[GreedW_ahn3ac_df$acq_sync==TRUE,])
 raster::shapefile(GreedW_withac_shp, "GreedW_avi_wacq.shp",overwrite=TRUE)
 
 ReedW_ahn3ac=raster::intersect(ReedW_shp,ahn3_actime)
@@ -98,7 +97,7 @@ ReedW_ahn3ac_df=ReedW_ahn3ac@data
 
 ReedW_ahn3ac_df$acq_sync <- ReedW_ahn3ac_df$year == ReedW_ahn3ac_df$Jaar
 
-ReedW_withac_shp=CreateShape(ReedW_ahn3ac_df)
+ReedW_withac_shp=CreateShape(ReedW_ahn3ac_df[ReedW_ahn3ac_df$acq_sync==TRUE,])
 raster::shapefile(ReedW_withac_shp, "ReedW_avi_wacq.shp",overwrite=TRUE)
 
 BReed_ahn3ac=raster::intersect(BReed_shp,ahn3_actime)
@@ -106,7 +105,7 @@ BReed_ahn3ac_df=BReed_ahn3ac@data
 
 BReed_ahn3ac_df$acq_sync <- BReed_ahn3ac_df$year == BReed_ahn3ac_df$Jaar
 
-BReed_withac_shp=CreateShape(BReed_ahn3ac_df)
+BReed_withac_shp=CreateShape(BReed_ahn3ac_df[BReed_ahn3ac_df$acq_sync==TRUE,])
 raster::shapefile(BReed_withac_shp, "BReed_avi_wacq.shp",overwrite=TRUE)
 
 SaviW_ahn3ac=raster::intersect(SaviW_shp,ahn3_actime)
@@ -114,8 +113,49 @@ SaviW_ahn3ac_df=SaviW_ahn3ac@data
 
 SaviW_ahn3ac_df$acq_sync <- SaviW_ahn3ac_df$year == SaviW_ahn3ac_df$Jaar
 
-SaviW_withac_shp=CreateShape(SaviW_ahn3ac_df)
+SaviW_withac_shp=CreateShape(SaviW_ahn3ac_df[SaviW_ahn3ac_df$acq_sync==TRUE,])
 raster::shapefile(SaviW_withac_shp, "SaviW_avi_wacq.shp",overwrite=TRUE)
+
+#Apply sovon wetland filter
+
+GreedW_sovwet=raster::intersect(GreedW_shp,sovonwetland)
+GreedW_sovwet_df=GreedW_sovwet@data
+
+ReedW_sovwet=raster::intersect(ReedW_shp,sovonwetland)
+ReedW_sovwet_df=ReedW_sovwet@data
+
+BReed_sovwet=raster::intersect(BReed_shp,sovonwetland)
+BReed_sovwet_df=BReed_sovwet@data
+
+SaviW_sovwet=raster::intersect(SaviW_shp,sovonwetland)
+SaviW_sovwet_df=SaviW_sovwet@data
+
+raster::shapefile(SaviW_sovwet, "SaviW_sovwet.shp",overwrite=TRUE)
+
+# With time filter
+GreedW_sovwet_ahn3ac=raster::intersect(GreedW_withac_shp,sovonwetland)
+GreedW_sovwet_ahn3ac_df=GreedW_sovwet_ahn3ac@data
+
+raster::shapefile(GreedW_sovwet_ahn3ac, "GreedW_sovwet_ahn3ac.shp",overwrite=TRUE)
+
+ReedW_sovwet_ahn3ac=raster::intersect(ReedW_withac_shp,sovonwetland)
+ReedW_sovwet_ahn3ac_df=ReedW_sovwet_ahn3ac@data
+
+raster::shapefile(ReedW_sovwet_ahn3ac, "ReedW_sovwet_ahn3ac.shp",overwrite=TRUE)
+
+BReed_sovwet_ahn3ac=raster::intersect(BReed_withac_shp,sovonwetland)
+BReed_sovwet_ahn3ac_df=BReed_sovwet_ahn3ac@data
+
+raster::shapefile(BReed_sovwet_ahn3ac, "BReed_sovwet_ahn3ac.shp",overwrite=TRUE)
+
+SaviW_sovwet_ahn3ac=raster::intersect(SaviW_withac_shp,sovonwetland)
+SaviW_sovwet_ahn3ac_df=SaviW_sovwet_ahn3ac@data
+
+raster::shapefile(SaviW_sovwet_ahn3ac, "SaviW_sovwet_ahn3ac.shp",overwrite=TRUE)
+
+# LGN7 filter
+
+# LGN7 add landcover classes
 
 # Stat. report about nof presence
 
