2d2f17dc233dcb11470c3d57292b3192f7d1ed72
Zsofia Koma <komazsofi@gmail.com>
2019-07-10 16:13:32 +0200

    add lgn7

--- Paper2_v2/1_bird_data_preprocess/Process_PresOnly.R
+++ Paper2_v2/1_bird_data_preprocess/Process_PresOnly.R
@@ -17,6 +17,7 @@ birdfile="avimap_observations_reedland_birds.csv" # using the one which contains
 ahn3="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/DataProcess_Paper2_1/lidar/ahn3.shp"
 ahn3_actimefile="ahn3_measuretime.shp"
 sovonwetlandfile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/_Input_Datasets/filters/Sovon/Moeras_gaten_gevuld_100m.shp"
+landcoverfile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/_Input_Datasets/filters/3_LGN7/Data/LGN7.tif"
 
 #Import
 birds=read.csv(birdfile,sep=";")
@@ -24,6 +25,9 @@ birds=read.csv(birdfile,sep=";")
 ahn3_poly = readOGR(dsn=ahn3)
 ahn3_actime = readOGR(dsn=ahn3_actimefile)
 sovonwetland = readOGR(dsn=sovonwetlandfile)
+
+landcover=stack(landcoverfile)
+proj4string(landcover) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 # Select only one species
 species=unique(birds$species)
@@ -153,9 +157,55 @@ SaviW_sovwet_ahn3ac_df=SaviW_sovwet_ahn3ac@data
 
 raster::shapefile(SaviW_sovwet_ahn3ac, "SaviW_sovwet_ahn3ac.shp",overwrite=TRUE)
 
-# LGN7 filter
-
 # LGN7 add landcover classes
+
+GreedW_lgn7_ahn3ac_int=extract(landcover,GreedW_withac_shp)
+GreedW_withac_shp$landcover_lgn7=GreedW_lgn7_ahn3ac_int[,1]
+
+raster::shapefile(GreedW_withac_shp, "GreedW_avi_wacq_lgn7.shp",overwrite=TRUE)
+
+GreedW_withac_lgn7=GreedW_withac_shp@data[(GreedW_withac_shp@data$landcover_lgn7==16 | GreedW_withac_shp@data$landcover_lgn7==17 | GreedW_withac_shp@data$landcover_lgn7==30
+                                           | GreedW_withac_shp@data$landcover_lgn7==41 | GreedW_withac_shp@data$landcover_lgn7==42 | GreedW_withac_shp@data$landcover_lgn7==43 
+                                           | GreedW_withac_shp@data$landcover_lgn7==45),]
+
+GreedW_withac_lgn7_shp=CreateShape(GreedW_withac_lgn7)
+raster::shapefile(GreedW_withac_lgn7_shp, "GreedW_withac_flgn7.shp",overwrite=TRUE)
+
+ReedW_lgn7_ahn3ac_int=extract(landcover,ReedW_withac_shp)
+ReedW_withac_shp$landcover_lgn7=ReedW_lgn7_ahn3ac_int[,1]
+
+raster::shapefile(ReedW_withac_shp, "ReedW_avi_wacq_lgn7.shp",overwrite=TRUE)
+
+ReedW_withac_lgn7=ReedW_withac_shp@data[(ReedW_withac_shp@data$landcover_lgn7==16 | ReedW_withac_shp@data$landcover_lgn7==17 | ReedW_withac_shp@data$landcover_lgn7==30
+                                           | ReedW_withac_shp@data$landcover_lgn7==41 | ReedW_withac_shp@data$landcover_lgn7==42 | ReedW_withac_shp@data$landcover_lgn7==43 
+                                           | ReedW_withac_shp@data$landcover_lgn7==45),]
+
+ReedW_withac_lgn7_shp=CreateShape(ReedW_withac_lgn7)
+raster::shapefile(ReedW_withac_lgn7_shp, "ReedW_withac_flgn7.shp",overwrite=TRUE)
+
+BReed_lgn7_ahn3ac_int=extract(landcover,BReed_withac_shp)
+BReed_withac_shp$landcover_lgn7=BReed_lgn7_ahn3ac_int[,1]
+
+raster::shapefile(BReed_withac_shp, "BReed_avi_wacq_lgn7.shp",overwrite=TRUE)
+
+BReed_withac_lgn7=BReed_withac_shp@data[(BReed_withac_shp@data$landcover_lgn7==16 | BReed_withac_shp@data$landcover_lgn7==17 | BReed_withac_shp@data$landcover_lgn7==30
+                                           | BReed_withac_shp@data$landcover_lgn7==41 | BReed_withac_shp@data$landcover_lgn7==42 | BReed_withac_shp@data$landcover_lgn7==43 
+                                           | BReed_withac_shp@data$landcover_lgn7==45),]
+
+BReed_withac_lgn7_shp=CreateShape(BReed_withac_lgn7)
+raster::shapefile(BReed_withac_lgn7_shp, "BReed_withac_flgn7.shp",overwrite=TRUE)
+
+SaviW_lgn7_ahn3ac_int=extract(landcover,SaviW_withac_shp)
+SaviW_withac_shp$landcover_lgn7=SaviW_lgn7_ahn3ac_int[,1]
+
+raster::shapefile(SaviW_withac_shp, "SaviW_avi_wacq_lgn7.shp",overwrite=TRUE)
+
+SaviW_withac_lgn7=SaviW_withac_shp@data[(SaviW_withac_shp@data$landcover_lgn7==16 | SaviW_withac_shp@data$landcover_lgn7==17 | SaviW_withac_shp@data$landcover_lgn7==30
+                                           | SaviW_withac_shp@data$landcover_lgn7==41 | SaviW_withac_shp@data$landcover_lgn7==42 | SaviW_withac_shp@data$landcover_lgn7==43 
+                                           | SaviW_withac_shp@data$landcover_lgn7==45),]
+
+SaviW_withac_lgn7_shp=CreateShape(SaviW_withac_lgn7)
+raster::shapefile(SaviW_withac_lgn7_shp, "SaviW_withac_flgn7.shp",overwrite=TRUE)
 
 # Stat. report about nof presence
 
