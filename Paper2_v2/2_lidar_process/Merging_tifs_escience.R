6bdde62b380d070c0919bf6c09bcee87ea1c6b93
Zsofia Koma <komazsofi@gmail.com>
2019-07-11 16:53:44 +0200

    flip

--- Paper2_v2/2_lidar_process/Merging_tifs_escience.R
+++ Paper2_v2/2_lidar_process/Merging_tifs_escience.R
@@ -11,7 +11,7 @@ library(dplyr)
 workingdirectory="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2_2/tifs/"
 setwd(workingdirectory)
 
-req_ahn3tiles_esciencefile="req_ahn3tiles_escience.shp"
+req_ahn3tiles_esciencefile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2_2/req_ahn3tiles_escience.shp"
 ahn3_actimefile="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/_Input_Datasets/lidar/ahn3_measuretime.shp"
 
 # Import
@@ -24,8 +24,30 @@ req_ahn3tiles_escience_acqtime <- req_ahn3tiles_escience_acqtime[,-(10:11)]
 
 raster::shapefile(req_ahn3tiles_escience_acqtime, "req_ahn3tiles_escience_acqtime.shp",overwrite=TRUE)
 
-#Merging
-f <- list.files(path = "D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2_2/tifs/", pattern = ".tif$", full.names = TRUE)
-rl <- lapply(f, raster)
+req_ahn3tiles_escience_acqtime$listname=paste(req_ahn3tiles_escience_acqtime$name,".tif",sep="")
+sel_2014tifs=req_ahn3tiles_escience_acqtime[req_ahn3tiles_escience_acqtime$Jaar==2014,]
 
-mergedtif=do.call(merge, c(rl, tolerance = 1))
+write.table(sel_2014tifs$listname, file = "for2014_mosaic.txt", append = FALSE, quote = FALSE, sep = "", 
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE, 
+            col.names = FALSE, qmethod = c("escape", "double"))
+
+sel_2015tifs=req_ahn3tiles_escience_acqtime[req_ahn3tiles_escience_acqtime$Jaar==2015,]
+
+write.table(sel_2015tifs$listname, file = "for2015_mosaic.txt", append = FALSE, quote = FALSE, sep = "", 
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE, 
+            col.names = FALSE, qmethod = c("escape", "double"))
+
+#Flipping
+workingdirectory="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2_2/tifs/lidar_2015/"
+setwd(workingdirectory)
+
+tif_file.names <- dir("D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Dataprocess_Paper2_2/tifs/lidar_2015/", pattern =".tif")
+
+for (i in 1:length(tif_file.names)) {
+  
+  lidar_data=stack(tif_file.names[i])
+  lidar_data=flip(lidar_data,direction = 'y')
+  
+  writeRaster(lidar_data,paste(tif_file.names[i],"_flip.tif",sep=""))
+  
+}
