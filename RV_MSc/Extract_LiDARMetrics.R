e719371a8abbf2495e139ecb1b1d5efdb38bec24
Zsofia Koma <komazsofi@gmail.com>
2019-07-16 15:52:58 +0200

    add percentiles features

--- RV_MSc/Extract_LiDARMetrics.R
+++ RV_MSc/Extract_LiDARMetrics.R
@@ -22,7 +22,10 @@ butterflysp_df_gr <- butterflysp_df %>%
   summarise(nofobs = length(Transect))
 
 # Direct point cloud based metrics
-Transect=c(123,362,698,1244)
+Transect=c(123,362)
+dpcloudfea_exp_df <- data.frame(matrix(ncol = 3, nrow = 0))
+x <- c("Transect", "Transect_ID", "H_mean_25m")
+colnames(dpcloudfea_exp_df) <- x
 
 for (i in Transect) {
   print(i)
@@ -35,11 +38,19 @@ for (i in Transect) {
     
     las_clip=lasclipCircle(las,butterflysp_df_sel$x[j],butterflysp_df_sel$y[j],25)
     
-    las_vegetation=lasfilter(las_clip, Classification == 1L)
+    las_norm=lasnormalize(las_clip, knnidw(k=10,p=2))
     
-    plot(las_vegetation)
+    nofret_pheightlay_b02=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<0.2)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_02_05=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<0.5 & las_norm@data$Z>0.2)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_05_1=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<1 & las_norm@data$Z>0.5)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_1_2=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<2 & las_norm@data$Z>1)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_2_5=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<5 & las_norm@data$Z>2)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_5_10=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<10 & las_norm@data$Z>5)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_10_20=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z<20 & las_norm@data$Z>10)])/length(las_norm@data$Z))*100
+    nofret_pheightlay_a20=(nrow(las_norm@data[(las_norm@data$Classification==1L & las_norm@data$Z>20)])/length(las_norm@data$Z))*100
     
-    print(mean(las_vegetation@data$Z))
+    newline <- data.frame(t(c(Transect=i,Transect_ID=paste(butterflysp_df_sel$Tr_sec[j],sep=""),H_mean_25m = mean(las_vegetation@data$Z))))
+    dpcloudfea_exp_df <- rbind(dpcloudfea_exp_df, newline)
     
   }
   
