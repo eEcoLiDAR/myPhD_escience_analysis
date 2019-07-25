b561c3d5ecdbeb4e4aa93b6a57e36b95d23cef72
komazsofi <komazsofi@gmail.com>
2019-07-25 16:24:23 +0200

    start test

--- Process_WholeNL/LiDARProcess.R
+++ Process_WholeNL/LiDARProcess.R
@@ -1,22 +1,37 @@
 library(lidR)
 library(future)
+library(mapview)
 
 #Global settings
-workdir="D:/Sync/_Amsterdam/10_ProcessWholeNL/Test/"
+#workdir="D:/Sync/_Amsterdam/10_ProcessWholeNL/Test/"
+workdir="D:/Koma/ProcessWholeNL/TileGroup_9/"
 setwd(workdir)
 
-chunksize=1000
+chunksize=2500
 buffer=10
 resolution=10
 
 rasterOptions(maxmemory = 200000000000)
 
 # Set up cataloge
-plan(multisession)
+plan(multisession, workers = 5L)
+set_lidr_threads(5L)
+
+# Retile
 ctg <- catalog(workdir)
 
 opt_chunk_buffer(ctg) <- buffer
 opt_chunk_size(ctg) <- chunksize
-opt_output_files(ctg) <- paste(workdir,"normalized_neibased/{XLEFT}_{YBOTTOM}_norm",sep="")
+opt_output_files(ctg) <- paste(workdir,"retiled/{XLEFT}_{YBOTTOM}_",sep="")
+opt_filter(ctg) <- "-drop_class 6 9"
 
-normalized_ctg=lasnormalize(ctg,knnidw(k=25,p=2))
+newctg = catalog_retile(ctg)
+
+# Normalization
+newctg <- catalog(paste(workdir,"retiled/",sep=""))
+
+opt_chunk_buffer(newctg) <- buffer
+opt_chunk_size(newctg) <- chunksize
+opt_output_files(newctg) <- paste(workdir,"norm/{XLEFT}_{YBOTTOM}_norm",sep="")
+
+normalized_ctg=lasnormalize(newctg,knnidw(k=25,p=2))
