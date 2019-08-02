06417bd65c121d939aec846d087ef696e1676d49
Zsofia Koma <komazsofi@gmail.com>
2019-08-02 15:41:38 +0200

    adapt

--- Process_WholeNL/Metrics_calc.R
+++ Process_WholeNL/Metrics_calc.R
@@ -16,86 +16,63 @@ rasterOptions(maxmemory = 200000000000)
 # Set up cataloge
 plan(multisession, workers = 2L)
 
-# Execute
+# Set cataloge
 ctg <- catalog(workdir)
 
 opt_chunk_size(ctg) <- chunksize
 opt_output_files(ctg) <- ""
 
-# One by one
+# Calc metrics related to both ground and vegetation points
 
-h95p = grid_metrics(ctg, ~quantile(Z, 0.95),res=resolution,filter = ~Classification == 1L)
-proj4string(h95p) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(h95p,paste("h95p_",groupid,".tif",sep=""),overwrite=TRUE)
+opt_filter(ctg) <- "-keep_class 1 2"
 
-isd = grid_metrics(ctg, ~sd(Intensity),res=resolution)
-proj4string(isd) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(isd,paste("isd_",groupid,".tif",sep=""),overwrite=TRUE)
+myMetrics = function(Z,I,R,Classification) 
+{
+  metrics = list(
+    isd=sd(I),
+    echomean=mean(R),
+    lb1dense=(length(Z[Classification==1 & Z<1])/length(Z))*100,
+    l12dense=(length(Z[Classification==1 & Z>1 & Z<2])/length(Z))*100,
+    l23dense=(length(Z[Classification==1 & Z>1 & Z<2])/length(Z))*100,
+    l34dense=(length(Z[Classification==1 & Z>3 & Z<4])/length(Z))*100,
+    l45dense=(length(Z[Classification==1 & Z>4 & Z<5])/length(Z))*100,
+    l510dense=(length(Z[Classification==1 & Z>5 & Z<10])/length(Z))*100,
+    l1015dense=(length(Z[Classification==1 & Z>10 & Z<15])/length(Z))*100,
+    l1520dense=(length(Z[Classification==1 & Z>15 & Z<20])/length(Z))*100,
+    la20dense=(length(Z[Classification==1 & Z>20])/length(Z))*100,
+    pulsepen=(length(Z[Classification==2])/length(Z))*100
+    
+  )  
+  
+  return(metrics)
+}
 
-hsd = grid_metrics(ctg, ~sd(Z),res=resolution, filter = ~Classification == 1L)
-proj4string(hsd) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(hsd,paste("hsd_",groupid,".tif",sep=""),overwrite=TRUE)
+Metrics = grid_metrics(ctg, ~myMetrics(Z,Intensity,ReturnNumber,Classification), res=10)
+proj4string(Metrics ) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+writeRaster(Metrics ,paste("Metrics_",groupid,".tif",sep=""),overwrite=TRUE)
 
-hskew = grid_metrics(ctg, ~skewness(Z),res=resolution, filter = ~Classification == 1L)
-proj4string(hskew) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(hskew,paste("hskew_",groupid,".tif",sep=""),overwrite=TRUE)
+# Calc vegetation related metrics
 
-h25p = grid_metrics(ctg, ~quantile(Z, 0.25),res=resolution, filter = ~Classification == 1L)
-proj4string(h25p) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(h25p,paste("h25p_",groupid,".tif",sep=""),overwrite=TRUE)
+opt_filter(ctg) <- "-keep_class 1"
 
-h50p = grid_metrics(ctg, ~quantile(Z, 0.50),res=resolution, filter = ~Classification == 1L)
-proj4string(h50p) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(h50p,paste("h50p_",groupid,".tif",sep=""),overwrite=TRUE)
+myVegMetrics = function(Z) 
+{
+  library(e1071)
+  metrics = list(
+    h95p=quantile(Z, 0.95),
+    hsd=sd(Z),
+    hsd_b3=sd(Z[Z<3]),
+    hskew=skewness(Z),
+    hskew_b3=skewness(Z[Z<3]),
+    h25p=quantile(Z, 0.25),
+    h50p=quantile(Z, 0.50),
+    h75p=quantile(Z, 0.75),
+    nofretamean=(length(Z[Z>mean(Z)])/length(Z))*100
+  )  
+  
+  return(metrics)
+}
 
-h75p = grid_metrics(ctg, ~quantile(Z, 0.75),res=resolution, filter = ~Classification == 1L)
-proj4string(h75p) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(h75p,paste("h75p_",groupid,".tif",sep=""),overwrite=TRUE)
-
-echomean = grid_metrics(ctg, ~mean(ReturnNumber),res=resolution)
-proj4string(echomean) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(echomean,paste("echomean_",groupid,".tif",sep=""),overwrite=TRUE)
-
-lb1dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z<1])/length(Z))*100,res=resolution)
-proj4string(lb1dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(lb1dense,paste("lb1dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l12dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>1 & Z<2])/length(Z))*100,res=resolution)
-proj4string(l12dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l12dense,paste("l12dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l23dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>2 & Z<3])/length(Z))*100,res=resolution)
-proj4string(l23dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l23dense,paste("l23dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l34dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>3 & Z<4])/length(Z))*100,res=resolution)
-proj4string(l34dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l34dense,paste("l34dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l45dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>4 & Z<5])/length(Z))*100,res=resolution)
-proj4string(l45dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l45dense,paste("l45dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l510dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>5 & Z<10])/length(Z))*100,res=resolution)
-proj4string(l510dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l510dense,paste("l510dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l1015dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>10 & Z<15])/length(Z))*100,res=resolution)
-proj4string(l1015dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l1015dense,paste("l1015dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-l1520dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>15 & Z<20])/length(Z))*100,res=resolution)
-proj4string(l1520dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(l1520dense,paste("l1520dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-la20dense = grid_metrics(ctg, ~(length(Z[Classification==1 & Z>20])/length(Z))*100,res=resolution)
-proj4string(la20dense) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(la20dense,paste("la20dense_",groupid,".tif",sep=""),overwrite=TRUE)
-
-pulsepen = grid_metrics(ctg, ~(length(Z[Classification==2])/length(Z))*100,res=resolution)
-proj4string(pulsepen) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(pulsepen,paste("pulsepen_",groupid,".tif",sep=""),overwrite=TRUE)
-
-nofretamean = grid_metrics(ctg, ~(length(Z[Z>mean(Z)])/length(Z))*100,res=resolution,filter = ~Classification == 1L)
-proj4string(nofretamean) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-writeRaster(nofretamean,paste("nofretamean_",groupid,".tif",sep=""),overwrite=TRUE)
+VegMetrics = grid_metrics(ctg, ~myVegMetrics(Z), res=10)
+proj4string(VegMetrics) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+writeRaster(VegMetrics,paste("VegMetrics_",groupid,".tif",sep=""),overwrite=TRUE)
