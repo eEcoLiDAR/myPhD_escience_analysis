26e64d66b9d07aee85626bd9fd2a86ec7acba83e
Zsofia Koma <komazsofi@gmail.com>
2019-10-04 16:18:26 +0200

    make it so

--- andras_project/anal_hobo.R
+++ andras_project/anal_hobo.R
@@ -6,32 +6,41 @@ library(ggplot2)
 library(scales)
 library(gridExtra)
 
-workingdir="C:/Koma/Sync/_Amsterdam/11_AndrasProject/FieldData/"
+workingdir="D:/Sync/_Amsterdam/11_AndrasProject/tisza/2017_poroszlo/"
 setwd(workingdir)
 
-data=read.csv("1_sensors_tisza_field_2016_07_26_29.csv")
+masterlist=list.files(pattern="*masterfile.csv")
+
+data=read.csv(masterlist[1])
 
 for (i in seq(1,length(data$hobo))) {
   print(i)
   
-  hobo <- read_excel(paste(workingdir,data$dir[i],"/",data$hobo[i],sep=""),sheet = 1,skip=1)
+  hobo <- read_excel(paste(data$dir[i],"/",data$hobo[i],sep=""),sheet = 1,skip=1)
   names(hobo)<-c('nofmes','date','temp','none')
   df <- subset(hobo, select = c('nofmes','date','temp'))
   
   df$date = format(as.POSIXct(df$date,format="%m/%d/%y %H:%M:%S",tz=Sys.timezone()), "%Y-%m-%d %H:%M:%S")
+  
+  df=df[which(df$date>as.POSIXct(data$startdate[1],format="%Y-%m-%d %H:%M:%S",tz=Sys.timezone())),]
+  df=df[which(df$date<as.POSIXct(data$enddate[1],format="%Y-%m-%d %H:%M:%S",tz=Sys.timezone())),]
   
   df$point_name <- data$point_name[i]
   df$veg_type <- data$veg_type[i]
   
   df$temp <- as.numeric(df$temp)
   
-  write.csv(df,paste(workingdir,"/1_sensors_tisza_field_2016/",data$water_temp[i],"_",data$class[i],".csv",sep=""))
+  write.csv(df,paste(workingdir,data$transectid[i],"_",data$water_temp[i],"_cleaned.csv",sep=""))
+  
+  data$min_temp[i] <- min(df$temp)
+  data$max_temp[i] <- max(df$temp)
+  data$range_temp[i] <- range(df$temp)
+  data$mean_temp[i] <- mean(df$temp)
   
 }
 
-setwd(paste(workingdir,"/1_sensors_tisza_field_2016/",sep=""))
-
-files <- list.files(pattern = "*.csv")
+# Visualize
+files <- list.files(pattern = "*cleaned.csv")
 
 allcsv <- lapply(files,function(j){
   read.csv(j, header=TRUE)
@@ -42,6 +51,17 @@ allcsv_df <- do.call(rbind.data.frame, allcsv)
 ggplot(allcsv_df, aes(x=as.POSIXct(date), y=temp,color=point_name)) + geom_line(size=2) + theme_minimal() +
   scale_x_datetime(labels = date_format("%d/%H:%M"),date_breaks = "5 hours") +
   theme(axis.text.x = element_text(angle=45))+
-  xlab("Date")+ylab("Temperature [°C]")
+  xlab("Date")+ylab("Water temperature [°C]")
 
-ggsave("1_sensors_tisza_field_2016.png",width = 20, height = 10)
+ggsave(paste(data$transectid[1],".png",sep=""),width = 20, height = 10)
+
+# Normalize
+controlid=grep("control", data$point_name)
+
+data$diff_min_temp2 <- data$min_temp-data$min_temp[controlid[2]]
+data$diff_max_temp2 <- data$max_temp-data$max_temp[controlid[2]]
+data$diff_range_temp2 <- data$range_temp-data$range_temp[controlid[2]]
+data$diff_mean_temp2 <- data$mean_temp-data$mean_temp[controlid[2]]
+
+write.csv(data,paste(workingdir,data$transectid[1],"_masterfile_final.csv",sep=""))
+
--- andras_project/create_transects.R
+++ andras_project/create_transects.R
@@ -2,12 +2,14 @@ library(readxl)
 library(data.table)
 library(rgdal)
 
-workingdir="C:/Koma/Sync/_Amsterdam/11_AndrasProject/FieldData/"
-setwd(workingdir)
-
 filename="tisza"
 #filename="balaton"
 #filename="ferto"
+
+maxdist=1000
+
+workingdir="D:/Sync/_Amsterdam/11_AndrasProject/"
+setwd(paste(workingdir,"/",filename,"/",sep=""))
 
 # Separate shapefile
 shp=readOGR(".",filename)
@@ -19,24 +21,15 @@ for (g in seq(1,length(controlid))) {
   print(g)
   
   shp.df$distcont <- sqrt((shp.df$coords.x1[controlid[g]]-shp.df$coords.x1)^2+(shp.df$coords.x2[controlid[g]]-shp.df$coords.x2)^2)
-  control=shp.df[which(shp.df$distcont<1000),]
+  control=shp.df[which(shp.df$distcont<maxdist),]
   
   control$groupid <- g
   
-  write.csv(control,paste(g,"_",filename,".csv",sep=""))
+  write.csv(control,paste(g,"_",filename,'_',paste(as.character(control$water_temp),collapse="_"),"formaster.csv",sep=""))
+  
+  print(g)
+  print(paste(as.character(control$water_temp),collapse="_") )
 }
 
-files <- list.files(pattern = paste("_",filename,".csv",sep=""))
 
-allcsv <- lapply(files,function(i){
-  read.csv(i, header=TRUE)
-})
-
-allcsv_df <- do.call(rbind.data.frame, allcsv)
-
-coordinates(allcsv_df)=~coords.x1+coords.x2
-proj4string(allcsv_df)<- CRS("+proj=utm +zone=34 +datum=WGS84 +units=m +no_defs")
-
-# Export shapefile
-rgdal::writeOGR(allcsv_df, '.', paste(filename,"_grouped",sep=""), 'ESRI Shapefile',overwrite_layer = TRUE)
 
