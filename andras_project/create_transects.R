dec631b1c9deefa7ed0ef50435d661c9b8fbb9e5
komazsofi <komazsofi@gmail.com>
2019-10-09 22:08:21 +0200

    small change

--- andras_project/create_transects.R
+++ andras_project/create_transects.R
@@ -33,30 +33,4 @@ for (filename in locations) {
   }
 }
 
-maxdist=1000
-
-workingdir="D:/Sync/_Amsterdam/11_AndrasProject/balaton/"
-setwd(paste(workingdir,"/",filename,"/",sep=""))
-
-# Separate shapefile
-shp=readOGR(".",filename)
-shp.df <- as(shp, "data.frame")
-
-controlid=grep("control", shp.df$point_name)
-
-for (g in seq(1,length(controlid))) {
-  print(g)
-  
-  shp.df$distcont <- sqrt((shp.df$coords.x1[controlid[g]]-shp.df$coords.x1)^2+(shp.df$coords.x2[controlid[g]]-shp.df$coords.x2)^2)
-  control=shp.df[which(shp.df$distcont<maxdist),]
-  
-  control$groupid <- g
-  
-  write.csv(control,paste(g,"_",filename,'_',paste(as.character(control$water_temp),collapse="_"),"formaster.csv",sep=""))
-  
-  print(g)
-  print(paste(as.character(control$water_temp),collapse="_") )
-}
-
-
 
